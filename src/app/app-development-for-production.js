0ee89248ec712b586a7e3387501289a4b3ff9630
Yariv Livay <yarivlivay@gmail.com>
2016-04-02 00:23:07 +0300

    Let defaults do their magic

--- src/app/app-development-for-production.js
+++ src/app/app-development-for-production.js
@@ -1,6 +1,5 @@
 angular.module( 'App' ).config( function( EnvironmentProvider, $compileProvider )
 {
-	EnvironmentProvider.env = 'production';
 	EnvironmentProvider.buildType = 'development';
 	$compileProvider.debugInfoEnabled( true );
 } );
--- src/auth/app-development-for-production.js
+++ src/auth/app-development-for-production.js
@@ -1,6 +1,5 @@
 angular.module( 'App' ).config( function( EnvironmentProvider, $compileProvider, $locationProvider )
 {
-	EnvironmentProvider.env = 'production';
 	EnvironmentProvider.buildType = 'development';
 	$compileProvider.debugInfoEnabled( true );
 } );
