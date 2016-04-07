f91f8b5b5e86a292df61a63c739efd422ac87a80
David DeCarmine <hworld@holo-world.com>
2016-04-07 06:24:26 +0100

    Adding copy in for owned games in library.

--- src/app/views/library/collection/collection-controller.js
+++ src/app/views/library/collection/collection-controller.js
@@ -99,12 +99,12 @@ angular.module( 'App.Views' ).controller( 'Library.CollectionCtrl', function(
 			user = new User( payload.user );
 			this.user = user;
 			this.isOwner = App.user && user.id == App.user.id;
-			// if ( this.isOwner ) {
-			// 	App.title = gettextCatalog.getString( 'library.collection.developer_owner_page_title', { user: user.display_name } );
-			// }
-			// else {
-			// 	App.title = gettextCatalog.getString( 'library.collection.developer_page_title', { user: user.display_name } );
-			// }
+			if ( this.isOwner ) {
+				App.title = gettextCatalog.getString( 'Your Owned Games', { user: user.display_name } );
+			}
+			else {
+				App.title = gettextCatalog.getString( 'Games Owned by {{ user }}', { user: user.display_name } );
+			}
 		}
 		else if ( this.type == 'bundle' ) {
 			bundle = new GameBundle( payload.bundle );
