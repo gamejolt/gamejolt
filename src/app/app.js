3035e78226138381584833ade9822c86712093d7
David DeCarmine <hworld@holo-world.com>
2016-04-14 13:23:21 +0100

    Tracking source/referrer for all game related ticks.

--- src/app/app.js
+++ src/app/app.js
@@ -62,6 +62,7 @@ angular.module( 'App', [
 	'gj.Scroll.AutoScroll',
 	'gj.Scroll.FixedResizer',
 	'gj.EditableAccordion',
+	'gj.Referrer',
 	'gj.HistoryTick',
 	'gj.Pagination',
 	'gj.Modal.Confirm',
