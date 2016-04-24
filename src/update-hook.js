9460d89c4a5d34466dc8485c53eaa6a2284a9d6e
YLivay <yarivlivay@gmail.com>
2016-04-24 14:16:32 +0300

    Fix update game wrapper pre update hook for all OSes

--- src/update-hook.js
+++ src/update-hook.js
@@ -1,23 +1,70 @@
 var fs = require( 'fs' );
 var path = require( 'path' );
 
+function copy( source, target )
+{
+	return new Promise( function( resolve, reject )
+	{
+		var readStream = fs.createReadStream( source );
+		readStream.on( 'error', reject );
+
+		var writeStream = fs.createWriteStream( target );
+		writeStream.on('error', reject );
+
+		writeStream.on( 'close', function()
+		{
+			resolve();
+		} );
+
+		readStream.pipe( writeStream );
+	} );
+}
+
+function unlink( source )
+{
+	return new Promise( function( resolve, reject )
+	{
+		fs.unlink( source, function( err )
+		{
+			if ( err ) {
+				return reject( err );
+			}
+			resolve();
+		} );
+	} );
+}
+
+function rename( source, target )
+{
+	return new Promise( function( resolve, reject )
+	{
+		fs.rename( source, target, function( err )
+		{
+			if ( err ) {
+				return reject( err );
+			}
+			resolve();
+		} );
+	} );
+}
+
 module.exports.pre = function( version, packagePath )
 {
 	var nodeModules, gameWrapper;
 	switch ( process.platform )
 	{
 		case 'win32':
-			nodeModules = path.join( packagePath, 'node_modules' );
+			nodeModules = path.join( packagePath, '..', 'node_modules' );
 			gameWrapper = 'game_jolt_game_wrapper_win32.exe';
 			break;
 
 		case 'linux':
-			nodeModules = path.join( packagePath, 'node_modules' );
+			nodeModules = path.join( packagePath, '..', 'node_modules' );
 			gameWrapper = 'game_jolt_game_wrapper_linux';
 			break;
 
 		case 'darwin':
-			nodeModules = path.join( packagePath, 'app.nw', 'node_modules' );
+			nodeModules = path.join( packagePath, 'node_modules' );
 			gameWrapper = 'game_jolt_game_wrapper_osx';
 			break;
 	}
@@ -52,27 +99,47 @@ module.exports.pre = function( version, packagePath )
 			return true;
 		}
 
-		return new Promise( function( resolve, reject )
-		{
-			fs.rename( gameWrapperPath, gameWrapperPath + '.2', function( err )
+		var shouldContinue = true;
+		return unlink( gameWrapperPath + '.2' )
+			.catch( function( err ) {} )
+			.then( function()
 			{
-				if ( err ) {
-					console.log( 'Failed to rename ' + gameWrapperPath + ' (' + err.message + '). Aborting update.' );
-					return resolve( false );
+				return copy( gameWrapperPath, gameWrapperPath + '.2' );
+			} )
+			.catch( function( err )
+			{
+				console.log( 'Failed to copy ' + gameWrapperPath + ' (' + err.message + '). Aborting update.' );
+				shouldContinue = false;
+			} )
+			.then( function()
+			{
+				if ( !shouldContinue ) {
+					return;
 				}
 
-				try {
-					fs.renameSync( gameWrapperPath + '.2', gameWrapperPath )
-				}
-				catch ( err ) {
-					console.log( 'Failed to rename ' + gameWrapperPath + '.2 back (' + err.message + '). Continuing anyways.' );
-					return resolve( true );
+				return unlink( gameWrapperPath );
+			} )
+			.catch ( function( err )
+			{
+				console.log( 'Failed to unlink ' + gameWrapperPath + ' (' + err.message + '). Aborting update.' );
+				shouldContinue = false;
+			} )
+			.then( function()
+			{
+				if ( !shouldContinue ) {
+					return;
 				}
 
-				console.log( 'Renamed ' + gameWrapperPath + ' successfully. Updating.')
-				return resolve( true );
+				return rename( gameWrapperPath + '.2', gameWrapperPath );
+			} )
+			.catch( function( err )
+			{
+				console.log( 'Failed to rename ' + gameWrapperPath + '.2 back (' + err.message + '). Continuing anyways.' );
+			} )
+			.then( function()
+			{
+				return shouldContinue;
 			} );
-		} );
 	} );
 };
 
