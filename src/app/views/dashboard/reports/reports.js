4422614cabbfa9265b098e582d16d16969036b12
David DeCarmine <hworld@holo-world.com>
2016-04-25 17:59:20 +0100

    Bunch of work on the analytics section for games.

--- src/app/views/dashboard/reports/reports.js
+++ src/app/views/dashboard/reports/reports.js
@@ -5,11 +5,28 @@ angular.module( 'App.Views' ).config( function( $stateProvider )
 		controller: 'Dashboard.ReportsCtrl',
 		controllerAs: 'reportsCtrl',
 		templateUrl: '/app/views/dashboard/reports/reports.html',
+		redirectTo: function()
+		{
+			this.go( 'dashboard.reports.view', {
+				period: 'all',
+				resource: 'Game',
+				resourceId: 1050,
+				eventType: 'view',
+			} );
+		},
 		resolve: {
 			payload: function( Api )
 			{
 				return Api.sendRequest( '/web/dash/reports' );
 			}
-		}
+		},
+	} );
+
+	$stateProvider.state( 'dashboard.reports.view', {
+		url: '/:period/:resource/:resourceId/:eventType?year&month',
+		controller: function( $scope, $stateParams )
+		{
+			$scope.reportsCtrl.stateChanged( $stateParams );
+		},
 	} );
 } );
