799c950967f931e9b6f0dbf135e526ead4244101
Nik Sudan <niksudan@gmail.com>
2016-04-27 20:56:24 +0100

    Make fade collapse work on text post content

--- src/app/components/devlog/post/text/text-directive.js
+++ src/app/components/devlog/post/text/text-directive.js
@@ -2,6 +2,9 @@ angular.module( 'App.Devlog.Post.Text' ).directive( 'gjDevlogPostText', function
 {
 	return {
 		restrict: 'AE',
-		templateUrl: '/app/components/devlog/post/text/text.html'
+		templateUrl: '/app/components/devlog/post/text/text.html',
+		bindToController: true,
+		controllerAs: 'postCtrl',
+		controller: function() {}
 	};
 } );
