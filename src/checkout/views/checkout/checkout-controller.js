074b2dbc21c918081fd7aef6b27296f1b52edb56
David DeCarmine <hworld@holo-world.com>
2016-05-01 17:13:41 +0100

    Redirect to correct location after buying game in client.

--- src/checkout/views/checkout/checkout-controller.js
+++ src/checkout/views/checkout/checkout-controller.js
@@ -1,4 +1,4 @@
-angular.module( 'App.Views' ).controller( 'CheckoutCtrl', function( $window, App, Environment, Sellable, Game, payload )
+angular.module( 'App.Views' ).controller( 'CheckoutCtrl', function( $window, App, Environment, Sellable, Game, Growls, payload )
 {
 	this.cards = payload.cards || [];
 	this.sellable = new Sellable( payload.sellable );
@@ -11,6 +11,23 @@ angular.module( 'App.Views' ).controller( 'CheckoutCtrl', function( $window, App
 
 	this.onSubmit = function( $response )
 	{
-		$window.location = $response.redirectUrl;
+		var redirect = null;
+
+		// For client, the orders are always done as a user.
+		// We will always go back to game page in those cases.
+		// For non-users on site they may have to go to a key page.
+		if ( Environment.isClient ) {
+			redirect = Environment.wttfBaseUrl + '/games/' + this.game.slug + '/' + this.game.id;
+		}
+		else {
+			redirect = $response.redirectUrl;
+		}
+
+		if ( !redirect ) {
+			Growls.error( 'Could not redirect.' );
+			return;
+		}
+
+		$window.location.href = redirect;
 	};
 } );
