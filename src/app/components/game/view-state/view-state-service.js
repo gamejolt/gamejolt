7b2fd0000b5a11e40379e0221e30798783b82081
Nik Sudan <niksudan@gmail.com>
2016-05-08 13:31:10 +0100

    Game_ViewState now tracks if a game is a devlog
    A game becomes a devlog if it is in development

--- src/app/components/game/view-state/view-state-service.js
+++ src/app/components/game/view-state/view-state-service.js
@@ -1,10 +1,11 @@
-angular.module( 'App.Game.ViewState' ).service( 'Game_ViewState', function()
+angular.module( 'App.Game.ViewState' ).service( 'Game_ViewState', function( Game )
 {
 	this.setGame = function( game )
 	{
 		this.game = game;
 		this.hasCover = false;
 		this.showExtendedHeader = false;
+		this.isDevlog = this.game.development_status == Game.DEVELOPMENT_STATUS_WIP;
 	};
 
 	this.showExtended = function()
