15b6934c767125bdef12bfa6078e296f9dc9fd2f
David DeCarmine <hworld@holo-world.com>
2016-05-11 13:00:04 +0100

    Add social stuff to the marketplace landing page.

--- src/app/views/landing/marketplace/marketplace-controller.js
+++ src/app/views/landing/marketplace/marketplace-controller.js
@@ -1,8 +1,13 @@
-angular.module( 'App.Views' ).controller( 'MarketplaceCtrl', function( App, Game, Fireside_Post, payload )
+angular.module( 'App.Views' ).controller( 'MarketplaceCtrl', function( App, Meta, Game, Fireside_Post, payload )
 {
 	var _this = this;
 
-	App.title = 'Selling Games on Game Jolt';
+	App.title = 'Sell Your Games';
+
+	Meta.description = payload.metaDescription;
+	Meta.fb = payload.fb;
+	Meta.twitter = payload.twitter;
+	Meta.fb.image = Meta.twitter.image = '/app/views/landing/marketplace/social.png';
 
 	this.firesidePosts = Fireside_Post.populate( payload.firesidePosts );
 	this.games = Game.populate( payload.games );
