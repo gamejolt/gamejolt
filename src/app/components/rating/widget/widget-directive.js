5a4365a69f4632b5433903688038eacae024d4c1
David DeCarmine <hworld@holo-world.com>
2016-05-15 00:56:23 +0100

    Change rating widget to be light by default with the ability to go on dark background. Rating widget can also be used a bit easier on XS size. Removed the bouncing animation when clicking on a rating because it wasn't working.

--- src/app/components/rating/widget/widget-directive.js
+++ src/app/components/rating/widget/widget-directive.js
@@ -10,7 +10,7 @@ angular.module( 'App.Rating.Widget' ).directive( 'gjRatingWidget', function()
 		},
 		bindToController: true,
 		controllerAs: 'ctrl',
-		controller: function( $rootScope, $q, $transition, $element, Game_Rating, gettextCatalog )
+		controller: function( $rootScope, Game_Rating, gettextCatalog )
 		{
 			var _this = this;
 
@@ -55,25 +55,6 @@ angular.module( 'App.Rating.Widget' ).directive( 'gjRatingWidget', function()
 						_this.isProcessing = false;
 
 						$rootScope.$broadcast( 'onGameRatingChange', _this.game.id );
-					} )
-					.then( function()
-					{
-						// Does a bounce animation after rating.
-						var blips = $element[0].querySelectorAll( '.rating-widget-icon:nth-child(-n+' + index + ')' );
-						if ( blips.length ) {
-							blips = angular.element( blips );
-
-							var anims = [];
-							for ( var i = 0; i < blips.length; ++i ) {
-								var blip = blips.eq( i );
-								anims.push( $transition( blip, 'rating-widget-icon-bounce', { animation: true } ) );
-							}
-
-							$q.all( anims ).then( function()
-							{
-								blips.removeClass( 'rating-widget-icon-bounce' );
-							} );
-						}
 					} );
 			};
 
