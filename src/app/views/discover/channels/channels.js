c84c35555c68d30bd2a27074e91dba9312d208f8
David DeCarmine <hworld@holo-world.com>
2016-05-20 16:40:32 +0100

    Lazy load channel controllers.

--- src/app/views/discover/channels/channels.js
+++ src/app/views/discover/channels/channels.js
@@ -4,5 +4,13 @@ angular.module( 'App.Views' ).config( function( $stateProvider )
 		url: '/channels',
 		abstract: true,
 		template: '<ui-view></ui-view>',
+		resolve: {
+
+			// Channel controllers are lazy loaded.
+			components: function( $ocLazyLoad )
+			{
+				return $ocLazyLoad.load( '/app/modules/channels.js' );
+			},
+		}
 	} );
 } );
