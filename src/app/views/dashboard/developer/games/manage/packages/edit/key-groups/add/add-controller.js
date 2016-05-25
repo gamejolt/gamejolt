bf75caf9eb32adc59bfa68c2f106a78721da620c
Ashley Gwinnell <ashley@forceofhab.it>
2016-05-25 16:54:24 +0100

    Refactor key / key-group form code to use _save and _remove on the models.

--- src/app/views/dashboard/developer/games/manage/packages/edit/key-groups/add/add-controller.js
+++ src/app/views/dashboard/developer/games/manage/packages/edit/key-groups/add/add-controller.js
@@ -1,7 +1,13 @@
 angular.module( 'App.Views' ).controller( 'Dashboard.Developer.Games.Manage.Packages.Edit.KeyGroups.AddCtrl', function(
-	$scope, $sce, $state, Environment, Sellable, Sellable_KeyGroup )
+	$scope, $sce, $state, Environment, Sellable, Sellable_KeyGroup, Key )
 {
 	var _this = this;
 
+	this.onKeyGroupAdded = function( formModel )
+	{
+		console.log('onKeyGroupAdded');
+		console.log( formModel );
+		//$state.go( 'dashboard.developer.games.manage.packages.edit.keygroups.edit', { sellableId: formModel.sellable_id, keyGroupId: formModel.id } );
+	};
 
 } );
--- src/app/views/dashboard/developer/games/manage/packages/edit/key-groups/edit/edit-controller.js
+++ src/app/views/dashboard/developer/games/manage/packages/edit/key-groups/edit/edit-controller.js
@@ -7,6 +7,31 @@ angular.module( 'App.Views' ).controller( 'Dashboard.Developer.Games.Manage.Pack
 	this.keyGroup = keyGroupPayload.keyGroup ? new Sellable_KeyGroup( keyGroupPayload.keyGroup ) : null;
 	this.keys = keyGroupPayload.keys ? Key.populate( keyGroupPayload.keys ) : [];
 
+	this.onKeyGroupSaved = function( model )
+	{
+		Growls.success(
+						'Saved',
+						'Success'
+					);
+	};
+
+	this.removeGroup = function( keyGroup, disableKeys )
+	{
+		ModalConfirm.show( 'Remove Key Group?' )//gettextCatalog.getString( 'dash.games.remove_confirmation' ) )
+			.then( function()
+			{
+				return keyGroup.$remove( disableKeys );
+			} )
+			.then( function()
+			{
+				Growls.success(
+					'Removed', //gettextCatalog.getString( 'dash.games.removed_growl' ),
+					'Removed' //gettextCatalog.getString( 'dash.games.removed_growl_title' )
+				);
+				$state.go( 'dashboard.developer.games.manage.packages.edit.keygroups.list' );
+			} );
+	}
+
 	this.removeKey = function( key )
 	{
 		ModalConfirm.show( 'Remove Key?' )//gettextCatalog.getString( 'dash.games.remove_confirmation' ) )
