07f7defed0deee07d73f30178f53d3da0bc1cc34
David DeCarmine <hworld@holo-world.com>
2016-05-25 21:56:11 +0100

    Redirect to https on financials form.

--- src/app/views/dashboard/account/financials/financials.js
+++ src/app/views/dashboard/account/financials/financials.js
@@ -5,6 +5,9 @@ angular.module( 'App.Views' ).config( function( $stateProvider )
 		controller: 'Dashboard.Account.FinancialsCtrl',
 		controllerAs: 'financialsCtrl',
 		templateUrl: '/app/views/dashboard/account/financials/financials.html',
+		data: {
+			secure: true,
+		},
 		resolve: {
 			payload: function( Api )
 			{
