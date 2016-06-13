76e5a5d37dd7ae5ded25d44f5e5a7db0c6308c0d
David DeCarmine <hworld@holo-world.com>
2016-06-13 19:36:54 +0200

    Some work for adding devlog posts.

--- src/app/components/forms/dashboard/game/devlog-post/devlog-post-directive.js
+++ src/app/components/forms/dashboard/game/devlog-post/devlog-post-directive.js
@@ -1,4 +1,4 @@
-angular.module( 'App.Forms.Dashboard' ).directive( 'gjFormDashboardGameDevlogPost', function( Form )
+angular.module( 'App.Forms.Dashboard' ).directive( 'gjFormDashboardGameDevlogPost', function( Form, Fireside_Post )
 {
 	var form = new Form( {
 		model: 'Fireside_Post',
@@ -6,10 +6,16 @@ angular.module( 'App.Forms.Dashboard' ).directive( 'gjFormDashboardGameDevlogPos
 	} );
 
 	form.scope.game = '=gjGame';
+	form.scope.postType = '=';
 
 	form.onInit = function( scope )
 	{
+		scope.Fireside_Post = Fireside_Post;
+
 		scope.formModel.game_id = scope.game.id;
+		scope.formModel.type = scope.postType;
+
+		scope.formModel.status = Fireside_Post.STATUS_ACTIVE;
 	};
 
 	return form;
