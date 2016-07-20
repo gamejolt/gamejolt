e994c264d2441548c3ec827179c4d6d35cf2380e
David DeCarmine <hworld@holo-world.com>
2016-07-21 01:29:24 +0800

    Get location enforcing working on devlog posts.

--- src/app/views/discover/games/view/devlog/view/view-controller.ts
+++ src/app/views/discover/games/view/devlog/view/view-controller.ts
@@ -1,6 +1,7 @@
 import { Injectable, Inject } from 'ng-metadata/core';
 import { Fireside_Post } from './../../../../../../../lib/gj-lib-client/components/fireside/post/post-model';
 import { App } from './../../../../../../app-service';
+import { Location } from './../../../../../../../lib/gj-lib-client/components/location/location-service';
 
 @Injectable()
 export class ViewCtrl
@@ -8,9 +9,11 @@ export class ViewCtrl
 	post: Fireside_Post;
 
 	constructor(
+		@Inject( '$scope' ) $scope: ng.IScope,
 		@Inject( 'App' ) app: App,
 		@Inject( 'Fireside_Post' ) firesidePostModel: typeof Fireside_Post,
-		@Inject( 'payload' ) payload: any
+		@Inject( 'payload' ) payload: any,
+		@Inject( 'Location' ) location: Location,
 	)
 	{
 		this.post = new firesidePostModel( payload.post );
@@ -18,5 +21,10 @@ export class ViewCtrl
 		this.post.$expanded();
 
 		app.title = this.post.title;
+
+		location.enforce( {
+			slug: $scope['gameCtrl'].game.slug,
+			postSlug: this.post.slug,
+		} );
 	}
 }
