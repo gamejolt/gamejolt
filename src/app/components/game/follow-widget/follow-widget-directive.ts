c3a521f6f764d6d05c0b949434bee0a0799c576b
David DeCarmine <hworld@holo-world.com>
2016-07-27 15:36:00 +0800

    Follow widget tweaks.

--- src/app/components/game/follow-widget/follow-widget-directive.ts
+++ src/app/components/game/follow-widget/follow-widget-directive.ts
@@ -10,31 +10,36 @@ export class FollowWidgetComponent
 	@Input( '<' ) game: any;
 
 	followTooltip: string;
+	isProcessing = false;
 
 	constructor(
 		@Inject( 'gettextCatalog' ) private gettextCatalog: ng.gettext.gettextCatalog,
 		@Inject( 'Growls' ) private Growls: any,
-		@Inject( 'Analytics' ) private Analytics: any,
 	)
 	{
 		this.followTooltip = gettextCatalog.getString( 'Follow this game to add it to your Library and be notified when new posts are added.' );
 	}
 
-	follow()
+	onClick()
 	{
+		if ( this.isProcessing ) {
+			return;
+		}
+
+		this.isProcessing = true;
+		let promise: ng.IPromise<any>;
+
 		if ( !this.game.is_following ) {
-			this.game.$follow()
+			promise = this.game.$follow()
 				.catch( () =>
 				{
 					this.Growls.error(
 						this.gettextCatalog.getString( 'Something has prevented you from following this game.' )
 					);
 				} );
-
-			this.Analytics.trackEvent( 'game-following', 'follow' );
 		}
 		else {
-			this.game.$unfollow()
+			promise = this.game.$unfollow()
 				.catch( () =>
 				{
 					this.Growls.error(
@@ -42,8 +47,8 @@ export class FollowWidgetComponent
 						this.gettextCatalog.getString( 'library.followed.remove_game_error_growl_title' )
 					);
 				} );
-
-			this.Analytics.trackEvent( 'game-following', 'unfollow' );
 		}
+
+		promise.then( () => this.isProcessing = false );
 	}
 }
