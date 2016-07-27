ef0bac0f9d6a1032c009dcf737efda43daa3844b
David DeCarmine <hworld@holo-world.com>
2016-07-27 20:11:11 +0800

    Better translation in the dev stage selector.

--- src/app/components/forms/dashboard/game/dev-stage-selector/confirm-controller.ts
+++ src/app/components/forms/dashboard/game/dev-stage-selector/confirm-controller.ts
@@ -7,16 +7,35 @@ export class ModalCtrl
 	to: string;
 	action: string;
 
+	fromTranslated: string;
+	toTranslated: string;
+
 	constructor(
 		@Inject( '$modalInstance' ) private $modalInstance: any,
 		@Inject( 'Game' ) private gameModel: any,
 		@Inject( 'game' ) public game: any,
 		@Inject( 'stage' ) public stage: number,
+		@Inject( 'gettextCatalog' ) private gettextCatalog: ng.gettext.gettextCatalog,
 	)
 	{
 		this.from = this._getStatusString( this.game.development_status );
 		this.to = this._getStatusString( this.stage );
 		this.action = `${this.from}:${this.to}`;
+
+		this.fromTranslated = this._getStatusTranslated( this.game.development_status );
+		this.toTranslated = this._getStatusTranslated( this.stage );
+	}
+
+	private _getStatusTranslated( stage: number )
+	{
+		if ( stage == this.gameModel.DEVELOPMENT_STATUS_DEVLOG ) {
+			return this.gettextCatalog.getString( 'devlog-only', {}, 'development status' )
+		}
+		else if ( stage == this.gameModel.DEVELOPMENT_STATUS_WIP ) {
+			return this.gettextCatalog.getString( 'early access', {}, 'development status' )
+		}
+
+		return this.gettextCatalog.getString( 'complete', {}, 'development status' )
 	}
 
 	private _getStatusString( stage: number )
@@ -28,7 +47,7 @@ export class ModalCtrl
 			return 'wip';
 		}
 
-		return 'finished';
+		return 'complete';
 	}
 
 	proceed()
