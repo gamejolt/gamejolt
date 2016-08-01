30340c3baedbb80e2183381ed0df01bb06c996b9
David DeCarmine <hworld@holo-world.com>
2016-08-01 17:39:15 +0800

    Delete devlogs landing page.

--- src/app/views/landing/devlogs/devlogs.ts
+++
@@ -1,7 +0,0 @@
-import { provide } from 'ng-metadata/core';
-import { DevlogsCtrl } from './devlogs-controller';
-
-export default angular.module( 'App.Views.Landing.Devlogs', [
-] )
-.controller( ...provide( 'Landing.DevlogsCtrl', { useClass: DevlogsCtrl } ) )
-.name;
--- src/app/views/landing/landing.ts
+++ src/app/views/landing/landing.ts
@@ -1,8 +1,6 @@
 import Marketplace from './marketplace/marketplace';
-import Devlogs from './devlogs/devlogs';
 
 export default angular.module( 'App.Views.Landing', [
 	Marketplace,
-	Devlogs,
 ] )
 .name;
