6682d997a30d63f8437cb826a5df54d8bdcd193d
David DeCarmine <hworld@holo-world.com>
2016-08-28 16:07:38 +0900

    Radio Redux!

---
+++ src/app/views/radio/radio-controller.ts
@@ -0,0 +1,36 @@
+import { Injectable, Inject } from 'ng-metadata/core';
+import { Meta } from './../../../lib/gj-lib-client/components/meta/meta-service';
+
+@Injectable()
+export class RadioCtrl
+{
+	song: any;
+	game: any;
+
+	currentSongTime: number;
+	currentSongDuration: number;
+
+	constructor(
+		@Inject( 'Meta' ) private meta: Meta,
+		@Inject( 'Api' ) private api: any,
+		@Inject( 'Game_Song' ) private songModel: any,
+		@Inject( 'Game' ) private gameModel: any,
+		@Inject( 'Analytics' ) private analytics: any,
+	)
+	{
+		meta.title = 'Indie Game Radio';
+		this.getNextSong();
+	}
+
+	getNextSong()
+	{
+		this.api.sendRequest( '/web/radio/get-random-song' )
+			.then( ( payload: any ) =>
+			{
+				this.song = new this.songModel( payload.song );
+				this.game = new this.gameModel( payload.game );
+				this.meta.title = this.song.title;
+				this.analytics.trackEvent( 'radio', 'load-song' );
+			} );
+	}
+}
