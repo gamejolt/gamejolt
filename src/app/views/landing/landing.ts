502e84a9fa51a11852e8f162dd4b15f0fc663adf
David DeCarmine <hworld@holo-world.com>
2016-10-30 15:37:13 -0600

    Pushing out what I have so far of game api landing page. Still needs a bunch of work.

--- src/app/views/landing/landing.ts
+++ src/app/views/landing/landing.ts
@@ -1,8 +1,10 @@
 import Marketplace from './marketplace/marketplace';
+import GameApi from './game-api/game-api';
 import GameApiDoc from './game-api-doc/game-api-doc';
 
 export default angular.module( 'App.Views.Landing', [
 	Marketplace,
+	GameApi,
 	GameApiDoc,
 ] )
 .name;
