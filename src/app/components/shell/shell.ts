3004ee2e7cdea0106a173aec8518d064d6748af8
David DeCarmine <hworld@holo-world.com>
2016-10-30 01:08:14 -0600

    Update shell/footer. Removed code for shell to show the left nav ever. Set the shell background to show the glitch background. The footer is totally redesigned.

--- src/app/components/shell/shell.ts
+++ src/app/components/shell/shell.ts
@@ -1,8 +1,6 @@
 import { provide } from 'ng-metadata/core';
 import { BodyComponent } from './body/body-directive';
-import { NavPaneComponent } from './nav-pane/nav-pane-directive';
 
 export default angular.module( 'App.Shell', [] )
 .directive( ...provide( BodyComponent ) )
-.directive( ...provide( NavPaneComponent ) )
 .name;
