253b4bb86dea352f2f0dfb822524f378c96337cf
David DeCarmine <hworld@holo-world.com>
2016-11-16 16:39:53 -0700

    About landing page.

--- src/app/views/landing/landing.ts
+++ src/app/views/landing/landing.ts
@@ -1,10 +1,12 @@
 import Learn from './learn/learn';
+import About from './about/about';
 import Marketplace from './marketplace/marketplace';
 import GameApi from './game-api/game-api';
 import GameApiDoc from './game-api-doc/game-api-doc';
 
 export default angular.module( 'App.Views.Landing', [
 	Learn,
+	About,
 	Marketplace,
 	GameApi,
 	GameApiDoc,
