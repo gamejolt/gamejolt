62c5007560a9fa0dcffba5ec5f09f2a6f39643f9
David DeCarmine <hworld@holo-world.com>
2017-01-11 15:03:03 -0700

    Update CDN

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require( 'gulp' );
 
 var config = {
-	staticCdn: 'https://b6d3e9q9.ssl.hwcdn.net',
+	staticCdn: 'https://s.gjcdn.net',
+	injectVersion: 2,
 	extraBower: {
 		'angular-bootstrap': [
 			'src/transition/transition.js',
