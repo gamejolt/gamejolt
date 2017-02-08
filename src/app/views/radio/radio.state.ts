4193db561507ba216c309496438cc72bde0f20b1
David DeCarmine <hworld@holo-world.com>
2017-02-07 19:18:47 -0700

    Fix radio translations.

--- src/app/views/radio/radio.state.ts
+++ src/app/views/radio/radio.state.ts
@@ -5,6 +5,8 @@ makeState( 'radio', {
 	url: '/radio',
 	lazyLoad: () => $import( './radio.module' ),
 	resolve: {
+		/*@ngInject*/
+		init: ( Translate: any ) => Translate.loadSection( 'main' ),
 		payload: () => Api.sendRequest( '/site-api/discover' ),
 	},
 } );
