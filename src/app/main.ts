a2b8debe34f30f7bcc39264a5e1e9b185d8b38ea
David DeCarmine <hworld@holo-world.com>
2017-02-09 20:13:21 -0700

    Pull in hammer-vue loader

--- src/app/main.ts
+++ src/app/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 
 import { Loader } from '../lib/gj-lib-client/components/loader/loader.service';
 import { HammerLoader } from '../lib/gj-lib-client/components/loader/hammer-loader';
+import { HammerVueLoader } from '../lib/gj-lib-client/components/loader/hammer-vue-loader';
 import { UploadLoader } from '../lib/gj-lib-client/components/loader/upload-loader';
 import { JqueryLoader } from '../lib/gj-lib-client/components/loader/jquery-loader';
 import { UiTreeLoader } from '../lib/gj-lib-client/components/loader/ui-tree-loader';
@@ -14,6 +15,7 @@ import { ChartLoader } from '../lib/gj-lib-client/components/loader/chart-loader
 import { SpectrumLoader } from '../lib/gj-lib-client/components/loader/spectrum-loader';
 
 Loader.addLoader( new HammerLoader() );
+Loader.addLoader( new HammerVueLoader() );
 Loader.addLoader( new UploadLoader() );
 Loader.addLoader( new JqueryLoader() );
 Loader.addLoader( new UiTreeLoader() );
@@ -21,7 +23,7 @@ Loader.addLoader( new JcropLoader() );
 Loader.addLoader( new ChartLoader() );
 Loader.addLoader( new SpectrumLoader() );
 
-if ( GJ_BUILD_TYPE == 'production' ) {
+if ( GJ_BUILD_TYPE === 'production' ) {
 	enableProdMode();
 }
 
