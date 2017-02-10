0dd41fffc011938e76889e2d953fba2d9f5a69a6
David DeCarmine <hworld@holo-world.com>
2017-02-09 17:49:00 -0700

    Include Vue into the site.

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,7 @@
 /// <reference path="./src/lib/gj-lib-client/typings/html.d.ts" />
 /// <reference path="./src/lib/gj-lib-client/typings/angular-gettext.d.ts" />
 /// <reference path="./src/lib/gj-lib-client/typings/angular.d.ts" />
+/// <reference path="./src/lib/gj-lib-client/typings/vue.d.ts" />
 
 declare interface Window {
 	_: _.LoDashStatic;
