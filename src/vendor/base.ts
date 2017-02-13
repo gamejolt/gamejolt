774e13e188109afb14d03c245429ab98476f0f5a
David DeCarmine <hworld@holo-world.com>
2017-02-13 14:22:27 -0700

    Add more polyfills to fix IE11

--- src/vendor/base.ts
+++ src/vendor/base.ts
@@ -1,7 +1,16 @@
-import 'core-js/modules/es6.object.assign';
-import 'core-js/modules/es6.object.keys';
-import 'core-js/modules/es6.array.find';
-import 'core-js/modules/es6.array.find-index';
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
 import 'core-js/es6/promise';
 
 import 'core-js/modules/es7.object.values';
