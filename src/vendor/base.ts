e3d494afbdd5d575dd0ff5297b2207508294e285
David DeCarmine <hworld@holo-world.com>
2017-02-13 13:12:27 -0700

    Polyfill Promise. We eventually replace with $q, but there may be code that calls Promise before we bootstrap angular facade.

--- src/vendor/base.ts
+++ src/vendor/base.ts
@@ -2,6 +2,7 @@ import 'core-js/modules/es6.object.assign';
 import 'core-js/modules/es6.object.keys';
 import 'core-js/modules/es6.array.find';
 import 'core-js/modules/es6.array.find-index';
+import 'core-js/es6/promise';
 
 import 'core-js/modules/es7.object.values';
 
