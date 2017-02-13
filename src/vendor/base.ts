27cf15395bb72162f314f838fe3891a40981b30f
David DeCarmine <hworld@holo-world.com>
2017-02-13 14:47:13 -0700

    Update polyfillts.

--- src/vendor/base.ts
+++ src/vendor/base.ts
@@ -1,21 +1,4 @@
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
-// import 'core-js/es6/math';
-// import 'core-js/es6/string';
-// import 'core-js/es6/date';
-import 'core-js/es6/array';
-// import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
-import 'core-js/es6/promise';
-
-import 'core-js/modules/es7.object.values';
-
-import 'reflect-metadata';
+import '../lib/gj-lib-client/utils/polyfills';
 
 import 'angular';
 import 'angular-sanitize';
