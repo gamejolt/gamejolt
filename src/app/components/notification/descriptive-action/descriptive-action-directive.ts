c2ec5d05dd4660ddfcc17444e4bac3271d380d30
David DeCarmine <hworld@holo-world.com>
2017-02-16 13:09:29 -0700

    Fix notifications for sellables. Fixes: https://github.com/gamejolt/issue-tracker/issues/817

--- src/app/components/notification/descriptive-action/descriptive-action-directive.ts
+++ src/app/components/notification/descriptive-action/descriptive-action-directive.ts
@@ -8,33 +8,32 @@ import * as template from '!html-loader!./descriptive-action.html';
 })
 export class DescriptiveActionComponent
 {
-	@Input( '<' ) notification: any;
+	@Input( '<' ) notification: Notification;
 
 	translationKey: string;
 	translationValues: Object | null;
 
 	constructor(
-		@Inject( 'Notification' ) notificationModel: typeof Notification,
 		@Inject( 'gettext' ) gettext: ng.gettext.gettextFunction,
 		@Inject( 'gettextCatalog' ) private gettextCatalog: ng.gettext.gettextCatalog,
 		@Inject( 'currencyFilter' ) currencyFilter: ng.IFilterCurrency
 	)
 	{
 		const translationKeys = {
-			[ notificationModel.TYPE_COMMENT_ADD_OBJECT_OWNER ]: gettext( 'notifications.new_comment_title_html' ),
-			[ notificationModel.TYPE_COMMENT_ADD ]: gettext( 'notifications.comment_reply_title_html' ),
-			[ notificationModel.TYPE_FORUM_POST_ADD ]: gettext( 'notifications.forum_post_title_html' ),
-			[ notificationModel.TYPE_FRIENDSHIP_REQUEST ]: gettext( 'notifications.friendship_request_title_html' ),
-			[ notificationModel.TYPE_FRIENDSHIP_ACCEPT ]: gettext( 'notifications.friendship_accepted_title_html' ),
-			[ notificationModel.TYPE_GAME_RATING_ADD ]: gettext( 'notifications.rating_title_html' ),
-			[ notificationModel.TYPE_GAME_FOLLOW ]: gettext( 'notifications.game_follow_title_html' ),
-			[ notificationModel.TYPE_SELLABLE_SELL ]: gettext( 'bought a package in <strong>{{ object }}</strong> for <strong>{{ amount }}</strong>.' ),
+			[ Notification.TYPE_COMMENT_ADD_OBJECT_OWNER ]: gettext( 'notifications.new_comment_title_html' ),
+			[ Notification.TYPE_COMMENT_ADD ]: gettext( 'notifications.comment_reply_title_html' ),
+			[ Notification.TYPE_FORUM_POST_ADD ]: gettext( 'notifications.forum_post_title_html' ),
+			[ Notification.TYPE_FRIENDSHIP_REQUEST ]: gettext( 'notifications.friendship_request_title_html' ),
+			[ Notification.TYPE_FRIENDSHIP_ACCEPT ]: gettext( 'notifications.friendship_accepted_title_html' ),
+			[ Notification.TYPE_GAME_RATING_ADD ]: gettext( 'notifications.rating_title_html' ),
+			[ Notification.TYPE_GAME_FOLLOW ]: gettext( 'notifications.game_follow_title_html' ),
+			[ Notification.TYPE_SELLABLE_SELL ]: gettext( 'bought a package in <strong>{{ object }}</strong> for <strong>{{ amount }}</strong>.' ),
 		};
 
 		this.translationKey = translationKeys[ this.notification.type ];
 		this.translationValues = null;
 
-		if ( this.notification.type == notificationModel.TYPE_SELLABLE_SELL ) {
+		if ( this.notification.type === Notification.TYPE_SELLABLE_SELL ) {
 			this.translationValues = {
 				object: this.notification.to_model.title,
 				amount: currencyFilter( this.notification.action_model.amount / 100, '$' ),
