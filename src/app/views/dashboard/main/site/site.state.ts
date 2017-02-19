7c808774cd9625c981b7614ca6b42d97c8d369a7
David DeCarmine <hworld@holo-world.com>
2017-02-18 17:38:47 -0700

    Add user site management page

---
+++ src/app/views/dashboard/main/site/site.state.ts
@@ -0,0 +1,15 @@
+import { makeState } from '../../../../../lib/gj-lib-client/utils/angular-facade';
+import { Api } from '../../../../../lib/gj-lib-client/components/api/api.service';
+
+makeState( 'dashboard.main.site', {
+	url: '/site',
+	lazyLoad: () => $import( './site.module' ),
+	resolve: {
+
+		/*@ngInject*/
+		payload: function()
+		{
+			return Api.sendRequest( '/web/dash/sites' );
+		},
+	}
+} );
