4adefe80d8a07ba35ce2968d07a9296abe98aca6
David DeCarmine <hworld@holo-world.com>
2017-03-15 16:41:30 -0600

    Rewrite search autocomplete to vue

--- src/app/components/search/autocomplete/autocomplete.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from 'ng-metadata/core';
-import { AutocompleteComponent } from './autocomplete-directive';
-
-@NgModule({
-	declarations: [
-		AutocompleteComponent,
-	],
-})
-export class SearchAutocompleteModule { }
---
+++ src/app/components/search/autocomplete/autocomplete.ts
@@ -0,0 +1,354 @@
+import * as Vue from 'vue';
+import { Component, Watch } from 'vue-property-decorator';
+import * as View from '!view!./autocomplete.html?style=./autocomplete.styl';
+
+import { App } from '../../../app-service';
+import { Search } from '../search-service';
+import { SearchHistory } from '../history/history-service';
+import { Popover } from '../../../../lib/gj-lib-client/components/popover/popover.service';
+import { User } from '../../../../lib/gj-lib-client/components/user/user.model';
+import { getProvider } from '../../../../lib/gj-lib-client/utils/utils';
+import { Game } from '../../../../lib/gj-lib-client/components/game/game.model';
+import { Analytics } from '../../../../lib/gj-lib-client/components/analytics/analytics.service';
+import { findVueParent, makeObservableService } from '../../../../lib/gj-lib-client/utils/vue';
+import { AppSearch } from '../search';
+import { stringSort } from '../../../../lib/gj-lib-client/utils/array';
+import { fuzzysearch } from '../../../../lib/gj-lib-client/utils/string';
+import { StateService } from 'angular-ui-router';
+import { AppPopover } from '../../../../lib/gj-lib-client/components/popover/popover';
+import { AppJolticon } from '../../../../lib/gj-lib-client/vue/components/jolticon/jolticon';
+import { AppTrackEvent } from '../../../../lib/gj-lib-client/components/analytics/track-event.directive.vue';
+import { AppRouterLink } from '../../router-link/router-link';
+import { AppGameThumbnailImg } from '../../../../lib/gj-lib-client/components/game/thumbnail-img/thumbnail-img';
+import { AppGameCompatIcons } from '../../game/compat-icons/compat-icons';
+
+const KEYCODE_UP = 38;
+const KEYCODE_DOWN = 40;
+const KEYCODE_ENTER = 13;
+const KEYCODE_ESC = 27;
+
+interface Command
+{
+	keyword: string;
+	state: string;
+	description: string;
+	authRequired?: boolean;
+	clientRequired?: boolean;
+	options?: Object;
+}
+
+@View
+@Component({
+	name: 'search-autocomplete',
+	components: {
+		AppPopover,
+		AppJolticon,
+		AppRouterLink,
+		AppGameThumbnailImg,
+		AppGameCompatIcons,
+	},
+	directives: {
+		AppTrackEvent,
+	}
+})
+export class AppSearchAutocomplete extends Vue
+{
+	mode: 'search' | 'command' = 'search';
+
+	selected = 0;
+	games: any[] = [];
+	devlogs: any[] = [];
+	users: User[] = [];
+	libraryGames: any[] = [];
+	items: any[] = [];
+
+	modes = [ 'search', 'command' ];
+
+	app?: App = undefined;
+	search: AppSearch = ({} as AppSearch);
+
+	// This gets debounced.
+	// We need to generate it debounced in the constructor.
+	sendSearch: () => void;
+
+	get isHidden()
+	{
+		return this.search.isEmpty();
+	}
+
+	created()
+	{
+		this.search = findVueParent( this, AppSearch ) as AppSearch;
+	}
+
+	mounted()
+	{
+		this.app = makeObservableService( getProvider<App>( 'App' ) );
+
+		// TODO: Remove lodash.
+		// Generate the debounced method.
+		this.sendSearch = _.debounce( () => this._sendSearch(), 200, { leading: false, maxWait: 750, trailing: true } );
+
+		this.search.setKeydownSpy( ( event: KeyboardEvent ) =>
+		{
+			let min = 0;
+			let max = 0;
+
+			if ( this.mode === 'search' ) {
+				max = this.items.length;
+			}
+			else if ( this.mode === 'command' ) {
+				max = this.filteredCommands.length - 1;
+			}
+
+			if ( event.keyCode === KEYCODE_DOWN ) {
+				this.selected = Math.min( this.selected + 1, max );
+			}
+			else if ( event.keyCode === KEYCODE_UP ) {
+				this.selected = Math.max( this.selected - 1, min );
+			}
+			else if ( event.keyCode === KEYCODE_ENTER ) {
+				this.selectActive();
+			}
+			else if ( event.keyCode === KEYCODE_ESC ) {
+
+				// If they had a command in there but escaped, then remove the whole command.
+				if ( this.mode === 'command' ) {
+					this.search.query = '';
+				}
+			}
+		} );
+	}
+
+	get commands()
+	{
+		const commands: Command[] = [
+			{
+				keyword: ':discover',
+				state: 'discover.home',
+				description: this.$gettext( 'commands.discover_description' ),
+			},
+			{
+				keyword: ':games',
+				state: 'discover.games.list._fetch',
+				options: { section: 'featured' },
+				description: this.$gettext( 'commands.games_description' ),
+			},
+			{
+				keyword: ':devlogs',
+				state: 'discover.devlogs.overview',
+				description: this.$gettext( 'Browse devlogs.' ),
+			},
+			{
+				keyword: ':dashboard',
+				state: 'dashboard.main.overview',
+				authRequired: true,
+				description: this.$gettext( 'commands.dashboard_description' ),
+			},
+			{
+				keyword: ':library',
+				state: 'library.overview',
+				authRequired: true,
+				description: this.$gettext( 'commands.library_description' ),
+			},
+			{
+				keyword: ':installed',
+				state: 'library.installed',
+				clientRequired: true,
+				description: this.$gettext( 'commands.installed_description' ),
+			},
+			{
+				keyword: ':account',
+				state: 'dashboard.account.edit',
+				authRequired: true,
+				description: this.$gettext( 'commands.account_description' ),
+			},
+			{
+				keyword: ':activity',
+				state: 'dashboard.activity.feed',
+				options: { tab: 'activity' },
+				authRequired: true,
+				description: this.$gettext( 'commands.activity_description' ),
+			},
+			{
+				keyword: ':notifications',
+				state: 'dashboard.activity.feed',
+				options: { tab: 'notifications' },
+				authRequired: true,
+				description: this.$gettext( 'View your notifications.' ),
+			},
+			{
+				keyword: ':settings',
+				state: 'settings',
+				authRequired: true,
+				description: this.$gettext( 'commands.settings_description' ),
+			},
+		];
+
+		return commands.sort( ( a, b ) => stringSort( a.keyword, b.keyword ) );
+	}
+
+	get filteredCommands()
+	{
+		const commands = this.commands;
+		const isLoggedIn = this.app && this.app.user;
+		const search = this.search;
+
+		if ( this.mode !== 'command' ) {
+			return commands;
+		}
+
+		const filteredCommands = [];
+		for ( const command of commands ) {
+			if ( !isLoggedIn && command.authRequired ) {
+				continue;
+			}
+
+			if ( !GJ_IS_CLIENT && command.clientRequired ) {
+				continue;
+			}
+
+			if ( search.query.length === 1
+				|| fuzzysearch( search.query.toLowerCase(), command.keyword )
+			) {
+				filteredCommands.push( command );
+			}
+		}
+
+		return filteredCommands;
+	}
+
+	getPopover()
+	{
+		return Popover.getPopover( 'search-autocomplete' );
+	}
+
+	inAvailableMode()
+	{
+		return this.modes.indexOf( this.mode ) !== -1;
+	}
+
+	private async _sendSearch()
+	{
+		if ( this.search.isEmpty() || !this.inAvailableMode() ) {
+			return;
+		}
+
+		// We store the query that we're waiting on.
+		const thisQuery = this.search.query;
+		const payload = await Search.search( thisQuery, { type: 'typeahead' } );
+
+		// We only update the payload if the query is still the same as when we sent.
+		// This makes sure we don't step on ourselves while typing fast.
+		// Payloads may not come back sequentially.
+		if ( this.search.query === thisQuery ) {
+			this.games = payload.games;
+			this.devlogs = payload.devlogs;
+			this.users = payload.users;
+			this.libraryGames = payload.libraryGames;
+
+			// All items so we can calculate global selection indexes easily.
+			// This needs to be in the order that they will show in the results list.
+			this.items = ([] as any[])
+				.concat( this.libraryGames )
+				.concat( this.games )
+				.concat( this.devlogs )
+				.concat( this.users )
+				;
+		}
+	}
+
+	selectActive()
+	{
+		if ( this.search.isEmpty() ) {
+			return;
+		}
+
+		if ( this.mode === 'search' ) {
+			SearchHistory.record( this.search.query );
+
+			// Selected the "show all results" option.
+			if ( this.selected === 0 ) {
+				this.viewAll();
+			}
+			else {
+				const item = this.items[ (this.selected - 1) ];
+				if ( item instanceof Game ) {
+					this.selectGame( item );
+				}
+				else if ( item instanceof User ) {
+					this.selectUser( item );
+				}
+				else if ( GJ_IS_CLIENT ) {
+					const LocalDbGame = getProvider<any>( 'LocalDb_Game' );
+					if ( item instanceof LocalDbGame ) {
+						this.selectLibraryGame( item );
+					}
+				}
+			}
+		}
+		else if ( this.mode === 'command' ) {
+			const command = this.filteredCommands[ this.selected ];
+			this.selectCommand( command );
+		}
+
+		this.search.blur();
+	}
+
+	viewAll()
+	{
+		const $state = getProvider<StateService>( '$state' );
+		$state.go( 'search.results', { q: this.search.query } );
+		Analytics.trackEvent( 'search', 'autocomplete', 'go-all' );
+	}
+
+	selectGame( game: any )
+	{
+		const $state = getProvider<StateService>( '$state' );
+		$state.go( 'discover.games.view.overview', { slug: game.slug, id: game.id } );
+		Analytics.trackEvent( 'search', 'autocomplete', 'go-game' );
+	}
+
+	selectUser( user: any )
+	{
+		const $state = getProvider<StateService>( '$state' );
+		$state.go( 'profile.overview', { username: user.username } );
+		Analytics.trackEvent( 'search', 'autocomplete', 'go-user' );
+	}
+
+	selectLibraryGame( localGame: any )
+	{
+		const $state = getProvider<StateService>( '$state' );
+		$state.go( 'discover.games.view.overview', { slug: localGame.slug, id: localGame.id } );
+		Analytics.trackEvent( 'search', 'autocomplete', 'go-library-game' );
+	}
+
+	selectCommand( command: Command )
+	{
+		if ( command && command.state ) {
+			this.search.query = '';  // Set it as blank.
+			const $state = getProvider<StateService>( '$state' );
+			$state.go( command.state, command.options || {} );
+			Analytics.trackEvent( 'search', 'autocomplete', 'go-command' );
+		}
+	}
+
+	@Watch( 'search.query' )
+	onChange()
+	{
+		// Reset the selected index.
+		this.selected = 0;
+
+		if ( this.search.isEmpty() || !this.app ) {
+			return;
+		}
+
+		if ( this.search.query[0] === ':' ) {
+			this.mode = 'command';
+		}
+		else {
+			this.mode = 'search';
+			this.sendSearch();
+		}
+	}
+}
