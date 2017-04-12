435f87ad71f49a910e6b89917c420672778f9fbf
YLivay <yarivlivay@gmail.com>
2017-04-12 23:46:29 +0300

    Use client voodoo with typings and no native modules, added client autostart service

---
+++ src/app/components/client/autostart/autostart.service.ts
@@ -0,0 +1,45 @@
+import { Settings } from '../../settings/settings.service';
+import { Autostarter } from 'client-voodoo';
+import { Device } from '../../../../lib/gj-lib-client/components/device/device.service';
+
+export class ClientAutoStart
+{
+	static init()
+	{
+		if ( Settings.get( 'autostart-client' ) ) {
+			this.set();
+		}
+	}
+
+	static canAutoStart()
+	{
+		return GJ_BUILD_TYPE == 'production' && Device.os() == 'windows';
+	}
+
+	static async set()
+	{
+		if ( !this.canAutoStart() ) {
+			return;
+		}
+
+		return Autostarter.set( process.execPath, [ '--silent-start' ] );
+	}
+
+	static async clear()
+	{
+		if ( !this.canAutoStart() ) {
+			return;
+		}
+
+		return Autostarter.unset();
+	}
+
+	static async check()
+	{
+		if ( !this.canAutoStart() ) {
+			return;
+		}
+
+		return Autostarter.isset();
+	}
+}
