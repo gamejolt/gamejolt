4343c9294ec338096cbd8764243e8952acbdee72
David DeCarmine <hworld@holo-world.com>
2017-06-18 10:40:56 -0600

    get new styling changes working better on mobile

--- src/app/views/dashboard/games/manage/api/api.ts
+++ src/app/views/dashboard/games/manage/api/api.ts
@@ -5,11 +5,13 @@ import * as View from '!view!./api.html';
 import { makeObservableService } from '../../../../../../lib/gj-lib-client/utils/vue';
 import { Screen } from '../../../../../../lib/gj-lib-client/components/screen/screen-service';
 import { AppManageGameApiNav } from './_nav/nav';
+import { AppNavTabList } from '../../../../../../lib/gj-lib-client/components/nav/tab-list/tab-list';
 
 @View
 @Component({
 	components: {
 		AppManageGameApiNav,
+		AppNavTabList,
 	},
 })
 export default class RouteDashGamesManageApi extends Vue {
