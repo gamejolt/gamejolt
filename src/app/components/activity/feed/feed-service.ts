f76af7a3f8789d38609a55f232a0c950330c436b
David DeCarmine <hworld@holo-world.com>
2017-07-22 17:03:59 -0600

    fix checking history state for ssr

--- src/app/components/activity/feed/feed-service.ts
+++ src/app/components/activity/feed/feed-service.ts
@@ -17,7 +17,8 @@ export class ActivityFeedService {
 
 	static bootstrap(items?: ActivityFeedInput[], options?: ActivityFeedContainerOptions) {
 		// vue-router maintains a history key for each route in the history.
-		const key: string | undefined = history.state && history.state.key;
+		const key: string | undefined =
+			typeof history !== 'undefined' ? history.state && history.state.key : undefined;
 
 		// If we have a state container for this historical key...
 		let state = this._states.find(item => item.key === key);
