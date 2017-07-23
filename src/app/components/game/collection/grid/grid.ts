130f85705ef3061ad60937bb02c5495e810b8879
David DeCarmine <hworld@holo-world.com>
2017-07-22 19:04:00 -0600

    game collection grid now refactored into items to make more performant. also fixed a bug with guests in that context

--- src/app/components/game/collection/grid/grid.ts
+++ src/app/components/game/collection/grid/grid.ts
@@ -1,25 +1,17 @@
 import Vue from 'vue';
-import { State } from 'vuex-class';
 import { Component, Prop } from 'vue-property-decorator';
-import * as View from '!view!./grid.html?style=./grid.styl';
+import * as View from '!view!./grid.html';
 
 import { GameCollection } from '../collection.model';
-import { AppGameCollectionThumbnail } from '../thumbnail/thumbnail';
-import { AppTrackEvent } from '../../../../../lib/gj-lib-client/components/analytics/track-event.directive.vue';
-import { Store } from '../../../../store/index';
+import { AppGameCollectionGridItem } from './item/item';
 
 @View
 @Component({
 	components: {
-		AppGameCollectionThumbnail,
-	},
-	directives: {
-		AppTrackEvent,
+		AppGameCollectionGridItem,
 	},
 })
 export class AppGameCollectionGrid extends Vue {
 	@Prop(Array) collections: GameCollection[];
 	@Prop(String) eventLabel?: string;
-
-	@State app: Store['app'];
 }
---
+++ src/app/components/game/collection/grid/item/item.ts
@@ -0,0 +1,29 @@
+import { State } from 'vuex-class';
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+import * as View from '!view!./item.html?style=./item.styl';
+
+import { GameCollection } from '../../collection.model';
+import { Store } from '../../../../../store/index';
+import { AppGameCollectionThumbnail } from '../../thumbnail/thumbnail';
+import { AppTrackEvent } from '../../../../../../lib/gj-lib-client/components/analytics/track-event.directive.vue';
+
+@View
+@Component({
+	components: {
+		AppGameCollectionThumbnail,
+	},
+	directives: {
+		AppTrackEvent,
+	},
+})
+export class AppGameCollectionGridItem extends Vue {
+	@Prop(GameCollection) collection: GameCollection;
+	@Prop(String) eventLabel?: string;
+
+	@State app: Store['app'];
+
+	get notOwner() {
+		return this.collection.from_subscription || !this.collection.isOwner;
+	}
+}
