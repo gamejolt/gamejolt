ae0213b7cae3825db51a3a0f9a94f6ea6fb52a4c
David DeCarmine <hworld@holo-world.com>
2017-07-25 21:29:00 -0600

    update gulpfile back to prod

--- gulpfile.js
+++ gulpfile.js
@@ -1,125 +1,9 @@
 var gulp = require('gulp');
 
 var config = {
-	staticCdn: 'https://gamejolt.com',
+	staticCdn: 'https://s.gjcdn.net',
 	injectVersion: 2,
 	framework: 'vue',
-	extraBower: {
-		'angular-bootstrap': [
-			'src/transition/transition.js',
-			'src/position/position.js',
-			'src/bindHtml/bindHtml.js',
-			'src/tooltip/tooltip.js',
-			'src/collapse/collapse.js',
-			'src/modal/modal.js',
-			'src/dateparser/dateparser.js',
-			'src/datepicker/datepicker.js',
-			'src/timepicker/timepicker.js',
-		],
-	},
-	rollup: {
-		vendor: {
-			'ng-metadata/core': 'vendor.ngMetadata_core',
-			'ng-metadata/common': 'vendor.ngMetadata_common',
-			'ng-metadata/platform': 'vendor.ngMetadata_platform',
-		},
-	},
-	modules: {
-		'vendor-app.js': {
-			bower: [
-				'moment',
-				'humanize-duration',
-				'angular-moment',
-				'angular-hotkeys',
-				'ocLazyLoad',
-				'angular-ui-utils',
-				'favico.js',
-				'angular-inview',
-				'angular-elastic',
-				'ua-parser-js',
-			],
-		},
-		'vendor-checkout.js': {
-			bower: ['angular-credit-cards', 'angular-ui-mask'],
-		},
-		'jquery.js': {
-			bower: ['jquery'],
-		},
-		'hammer.js': {
-			bower: ['hammerjs', 'angular-hammer'],
-		},
-		'jcrop.js': {
-			bower: ['jcrop'],
-		},
-		'graphs.js': {
-			bower: ['Chart.js', 'tc-angular-chartjs'],
-		},
-		'upload.js': {
-			bower: ['ng-file-upload'],
-		},
-		'ui-tree.js': {
-			bower: ['angular-ui-tree'],
-		},
-		'chat.js': {
-			components: ['chat'],
-		},
-		'primus.js': {
-			componentVendor: ['primus'],
-		},
-		'error-tracking.js': {
-			componentVendor: ['error-tracking'],
-		},
-		'theme-editor.js': {
-			bower: ['spectrum'],
-		},
-
-		// Sections of the site.
-		'dash.js': {
-			main: '/views/dashboard/dashboard.ts',
-			components: ['forms/dashboard', 'site-analytics', 'site-editor-modal'],
-			libComponents: [
-				'model',
-				'timezone',
-				'colorpicker',
-				'theme',
-				'content-block/editor',
-				'site',
-				'site/theme',
-				'site/template',
-				'site/content-block',
-				'site/build',
-			],
-			views: ['dashboard'],
-			bower: ['moment-timezone', 'ngTimezone'],
-		},
-		'forums.js': {
-			components: ['forms/forum', 'forum'],
-			views: ['forums'],
-		},
-		'channels.js': {
-			main: '/views/discover/channels/channels.ts',
-			views: ['discover/channels'],
-		},
-		'radio.js': {
-			main: '/views/radio/radio.ts',
-			views: ['radio'],
-		},
-
-		// For client.
-		'client.js': {
-			bower: ['dexie'],
-			components: ['client'],
-		},
-
-		'client-updater.js': {
-			components: ['client-updater'],
-		},
-
-		// For client sections.
-		'client-base.js': {
-			components: ['client-base'],
-		},
-	},
 	sections: ['auth', 'checkout', 'claim'],
 	translations: 'site-translations',
 	translationSections: {
@@ -140,7 +24,4 @@ require('./tasks/app.js')(config);
 require('./tasks/terms.js')(config);
 require('./tasks/game-api-doc.js')(config);
 
-gulp.task(
-	'pre',
-	gulp.parallel('terms', 'game-api-doc:nav', 'game-api-doc:compile')
-);
+gulp.task('pre', gulp.parallel('terms', 'game-api-doc:nav', 'game-api-doc:compile'));
