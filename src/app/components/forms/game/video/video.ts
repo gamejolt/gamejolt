34277dbe6dc6685cf6b8cbe0ca9454e5dc788b94
David DeCarmine <hworld@holo-world.com>
2017-07-31 23:33:25 -0600

    fix the video form -- wasn't fully ported from angular
    fixes: https://github.com/gamejolt/issue-tracker/issues/936

--- src/app/components/forms/game/video/video.ts
+++ src/app/components/forms/game/video/video.ts
@@ -1,4 +1,4 @@
-import { Component, Prop } from 'vue-property-decorator';
+import { Component, Prop, Watch } from 'vue-property-decorator';
 import * as View from '!view!./video.html';
 
 import { GameVideo } from '../../../../../lib/gj-lib-client/components/game/video/video.model';
@@ -8,14 +8,18 @@ import {
 } from '../../../../../lib/gj-lib-client/components/form-vue/form.service';
 import { Game } from '../../../../../lib/gj-lib-client/components/game/game.model';
 
+type FormModel = GameVideo & {
+	_url: string;
+};
+
 @View
 @Component({
 	components: {},
 })
-export class FormGameVideo extends BaseForm<GameVideo> implements FormOnInit {
+export class FormGameVideo extends BaseForm<FormModel> implements FormOnInit {
 	@Prop(Game) game: Game;
 
-	modelClass = GameVideo;
+	modelClass = GameVideo as any;
 	resetOnSubmit = true;
 	warnOnDiscard = false;
 
@@ -23,54 +27,32 @@ export class FormGameVideo extends BaseForm<GameVideo> implements FormOnInit {
 
 	onInit() {
 		this.setField('game_id', this.game.id);
+
+		// We use _url as the form model's URL and copy back and forth.
+		if (this.formModel.url) {
+			if (this.formModel.type === GameVideo.TYPE_VIMEO) {
+				this.setField('_url', 'https://www.vimeo.com/' + this.formModel.url);
+			} else if (this.formModel.type === GameVideo.TYPE_YOUTUBE) {
+				this.setField('_url', 'https://www.youtube.com/watch?v=' + this.formModel.url);
+			}
+		}
+	}
+
+	@Watch('formModel._url')
+	onUrlChange(url: string) {
+		// Check if we need to scrub out anything from the URL.
+		// Will be the case if they entered in a full URL such as http://www.youtube.com/watch?v=something, etc.
+		if (url) {
+			const youtubeMatch = url.match(GameVideo.REGEX.YOUTUBE);
+			const vimeoMatch = url.match(GameVideo.REGEX.VIMEO);
+
+			if (youtubeMatch) {
+				this.setField('type', GameVideo.TYPE_YOUTUBE);
+				this.setField('url', youtubeMatch[4]);
+			} else if (vimeoMatch) {
+				this.setField('type', GameVideo.TYPE_VIMEO);
+				this.setField('url', vimeoMatch[4]);
+			}
+		}
 	}
 }
-
-// angular.module( 'App.Forms.Dashboard' ).directive( 'gjFormDashboardGameVideo', function( Form, Game_Video )
-// {
-// 	var form = new Form( {
-// 		model: 'Game_Video',
-// 		template: require( './video.html' ),
-// 		resetOnSubmit: true,
-// 	} );
-
-// 	form.scope.game = '=gjGame';
-
-// 	form.onInit = function( scope )
-// 	{
-// 		// Set the game ID on the video form model from the game passed in.
-// 		scope.formModel.game_id = scope.game.id;
-
-// 		// We use _url as the form model's URL and copy back and forth.
-// 		if ( scope.formModel.url ) {
-// 			if ( scope.formModel.type === Game_Video.TYPE_VIMEO ) {
-// 				scope.formModel._url = 'https://www.vimeo.com/' + scope.formModel.url;
-// 			}
-// 			else if ( scope.formModel.type === Game_Video.TYPE_YOUTUBE ) {
-// 				scope.formModel._url = 'https://www.youtube.com/watch?v=' + scope.formModel.url;
-// 			}
-// 		}
-
-// 		scope.$watch( 'formModel._url', function( url )
-// 		{
-// 			// Check if we need to scrub out anything from the URL.
-// 			// Will be the case if they entered in a full URL such as http://www.youtube.com/watch?v=something, etc.
-// 			if ( url ) {
-
-// 				var youtubeMatch = url.match( Game_Video.REGEX.YOUTUBE );
-// 				var vimeoMatch = url.match( Game_Video.REGEX.VIMEO );
-
-// 				if ( youtubeMatch ) {
-// 					scope.formModel.type = Game_Video.TYPE_YOUTUBE;
-// 					scope.formModel.url = youtubeMatch[4];
-// 				}
-// 				else if ( vimeoMatch ) {
-// 					scope.formModel.type = Game_Video.TYPE_VIMEO;
-// 					scope.formModel.url = vimeoMatch[4];
-// 				}
-// 			}
-// 		} );
-// 	};
-
-// 	return form;
-// } );
