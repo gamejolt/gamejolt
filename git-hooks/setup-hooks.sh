bdb3644477767c7e91c60d8e88e01ecb2ebc34c2
David DeCarmine <hworld@holo-world.com>
2017-09-13 16:04:23 -0600

    try to setup git hooks to succeed even in failure

--- git-hooks/setup-hooks.sh
+++ git-hooks/setup-hooks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp git-hooks/pre-commit .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
+cp git-hooks/pre-commit .git/hooks/pre-commit || exit 0
+chmod +x .git/hooks/pre-commit || exit 0
