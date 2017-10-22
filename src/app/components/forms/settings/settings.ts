284bb6f49d270fb04a967f1557acb84348cdc244
David DeCarmine <hworld@holo-world.com>
2017-10-21 19:27:16 -0600

    get rid of friend online/offline notifications

--- src/app/components/forms/settings/settings.ts
+++ src/app/components/forms/settings/settings.ts
@@ -29,7 +29,6 @@ export class FormSettings extends BaseForm<any> implements FormOnInit {
 	ClientAutoStart = ClientAutoStart;
 
 	onInit() {
-		this.setField('chat_notify_friends_online', Settings.get('chat-notify-friends-online'));
 		this.setField('restricted_browsing', Settings.get('restricted-browsing'));
 		this.setField('broadcast_modal', Settings.get('broadcast-modal'));
 		this.setField('animated_thumbnails', Settings.get('animated-thumbnails'));
@@ -82,7 +81,6 @@ export class FormSettings extends BaseForm<any> implements FormOnInit {
 	}
 
 	onChange() {
-		Settings.set('chat-notify-friends-online', this.formModel.chat_notify_friends_online);
 		Settings.set('restricted-browsing', this.formModel.restricted_browsing);
 		Settings.set('broadcast-modal', this.formModel.broadcast_modal);
 		Settings.set('animated-thumbnails', this.formModel.animated_thumbnails);
