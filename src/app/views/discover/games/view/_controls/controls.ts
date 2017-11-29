bb0bcee810b4234d11c6dcab1379b856bb1e2e36
nilllzz <nidresch@googlemail.com>
2017-11-29 19:35:17 +0100

    revert disallowing users to follow their own games.

--- src/app/views/discover/games/view/_controls/controls.ts
+++ src/app/views/discover/games/view/_controls/controls.ts
@@ -1,12 +1,10 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import View from '!view!./controls.html';
-import { State } from 'vuex-class';
 
 import { AppGameFollowWidget } from '../../../../../components/game/follow-widget/follow-widget';
 import { AppGamePlaylistAddToWidget } from '../../../../../components/game-playlist/add-to-widget/add-to-widget';
 import { RouteState, RouteStore } from '../view.store';
-import { Store } from '../../../../../store/index';
 
 @View
 @Component({
@@ -16,7 +14,5 @@ import { Store } from '../../../../../store/index';
 	},
 })
 export class AppDiscoverGamesViewControls extends Vue {
-	@State app: Store['app'];
-
 	@RouteState game: RouteStore['game'];
 }
