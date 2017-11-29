07ff2e89c0c2fa573c1d5270bd68c4aeb559bac4
David DeCarmine <hworld@holo-world.com>
2017-11-29 16:11:34 -0700

    bunch of stuff for building client

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const shell = require('gulp-shell');
+const path = require('path');
 
-var config = {
+const config = {
 	staticCdn: 'https://b6d3e9q9.ssl.hwcdn.net',
 	injectVersion: 2,
 	framework: 'vue',
@@ -45,10 +47,44 @@ var config = {
 	},
 };
 
-require('game-jolt-frontend-lib/gulp/tasks/common')(config, __dirname);
-require('./tasks/client.js')(config);
-require('./tasks/app.js')(config);
-require('./tasks/terms.js')(config);
-require('./tasks/game-api-doc.js')(config);
+require('./src/lib/gj-lib-client/gulp/tasks/common')(config, __dirname);
 
-gulp.task('pre', gulp.parallel('terms', 'game-api-doc:nav', 'game-api-doc:compile'));
+// For releasing to S3.
+// We have to gather all the builds into the versioned folder before pushing.
+const packageJson = require('./package.json');
+const releaseDir = path.join('build/prod-client', 'v' + packageJson.version);
+const s3Dir = 's3://gjolt-data/data/client/releases/v' + packageJson.version;
+
+gulp.task(
+	'client:push-releases',
+	shell.task([
+		'aws s3 cp ' +
+			releaseDir +
+			'/linux64.tar.gz ' +
+			s3Dir +
+			'/game-jolt-client.tar.gz --acl public-read',
+		'aws s3 cp ' +
+			releaseDir +
+			'/linux64-package.tar.gz ' +
+			s3Dir +
+			'/linux64-package.tar.gz --acl public-read',
+
+		'aws s3 cp ' + releaseDir + '/osx.dmg ' + s3Dir + '/GameJoltClient.dmg --acl public-read',
+		'aws s3 cp ' +
+			releaseDir +
+			'/osx64-package.tar.gz ' +
+			s3Dir +
+			'/osx64-package.tar.gz --acl public-read',
+
+		'aws s3 cp ' +
+			releaseDir +
+			'/Setup.exe ' +
+			s3Dir +
+			'/GameJoltClientSetup.exe --acl public-read',
+		'aws s3 cp ' +
+			releaseDir +
+			'/win32-package.tar.gz ' +
+			s3Dir +
+			'/win32-package.tar.gz --acl public-read',
+	])
+);
