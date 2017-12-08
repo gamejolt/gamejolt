ae755741efd40a7e10619baae06fd399f4e2b702
David DeCarmine <hworld@holo-world.com>
2017-12-08 11:02:51 -0700

    Fix cookie not fetching properly for chat in client. Fixes: https://github.com/gamejolt/issue-tracker/issues/1054

--- src/app/components/chat/client.ts
+++ src/app/components/chat/client.ts
@@ -29,38 +29,40 @@ export interface ChatNewMessageEvent {
 	message: ChatMessage;
 }
 
-async function getCookie(name: string) {
-	// Within Client we have to access it connectedthis way.
-	if (GJ_IS_CLIENT) {
-		const gui = require('nw.gui') as typeof nwGui;
-		const win = gui.Window.get();
-		(win as any).cookies.get(
-			{
-				url: 'game-jolt-client',
-				name: name,
-			},
-			(cookieData: any) => {
-				if (!cookieData) {
-					return undefined;
+function getCookie(name: string): Promise<string | undefined> {
+	return new Promise(resolve => {
+		// Within Client we have to access it connectedthis way.
+		if (GJ_IS_CLIENT) {
+			const gui = require('nw.gui') as typeof nwGui;
+			const win = gui.Window.get();
+			(win as any).cookies.get(
+				{
+					url: 'game-jolt-client',
+					name: name,
+				},
+				(cookieData: any) => {
+					if (!cookieData) {
+						return resolve(undefined);
+					}
+					return resolve(cookieData.value);
 				}
-				return cookieData.value;
+			);
+		} else {
+			let i,
+				x,
+				y,
+				ARRcookies = document.cookie.split(';');
+			for (i = 0; i < ARRcookies.length; i++) {
+				x = ARRcookies[i].substr(0, ARRcookies[i].indexOf('='));
+				y = ARRcookies[i].substr(ARRcookies[i].indexOf('=') + 1);
+				x = x.replace(/^\s+|\s+$/g, '');
+				if (x === name) {
+					return resolve(unescape(y));
+				}
 			}
-		);
-	} else {
-		let i,
-			x,
-			y,
-			ARRcookies = document.cookie.split(';');
-		for (i = 0; i < ARRcookies.length; i++) {
-			x = ARRcookies[i].substr(0, ARRcookies[i].indexOf('='));
-			y = ARRcookies[i].substr(ARRcookies[i].indexOf('=') + 1);
-			x = x.replace(/^\s+|\s+$/g, '');
-			if (x === name) {
-				return unescape(y);
-			}
+			return resolve(undefined);
 		}
-		return undefined;
-	}
+	});
 }
 
 export class ChatClient {
