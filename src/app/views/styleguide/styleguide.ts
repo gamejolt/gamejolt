d5fce9e1b78a40bde74a14839c92e751e5fde014
David DeCarmine <hworld@holo-world.com>
2018-01-02 13:41:19 -0700

    new app-router-link

--- src/app/views/styleguide/styleguide.ts
+++ src/app/views/styleguide/styleguide.ts
@@ -8,6 +8,7 @@ import { AppScrollTo } from '../../../lib/gj-lib-client/components/scroll/to/to.
 import { AppListGroupStyleguide } from '../../../lib/gj-lib-client/components/list-group/list-group-styleguide';
 import { AppJolticonsStyleguide } from '../../../lib/gj-lib-client/components/jolticons/jolticons-styleguide';
 import { AppProgressBarStyleguide } from '../../../lib/gj-lib-client/components/progress/bar/bar-styleguide';
+import { AppLinkStyleguide } from '../../../lib/gj-lib-client/components/router/link-styleguide';
 import {
 	BaseRouteComponent,
 	RouteResolve,
@@ -18,6 +19,7 @@ import {
 	name: 'RouteStyleguide',
 	components: {
 		AppScrollAffix,
+		AppLinkStyleguide,
 		AppButtonStyleguide,
 		AppListGroupStyleguide,
 		AppProgressBarStyleguide,
