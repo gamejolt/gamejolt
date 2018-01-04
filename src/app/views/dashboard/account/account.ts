c5c834c56ff078b0fe1cd11150236a188808a8ed
nilllzz <nidresch@googlemail.com>
2018-01-04 20:53:36 +0100

    User headers (#170)

--- src/app/views/dashboard/account/account.ts
+++ src/app/views/dashboard/account/account.ts
@@ -7,12 +7,15 @@ import { AppJolticon } from '../../../../lib/gj-lib-client/vue/components/joltic
 import { AppPageHeader } from '../../../components/page-header/page-header';
 import { AppUserAvatar } from '../../../../lib/gj-lib-client/components/user/user-avatar/user-avatar';
 import { Store } from '../../../store/index';
-import { User } from '../../../../lib/gj-lib-client/components/user/user.model';
 import { RouteStoreName, RouteState, RouteStore } from './account.store';
+import { Api } from '../../../../lib/gj-lib-client/components/api/api.service';
 import {
 	BaseRouteComponent,
 	RouteResolve,
 } from '../../../../lib/gj-lib-client/components/route/route-component';
+import { AppMutation, AppStore } from '../../../../lib/gj-lib-client/vue/services/app/app-store';
+import { AppExpand } from '../../../../lib/gj-lib-client/components/expand/expand';
+import { AppMediaItemCover } from '../../../../_common/media-item/cover/cover';
 
 @View
 @Component({
@@ -21,19 +24,29 @@ import {
 		AppJolticon,
 		AppPageHeader,
 		AppUserAvatar,
+		AppExpand,
+		AppMediaItemCover,
 	},
 })
 export default class RouteDashAccount extends BaseRouteComponent {
 	@State app: Store['app'];
 	@RouteState heading: RouteStore['heading'];
 
+	@AppMutation setUser: AppStore['setUser'];
+
 	readonly Screen = Screen;
 
 	storeName = RouteStoreName;
 	storeModule = RouteStore;
 
-	@RouteResolve({})
-	async routeResolve() {
-		User.touch();
+	// We want to reload this data every time we come into this section.
+	@RouteResolve({ lazy: false, cache: false })
+	routeResolve() {
+		return Api.sendRequest('/web/dash/account', {});
+	}
+
+	routed(payload: any) {
+		// This will set our user with more fields required for managing it.
+		this.setUser(payload.user);
 	}
 }
--- src/app/views/dashboard/account/header/header.ts
+++ src/app/views/dashboard/account/header/header.ts
@@ -1,33 +1,45 @@
 import { State } from 'vuex-class';
 import { Component } from 'vue-property-decorator';
-import View from '!view!./avatar.html';
+import View from '!view!./header.html';
 
 import { Store } from '../../../../store/index';
 import { RouteStore, RouteMutation } from '../account.store';
 import { AppJolticon } from '../../../../../lib/gj-lib-client/vue/components/jolticon/jolticon';
 import { Screen } from '../../../../../lib/gj-lib-client/components/screen/screen-service';
 import { BaseRouteComponent } from '../../../../../lib/gj-lib-client/components/route/route-component';
-import { FormAvatar } from '../../../../components/forms/avatar/avatar';
+import { FormUserHeader } from '../../../../components/forms/user-header/header';
+import { ModalConfirm } from '../../../../../lib/gj-lib-client/components/modal/confirm/confirm-service';
 
 @View
 @Component({
-	name: 'RouteDashAccountAvatar',
+	name: 'RouteDashAccountHeader',
 	components: {
 		AppJolticon,
-		FormAvatar,
+		FormUserHeader,
 	},
 })
-export default class RouteDashAccountAvatar extends BaseRouteComponent {
+export default class RouteDashAccountHeader extends BaseRouteComponent {
 	@State app: Store['app'];
 	@RouteMutation setHeading: RouteStore['setHeading'];
 
 	readonly Screen = Screen;
 
 	get routeTitle() {
-		return this.$gettext('Your Avatar');
+		return this.$gettext('Your Header');
 	}
 
 	routeInit() {
-		this.setHeading(this.$gettext('Edit Your Avatar'));
+		this.setHeading(this.$gettext('Edit Your Profile Header'));
+	}
+
+	async clearHeader() {
+		const result = await ModalConfirm.show(
+			this.$gettext(`Are you sure you want to remove your profile header?`),
+			undefined,
+			'yes'
+		);
+		if (result) {
+			this.app.user!.$clearHeader();
+		}
 	}
 }
