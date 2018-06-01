eb63959ab28ef6fe0434c8812182efc6d44c4ced
Nils <nidresch@googlemail.com>
2018-06-01 10:57:55 +0200

    unlinking

--- src/app/views/dashboard/games/manage/game/linked-accounts/linked-accounts.ts
+++ src/app/views/dashboard/games/manage/game/linked-accounts/linked-accounts.ts
@@ -14,6 +14,7 @@ import {
 	Provider,
 } from '../../../../../../../lib/gj-lib-client/components/linked-account/linked-account.model';
 import { LinkedAccounts } from '../../../../../../../lib/gj-lib-client/components/linked-account/linked-accounts.service';
+import { Growls } from '../../../../../../../lib/gj-lib-client/components/growls/growls.service';
 
 @View
 @Component({
@@ -71,11 +72,39 @@ export default class RouteDashGamesManageGameLinkedAccounts extends BaseRouteCom
 		console.log('EMIT SYNC', e);
 	}
 
-	onLink(e: Event, provider: Provider) {
-		LinkedAccounts.link(
+	async onLink(_e: Event, provider: Provider) {
+		await LinkedAccounts.link(
 			this.$router,
 			provider,
 			'/web/dash/developer/games/linked-accounts/link/' + this.game.id + '/'
 		);
+	}
+
+	async onUnlink(_e: Event, provider: Provider) {
+		// TODO: client
+		const response = await Api.sendRequest(
+			'/web/dash/developer/games/linked-accounts/unlink/' + this.game.id + '/' + provider
+		);
+
+		const providerName = LinkedAccount.getProviderDisplayName(provider);
+		if (response.success) {
+			this.accounts = LinkedAccount.populate(response.accounts);
+			Growls.success(
+				this.$gettextInterpolate(
+					`Your %{ provider } account has been unlinked from %{ game }.`,
+					{
+						provider: providerName,
+						game: this.game.title,
+					}
+				),
+				this.$gettext('Account Unlinked')
+			);
+		} else {
+			Growls.error(
+				this.$gettextInterpolate(`Could not unlink your %{ provider } account.`, {
+					provider: providerName,
+				})
+			);
+		}
 	}
 }
