2d99b722b26bebb47417737598c787e548443812
Nils <nidresch@googlemail.com>
2018-06-08 13:11:05 +0200

    unlink youtube channels

--- src/app/views/dashboard/account/linked-accounts/linked-accounts.ts
+++ src/app/views/dashboard/account/linked-accounts/linked-accounts.ts
@@ -18,6 +18,7 @@ import { LinkedAccounts } from '../../../../../lib/gj-lib-client/components/link
 import { Growls } from '../../../../../lib/gj-lib-client/components/growls/growls.service';
 import { Translate } from '../../../../../lib/gj-lib-client/components/translate/translate.service';
 import { UserSetPasswordModal } from '../../../../components/user/set-password-modal/set-password-modal.service';
+import { ModalConfirm } from '../../../../../lib/gj-lib-client/components/modal/confirm/confirm-service';
 
 @View
 @Component({
@@ -113,8 +114,39 @@ export default class RouteDashAccountLinkedAccounts extends BaseRouteComponent {
 
 				// Try to unlink again once they've set one!
 				await this.onUnlink(e, provider);
+			} else {
+				Growls.error(this.$gettext('Failed to unlink account from the site.'));
 			}
-			// TODO: proper fail case?
+		}
+	}
+
+	async linkYouTubeChannel() {}
+
+	async unlinkYouTubeChannel(channel: YoutubeChannel) {
+		if (!this.user) {
+			return;
+		}
+
+		const result = await ModalConfirm.show(
+			this.$gettext(
+				'Are you you want to unlink this channel? Any videos you may have done as part of this channel will be removed from Game Jolt.'
+			)
+		);
+
+		if (!result) {
+			return;
+		}
+
+		const payload = await channel.$remove();
+		if (payload.success) {
+			this.channels = YoutubeChannel.populate(payload.channels);
+		} else {
+			Growls.error(
+				this.$gettextInterpolate(
+					'Failed to remove YouTube channel %{ title } form Game Jolt.',
+					{ title: channel.title }
+				)
+			);
 		}
 	}
 }
