1b64dc962cba5e8e4fcf614a7ad1b3f617b4c4c2
David DeCarmine <hworld@holo-world.com>
2018-09-17 11:46:29 -0700

    Split out event item management and user controls

--- src/app/components/devlog/post/view/view.ts
+++ src/app/components/devlog/post/view/view.ts
@@ -1,22 +1,27 @@
+import View from '!view!./view.html?style=./view.styl';
+import { AppAd } from 'game-jolt-frontend-lib/components/ad/ad';
+import { AppAdPlacement } from 'game-jolt-frontend-lib/components/ad/placement/placement';
+import {
+	canUserManagePost,
+	FiresidePost,
+} from 'game-jolt-frontend-lib/components/fireside/post/post-model';
+import { AppImgResponsive } from 'game-jolt-frontend-lib/components/img/responsive/responsive';
+import { AppResponsiveDimensions } from 'game-jolt-frontend-lib/components/responsive-dimensions/responsive-dimensions';
+import { Screen } from 'game-jolt-frontend-lib/components/screen/screen-service';
+import { AppScrollWhen } from 'game-jolt-frontend-lib/components/scroll/scroll-when.directive.vue';
+import { AppSketchfabEmbed } from 'game-jolt-frontend-lib/components/sketchfab/embed/embed';
+import { AppTimeAgo } from 'game-jolt-frontend-lib/components/time/ago/ago';
+import { AppVideoEmbed } from 'game-jolt-frontend-lib/components/video/embed/embed';
+import { AppVideo } from 'game-jolt-frontend-lib/components/video/video';
+import { AppWidgetCompiler } from 'game-jolt-frontend-lib/components/widget-compiler/widget-compiler';
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
-import View from '!view!./view.html?style=./view.styl';
-
-import { FiresidePost } from '../../../../../lib/gj-lib-client/components/fireside/post/post-model';
-import { AppTimeAgo } from '../../../../../lib/gj-lib-client/components/time/ago/ago';
-import { AppWidgetCompiler } from '../../../../../lib/gj-lib-client/components/widget-compiler/widget-compiler';
-import { AppResponsiveDimensions } from '../../../../../lib/gj-lib-client/components/responsive-dimensions/responsive-dimensions';
-import { AppImgResponsive } from '../../../../../lib/gj-lib-client/components/img/responsive/responsive';
-import { AppVideo } from '../../../../../lib/gj-lib-client/components/video/video';
-import { AppVideoEmbed } from '../../../../../lib/gj-lib-client/components/video/embed/embed';
-import { AppSketchfabEmbed } from '../../../../../lib/gj-lib-client/components/sketchfab/embed/embed';
-import { AppActivityFeedControls } from '../../../activity/feed/controls/controls';
+import { State } from 'vuex-class';
+import { Store } from '../../../../store';
+import { AppEventItemControls } from '../../../event-item/controls/controls';
+import { AppEventItemManage } from '../../../event-item/manage/manage';
 import { AppPollVoting } from '../../../poll/voting/voting';
-import { AppScrollWhen } from '../../../../../lib/gj-lib-client/components/scroll/scroll-when.directive.vue';
-import { Screen } from '../../../../../lib/gj-lib-client/components/screen/screen-service';
 import { AppDevlogPostViewPlaceholder } from './placeholder/placeholder';
-import { AppAd } from '../../../../../lib/gj-lib-client/components/ad/ad';
-import { AppAdPlacement } from '../../../../../lib/gj-lib-client/components/ad/placement/placement';
 
 @View
 @Component({
@@ -29,7 +34,8 @@ import { AppAdPlacement } from '../../../../../lib/gj-lib-client/components/ad/p
 		AppVideo,
 		AppVideoEmbed,
 		AppSketchfabEmbed,
-		AppActivityFeedControls,
+		AppEventItemManage,
+		AppEventItemControls,
 		AppPollVoting,
 		AppAd,
 		AppAdPlacement,
@@ -39,9 +45,18 @@ import { AppAdPlacement } from '../../../../../lib/gj-lib-client/components/ad/p
 	},
 })
 export class AppDevlogPostView extends Vue {
-	@Prop(FiresidePost) post!: FiresidePost;
-	@Prop(Boolean) showGameInfo?: boolean;
-	@Prop(Boolean) inModal?: boolean;
+	@Prop(FiresidePost)
+	post!: FiresidePost;
+
+	@Prop(Boolean)
+	showGameInfo?: boolean;
+
+	@State
+	app!: Store['app'];
 
 	readonly Screen = Screen;
+
+	get shouldShowManage() {
+		return this.post && canUserManagePost(this.post, this.app.user);
+	}
 }
