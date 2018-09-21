de4758a1e80849803c22f76414be4787b0ec99b5
Yariv <YLivay@users.noreply.github.com>
2018-09-21 20:31:49 +0300

    Client force upgrade (#206)
    * Add client upgrade section for when we have to force a client update
    
    * Show in the status bar if the client has any issue with self updating
    
    * Bump lib/gj-lib-client

--- src/_common/client/bootstrap.ts
+++ src/_common/client/bootstrap.ts
@@ -1,12 +1,12 @@
 import { VuexStore } from '../../lib/gj-lib-client/utils/vuex';
 import { initClientApiInterceptors } from './api/api.service';
+import { ClientUpdater } from './client-updater.service';
 import { Client } from './client.service';
-import { store } from './store/index';
 import { ClientUser } from './user/user.service';
 
 export function bootstrapCommonClient(sectionStore: VuexStore) {
 	initClientApiInterceptors();
 	Client.init();
 	ClientUser.init(sectionStore);
-	store.dispatch('bootstrap');
+	ClientUpdater.init();
 }
