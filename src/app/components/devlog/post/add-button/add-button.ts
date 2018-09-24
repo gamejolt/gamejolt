f971e7698ae2e0f24a90794930f72d227a5b5a69
David DeCarmine <hworld@holo-world.com>
2018-09-24 05:32:24 -0700

    Change styling of the add post button.

--- src/app/components/devlog/post/add-button/add-button.ts
+++ src/app/components/devlog/post/add-button/add-button.ts
@@ -2,15 +2,24 @@ import View from '!view!./add-button.html?style=./add-button.styl';
 import { Api } from 'game-jolt-frontend-lib/components/api/api.service';
 import { FiresidePost } from 'game-jolt-frontend-lib/components/fireside/post/post-model';
 import { Game } from 'game-jolt-frontend-lib/components/game/game.model';
+import { AppUserAvatarImg } from 'game-jolt-frontend-lib/components/user/user-avatar/img/img';
+import { AppState, AppStore } from 'game-jolt-frontend-lib/vue/services/app/app-store';
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import { DevlogPostEditModal } from '../edit-modal/edit-modal-service';
 
 @View
-@Component({})
+@Component({
+	components: {
+		AppUserAvatarImg,
+	},
+})
 export class AppDevlogPostAddButton extends Vue {
 	@Prop(Game)
 	game!: Game;
+
+	@AppState
+	user!: AppStore['user'];
 
 	readonly FiresidePost = FiresidePost;
 
