9a7ea51be30b0f24cf6f1b452d01c791e6e1b5e4
David DeCarmine <hworld@holo-world.com>
2018-10-01 08:43:56 -0700

    Fix track event directive not imported into broadcast card

--- src/app/components/broadcast-card/broadcast-card.ts
+++ src/app/components/broadcast-card/broadcast-card.ts
@@ -1,4 +1,5 @@
 import View from '!view!./broadcast-card.html?style=./broadcast-card.styl';
+import { AppTrackEvent } from 'game-jolt-frontend-lib/components/analytics/track-event.directive.vue';
 import { AppCard } from 'game-jolt-frontend-lib/components/card/card';
 import { FiresidePost } from 'game-jolt-frontend-lib/components/fireside/post/post-model';
 import Vue from 'vue';
@@ -8,6 +9,9 @@ import { Component, Prop } from 'vue-property-decorator';
 @Component({
 	components: {
 		AppCard,
+	},
+	directives: {
+		AppTrackEvent,
 	},
 })
 export class AppBroadcastCard extends Vue {
