3fb36e864fce2753264340aebbcd133667dc618e
David DeCarmine <hworld@holo-world.com>
2018-10-02 09:22:41 -0700

    Don't show the follow button in user list for yourself

--- src/app/components/user/list/item/item.ts
+++ src/app/components/user/list/item/item.ts
@@ -3,8 +3,10 @@ import { Screen } from 'game-jolt-frontend-lib/components/screen/screen-service'
 import { AppUserFollowWidget } from 'game-jolt-frontend-lib/components/user/follow-widget/follow-widget';
 import { AppUserAvatarImg } from 'game-jolt-frontend-lib/components/user/user-avatar/img/img';
 import { User } from 'game-jolt-frontend-lib/components/user/user.model';
+import { AppStore } from 'game-jolt-frontend-lib/vue/services/app/app-store';
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
+import { State } from 'vuex-class';
 
 @View
 @Component({
@@ -16,6 +18,9 @@ import { Component, Prop } from 'vue-property-decorator';
 export class AppUserListItem extends Vue {
 	@Prop(User)
 	user!: User;
+
+	@State
+	app!: AppStore;
 
 	readonly Screen = Screen;
 }
