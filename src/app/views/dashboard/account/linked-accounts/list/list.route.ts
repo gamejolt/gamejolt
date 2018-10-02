442070929852f69a34d99908f71048a6e5ec6f6b
Nils <nidresch@googlemail.com>
2018-10-02 03:42:47 +0200

    delete old code

--- src/app/views/dashboard/account/linked-accounts/list/list.route.ts
+++
@@ -1,8 +0,0 @@
-import { RouteConfig } from 'vue-router';
-
-export const routeDashAccountLinkedAccountsList: RouteConfig = {
-	name: 'dash.account.linked-accounts.list',
-	path: 'linked-accounts',
-	props: true,
-	component: () => import(/* webpackChunkName: "routeDashAccountLinkedAccountsList" */ './list'),
-};
