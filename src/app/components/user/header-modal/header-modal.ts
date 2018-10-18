10a875d3b2ac13f6588c505601ff8e71fe05c4e3
Nils <nidresch@googlemail.com>
2018-10-18 18:32:30 +0200

    remove info from user header modal

--- src/app/components/user/header-modal/header-modal.ts
+++ src/app/components/user/header-modal/header-modal.ts
@@ -1,11 +1,10 @@
-import { State } from 'vuex-class';
-import { Component } from 'vue-property-decorator';
 import View from '!view!./header-modal.html';
-
+import { User } from 'game-jolt-frontend-lib/components/user/user.model';
+import { Component } from 'vue-property-decorator';
+import { State } from 'vuex-class';
 import { BaseModal } from '../../../../lib/gj-lib-client/components/modal/base';
 import { Store } from '../../../store/index';
 import { FormUserHeader } from '../../forms/user-header/header';
-import { ModalConfirm } from '../../../../lib/gj-lib-client/components/modal/confirm/confirm-service';
 
 @View
 @Component({
@@ -14,16 +13,24 @@ import { ModalConfirm } from '../../../../lib/gj-lib-client/components/modal/con
 	},
 })
 export default class AppUserHeaderModal extends BaseModal {
-	@State app!: Store['app'];
+	@State
+	app!: Store['app'];
 
-	async clearHeader() {
-		const result = await ModalConfirm.show(
-			this.$gettext(`Are you sure you want to remove your profile header?`),
-			undefined,
-			'yes'
-		);
-		if (result) {
-			this.app.user!.$clearHeader();
+	// We don't want to close the modal after they've uploaded a header since they can set a crop
+	// after. We want to auto-close it after they've saved the crop, though.
+	previousHeaderId: number | null = null;
+
+	created() {
+		if (this.app.user && this.app.user.header_media_item) {
+			this.previousHeaderId = this.app.user.header_media_item.id;
 		}
+	}
+
+	onSaved(user: User) {
+		const newHeaderId = (user.header_media_item && user.header_media_item.id) || null;
+		if (this.previousHeaderId === newHeaderId) {
+			this.modal.resolve(user);
+		}
+		this.previousHeaderId = newHeaderId;
 	}
 }
