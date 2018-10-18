68a9ae91990018fcafa52b18f8490c70dd6a7c61
Nils <nidresch@googlemail.com>
2018-10-18 18:32:18 +0200

    add crop to user header

--- src/app/components/forms/user-header/header.ts
+++ src/app/components/forms/user-header/header.ts
@@ -1,31 +1,43 @@
-import { Component } from 'vue-property-decorator';
 import View from '!view!./header.html';
-
-import { User } from '../../../../lib/gj-lib-client/components/user/user.model';
+import { AppFormControlCrop } from 'game-jolt-frontend-lib/components/form-vue/control/crop/crop';
+import { ModalConfirm } from 'game-jolt-frontend-lib/components/modal/confirm/confirm-service';
+import { Component, Watch } from 'vue-property-decorator';
+import { State } from 'vuex-class';
 import { AppFormControlUpload } from '../../../../lib/gj-lib-client/components/form-vue/control/upload/upload';
-import { AppJolticon } from '../../../../lib/gj-lib-client/vue/components/jolticon/jolticon';
 import { AppForm } from '../../../../lib/gj-lib-client/components/form-vue/form';
 import {
 	BaseForm,
 	FormOnLoad,
 } from '../../../../lib/gj-lib-client/components/form-vue/form.service';
+import { User } from '../../../../lib/gj-lib-client/components/user/user.model';
+import { AppJolticon } from '../../../../lib/gj-lib-client/vue/components/jolticon/jolticon';
+import { Store } from '../../../store/index';
+
+type FormModel = User & {
+	crop: any;
+};
 
 @View
 @Component({
 	components: {
 		AppFormControlUpload,
+		AppFormControlCrop,
 		AppJolticon,
 	},
 })
-export class FormUserHeader extends BaseForm<User> implements FormOnLoad {
-	modelClass = User;
+export class FormUserHeader extends BaseForm<FormModel> implements FormOnLoad {
+	@State
+	app!: Store['app'];
+	modelClass = User as any;
 	saveMethod: '$saveHeader' = '$saveHeader';
 
 	maxFilesize = 0;
-	minSize = 0;
-	maxSize = 0;
 	minAspectRatio = 0;
 	maxAspectRatio = 0;
+	minWidth = 0;
+	minHeight = 0;
+	maxWidth = 0;
+	maxHeight = 0;
 
 	$refs!: {
 		form: AppForm;
@@ -35,12 +47,37 @@ export class FormUserHeader extends BaseForm<User> implements FormOnLoad {
 		return `/web/dash/header/save`;
 	}
 
+	get crop() {
+		return this.formModel.header_media_item
+			? this.formModel.header_media_item.getCrop()
+			: undefined;
+	}
+
+	@Watch('crop')
+	onCropChange() {
+		this.setField('crop', this.crop);
+	}
+
 	onLoad(payload: any) {
 		this.maxFilesize = payload.maxFilesize;
-		this.minSize = payload.minSize;
-		this.maxSize = payload.maxSize;
 		this.minAspectRatio = payload.minAspectRatio;
 		this.maxAspectRatio = payload.maxAspectRatio;
+		this.minWidth = payload.minWidth;
+		this.maxWidth = payload.maxWidth;
+		this.minHeight = payload.minHeight;
+		this.maxHeight = payload.maxHeight;
+	}
+
+	async clearHeader() {
+		const result = await ModalConfirm.show(
+			this.$gettext(`Are you sure you want to remove your profile header?`),
+			undefined,
+			'yes'
+		);
+
+		if (result) {
+			this.app.user!.$clearHeader();
+		}
 	}
 
 	headerSelected() {
