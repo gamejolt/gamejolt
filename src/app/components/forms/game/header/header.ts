6b785c863fec22e1a6291bc9e4711608f2135102
nilllzz <nidresch@googlemail.com>
2018-10-19 02:33:52 +0200

    Header crop (#217)

--- src/app/components/forms/game/header/header.ts
+++ src/app/components/forms/game/header/header.ts
@@ -1,29 +1,37 @@
-import { Component } from 'vue-property-decorator';
 import View from '!view!./header.html';
-
+import { AppFormControlCrop } from 'game-jolt-frontend-lib/components/form-vue/control/crop/crop';
+import { ModalConfirm } from 'game-jolt-frontend-lib/components/modal/confirm/confirm-service';
+import { Component, Watch } from 'vue-property-decorator';
+import { AppFormControlUpload } from '../../../../../lib/gj-lib-client/components/form-vue/control/upload/upload';
+import { AppForm } from '../../../../../lib/gj-lib-client/components/form-vue/form';
 import {
 	BaseForm,
 	FormOnLoad,
 } from '../../../../../lib/gj-lib-client/components/form-vue/form.service';
 import { Game } from '../../../../../lib/gj-lib-client/components/game/game.model';
-import { AppFormControlUpload } from '../../../../../lib/gj-lib-client/components/form-vue/control/upload/upload';
-import { AppForm } from '../../../../../lib/gj-lib-client/components/form-vue/form';
+
+type FormModel = Game & {
+	crop: any;
+};
 
 @View
 @Component({
 	components: {
 		AppFormControlUpload,
+		AppFormControlCrop,
 	},
 })
-export class FormGameHeader extends BaseForm<Game> implements FormOnLoad {
-	modelClass = Game;
+export class FormGameHeader extends BaseForm<FormModel> implements FormOnLoad {
+	modelClass = Game as any;
 	saveMethod = '$saveHeader' as '$saveHeader';
 
 	maxFilesize = 0;
 	minAspectRatio = 0;
 	maxAspectRatio = 0;
 	minWidth = 0;
+	minHeight = 0;
 	maxWidth = 0;
+	maxHeight = 0;
 
 	$refs!: {
 		form: AppForm;
@@ -33,12 +41,37 @@ export class FormGameHeader extends BaseForm<Game> implements FormOnLoad {
 		return `/web/dash/developer/games/header/save/${this.model!.id}`;
 	}
 
+	get crop() {
+		return this.formModel.header_media_item
+			? this.formModel.header_media_item.getCrop()
+			: undefined;
+	}
+
+	@Watch('crop')
+	onCropChange() {
+		this.setField('crop', this.crop);
+	}
+
 	onLoad(payload: any) {
 		this.maxFilesize = payload.maxFilesize;
 		this.minAspectRatio = payload.minAspectRatio;
 		this.maxAspectRatio = payload.maxAspectRatio;
 		this.minWidth = payload.minWidth;
 		this.maxWidth = payload.maxWidth;
+		this.minHeight = payload.minHeight;
+		this.maxHeight = payload.maxHeight;
+	}
+
+	async clearHeader() {
+		const result = await ModalConfirm.show(
+			this.$gettext(`Are you sure you want to remove your game header?`),
+			undefined,
+			'yes'
+		);
+
+		if (result) {
+			this.formModel.$clearHeader();
+		}
 	}
 
 	headerSelected() {
