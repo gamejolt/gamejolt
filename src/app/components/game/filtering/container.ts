d5ccbee41a448cb560bab841be5ef5c8d813a6b2
Nils <nidresch@googlemail.com>
2018-11-09 12:02:01 +0100

    remove silverlight and java applet game filters

--- src/app/components/game/filtering/container.ts
+++ src/app/components/game/filtering/container.ts
@@ -106,8 +106,6 @@ export class GameFilteringContainer {
 					html: Translate.$gettext('games.filtering.browser_html'),
 					flash: Translate.$gettext('games.filtering.browser_flash'),
 					unity: Translate.$gettext('games.filtering.browser_unity'),
-					applet: Translate.$gettext('games.filtering.browser_applet'),
-					silverlight: Translate.$gettext('games.filtering.browser_silverlight'),
 				},
 			},
 			maturity: {
