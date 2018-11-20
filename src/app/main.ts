d83867069ea2dd82bcc01d22c3c26af070140aa2
David DeCarmine <hworld@holo-world.com>
2018-11-20 14:55:44 -0800

    More disable service worker

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,5 @@
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
+// import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import { createApp } from './bootstrap';
-import { Analytics } from '../lib/gj-lib-client/components/analytics/analytics.service';
 
 const { app, router, store } = createApp();
 
@@ -12,28 +11,28 @@ router.onReady(() => {
 	app.$mount('#app');
 });
 
-if (GJ_BUILD_TYPE === 'production' && navigator.serviceWorker) {
-	OfflinePluginRuntime.install({
-		onUpdating() {
-			Analytics.trackEvent('sw', 'updating');
-		},
+// if (GJ_BUILD_TYPE === 'production' && navigator.serviceWorker) {
+// 	OfflinePluginRuntime.install({
+// 		onUpdating() {
+// 			Analytics.trackEvent('sw', 'updating');
+// 		},
 
-		onUpdateReady() {
-			// Tells to new SW to take control immediately
-			OfflinePluginRuntime.applyUpdate();
-			Analytics.trackEvent('sw', 'update-ready');
-		},
+// 		onUpdateReady() {
+// 			// Tells to new SW to take control immediately
+// 			OfflinePluginRuntime.applyUpdate();
+// 			Analytics.trackEvent('sw', 'update-ready');
+// 		},
 
-		onUpdated() {
-			Analytics.trackEvent('sw', 'updated');
+// 		onUpdated() {
+// 			Analytics.trackEvent('sw', 'updated');
 
-			// TODO: Hook into how we update based on site-api version.
-			// Reload the webpage to load into the new version
-			// window.location.reload();
-		},
+// 			// TODO: Hook into how we update based on site-api version.
+// 			// Reload the webpage to load into the new version
+// 			// window.location.reload();
+// 		},
 
-		onUpdateFailed() {
-			Analytics.trackEvent('sw', 'update-failed');
-		},
-	});
-}
+// 		onUpdateFailed() {
+// 			Analytics.trackEvent('sw', 'update-failed');
+// 		},
+// 	});
+// }
