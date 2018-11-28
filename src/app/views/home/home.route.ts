4df4d045fb46a5fdcb0631a2723d9573652c5ae8
David DeCarmine <hworld@holo-world.com>
2018-11-28 13:56:39 -0800

    Remove `props: true` to match master.

--- src/app/views/home/home.route.ts
+++ src/app/views/home/home.route.ts
@@ -3,6 +3,5 @@ import { RouteConfig } from 'vue-router';
 export const routeHome: RouteConfig = {
 	name: 'home',
 	path: '/',
-	props: true,
 	component: () => import(/* webpackChunkName: "routeHome" */ './home'),
 };
--- src/app/views/notifications/notifications.route.ts
+++ src/app/views/notifications/notifications.route.ts
@@ -3,6 +3,5 @@ import { RouteConfig } from 'vue-router';
 export const routeNotifications: RouteConfig = {
 	name: 'notifications',
 	path: '/notifications',
-	props: true,
 	component: () => import(/* webpackChunkName: "routeNotifications" */ './notifications'),
 };
