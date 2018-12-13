84f42598c98bccc625d5a9e0335b7687f817150f
hworld <hworld@holo-world.com>
2018-12-12 16:30:26 -0800

    Show more recommended games on game pages! (#248)

--- src/app/views/discover/games/view/overview/_recommended/recommended.ts
+++ src/app/views/discover/games/view/overview/_recommended/recommended.ts
@@ -1,12 +1,16 @@
 import View from '!view!./recommended.html';
+import { AppAdWidget } from 'game-jolt-frontend-lib/components/ad/widget/widget';
 import Vue from 'vue';
-import { Component, Prop } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
 import { Screen } from '../../../../../../../lib/gj-lib-client/components/screen/screen-service';
 import { AppGameGrid } from '../../../../../../components/game/grid/grid';
 import { AppGameGridPlaceholder } from '../../../../../../components/game/grid/placeholder/placeholder';
 import { AppGameList } from '../../../../../../components/game/list/list';
 import { AppGameListPlaceholder } from '../../../../../../components/game/list/placeholder/placeholder';
 import { RouteStore, RouteStoreModule } from '../../view.store';
+
+/** Number of games to show before inserting an ad. */
+const SplitIndex = 3;
 
 @View
 @Component({
@@ -15,18 +19,33 @@ import { RouteStore, RouteStoreModule } from '../../view.store';
 		AppGameGrid,
 		AppGameListPlaceholder,
 		AppGameList,
+		AppAdWidget,
 	},
 })
 export class AppDiscoverGamesViewOverviewRecommended extends Vue {
 	@RouteStoreModule.State
 	recommendedGames!: RouteStore['recommendedGames'];
 
-	@Prop(Number)
-	num?: number;
+	@RouteStoreModule.State
+	postsCount!: RouteStore['postsCount'];
 
 	readonly Screen = Screen;
 
-	get slicedGames() {
-		return this.num ? this.recommendedGames.slice(0, this.num) : this.recommendedGames;
+	get isLoaded() {
+		return this.recommendedGames.length > 0;
+	}
+
+	get shouldShowBottomAd() {
+		// We only want to show the bottom ad if there is enough room on the
+		// page.
+		return this.postsCount > 4 && Screen.isLg;
+	}
+
+	get gamesBeforeAd() {
+		return this.recommendedGames.slice(0, SplitIndex);
+	}
+
+	get gamesAfterAd() {
+		return Screen.isLg ? this.recommendedGames.slice(SplitIndex) : [];
 	}
 }
