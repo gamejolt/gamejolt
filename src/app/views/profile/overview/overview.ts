60c819a3dc563180b2bc5694a1cd82b3564f2ed7
David DeCarmine <hworld@holo-world.com>
2018-12-13 10:17:45 -0800

    Some tweaks

--- src/app/views/profile/overview/overview.ts
+++ src/app/views/profile/overview/overview.ts
@@ -228,7 +228,7 @@ export default class RouteProfileOverview extends BaseRouteComponent {
 	async reloadPreviewComments() {
 		if (this.user instanceof User) {
 			const $payload = await Api.sendRequest(
-				'/web/profile/comment-info/@' + this.user.username
+				'/web/profile/comment-overview/@' + this.user.username
 			);
 			this.overviewComments = Comment.populate($payload.comments);
 			this.user.comment_count = $payload.count;
