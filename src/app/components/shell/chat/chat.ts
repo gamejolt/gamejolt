d702d0ff64827fd16716c9a46e5c35a45a2913d5
David DeCarmine <hworld@holo-world.com>
2018-12-20 11:32:18 -0800

    Refactor.

--- src/app/components/shell/chat/chat.ts
+++ src/app/components/shell/chat/chat.ts
@@ -1,17 +1,12 @@
 import View from '!view!./chat.html';
-import { Meta } from 'game-jolt-frontend-lib/components/meta/meta-service';
 import Vue from 'vue';
 import { Component, Watch } from 'vue-property-decorator';
 import { Action, State } from 'vuex-class';
 import { EscapeStack } from '../../../../lib/gj-lib-client/components/escape-stack/escape-stack.service';
-import {
-	EventBus,
-	EventBusDeregister,
-} from '../../../../lib/gj-lib-client/components/event-bus/event-bus.service';
 import { Screen } from '../../../../lib/gj-lib-client/components/screen/screen-service';
 import { AppScrollInviewParent } from '../../../../lib/gj-lib-client/components/scroll/inview/parent';
 import { Store } from '../../../store/index';
-import { ChatClient, ChatNewMessageEvent } from '../../chat/client';
+import { ChatClient } from '../../chat/client';
 import { AppChatSidebar } from '../../chat/sidebar/sidebar';
 import { AppChatWindows } from '../../chat/windows/windows';
 
@@ -35,60 +30,14 @@ export class AppShellChat extends Vue {
 	@Action
 	toggleRightPane!: Store['toggleRightPane'];
 
-	private isWindowFocused = false;
-	private unfocusedNotificationsCount = 0;
-
-	private newMessageDeregister?: EventBusDeregister;
-	private focusCallback?: EventListener;
-	private blurCallback?: EventListener;
 	private escapeCallback?: Function;
 
-	get totalNotificationsCount() {
-		return this.chat.roomNotificationsCount + this.unfocusedNotificationsCount;
-	}
-
 	mounted() {
-		this.blurCallback = () => (this.isWindowFocused = false);
-		this.focusCallback = () => (this.isWindowFocused = true);
 		this.escapeCallback = () => this.hideChatPane();
-		this.newMessageDeregister = EventBus.on('Chat.newMessage', (event: ChatNewMessageEvent) => {
-			// If we have a general room open, and our window is unfocused or
-			// minimized, then increment our room notifications count (since
-			// they haven't seen this message yet). Note that if these messages
-			// came in because we were priming output for a room with old
-			// messages, we don't want to increase notification counts.
-			if (!this.isWindowFocused && this.chat.room) {
-				if (
-					!event.isPrimer &&
-					event.message &&
-					event.message.roomId === this.chat.room.id
-				) {
-					++this.unfocusedNotificationsCount;
-				}
-			}
-		});
-
-		window.addEventListener('blur', this.blurCallback);
-		window.addEventListener('focus', this.focusCallback);
 		EscapeStack.register(this.escapeCallback);
 	}
 
 	destroyed() {
-		if (this.newMessageDeregister) {
-			this.newMessageDeregister();
-			this.newMessageDeregister = undefined;
-		}
-
-		if (this.blurCallback) {
-			window.removeEventListener('blur', this.blurCallback);
-			this.blurCallback = undefined;
-		}
-
-		if (this.focusCallback) {
-			window.removeEventListener('blur', this.focusCallback);
-			this.focusCallback = undefined;
-		}
-
 		if (this.escapeCallback) {
 			EscapeStack.deregister(this.escapeCallback);
 			this.escapeCallback = undefined;
@@ -116,31 +65,6 @@ export class AppShellChat extends Vue {
 			}
 		} else {
 			this.chat.leaveRoom();
-		}
-	}
-
-	// Keep the title up to date with chat notification counts.
-	@Watch('totalNotificationsCount')
-	onNotificationsCountChange(count: number) {
-		Meta.chatCount = count;
-	}
-
-	@Watch('isWindowFocused')
-	onWindowFocusChanged(isFocused: boolean) {
-		// When the window is unfocused, start counting notifications for
-		// current room.
-		if (!isFocused) {
-			// Notify the client that we are unfocused, so it should start
-			// accumulating notifications for the current room.
-			this.chat.setFocused(false);
-		} else {
-			// When we focus it back, clear out all accumulated notifications.
-			// Set that we're not longer focused, and clear out room
-			// notifications. The user has now "seen" the messages.
-			this.unfocusedNotificationsCount = 0;
-
-			// Notify the client that we aren't unfocused anymore.
-			this.chat.setFocused(true);
 		}
 	}
 }
--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -12,7 +12,6 @@ import {
 import { Connection } from '../../lib/gj-lib-client/components/connection/connection-service';
 import { ContentFocus } from '../../lib/gj-lib-client/components/content-focus/content-focus.service';
 import { Growls } from '../../lib/gj-lib-client/components/growls/growls.service';
-import { Meta } from '../../lib/gj-lib-client/components/meta/meta-service';
 import { ModalConfirm } from '../../lib/gj-lib-client/components/modal/confirm/confirm-service';
 import {
 	ThemeActions,
@@ -298,7 +297,6 @@ export class Store extends VuexStore<Store, Actions, Mutations> {
 		} else {
 			this.unreadNotificationsCount += payload.count;
 		}
-		Meta.notificationCount = this.unreadActivityCount + this.unreadNotificationsCount;
 	}
 
 	@VuexMutation
@@ -308,7 +306,6 @@ export class Store extends VuexStore<Store, Actions, Mutations> {
 		} else {
 			this.unreadNotificationsCount = payload.count;
 		}
-		Meta.notificationCount = this.unreadActivityCount + this.unreadNotificationsCount;
 	}
 
 	@VuexMutation
