b889a70f5252cddc9b62dce8e820d4061b790e6c
David DeCarmine <hworld@holo-world.com>
2019-01-15 14:37:43 -0800

    Removed a ton of ad code that we no longer need.

--- src/app/components/game/listing/listing-container-service.ts
+++ src/app/components/game/listing/listing-container-service.ts
@@ -1,5 +1,4 @@
 import { Route } from 'vue-router';
-import { Ads, AdSettingsContainer } from '../../../../lib/gj-lib-client/components/ad/ads.service';
 import { Game } from '../../../../lib/gj-lib-client/components/game/game.model';
 import { GameFilteringContainer } from '../filtering/container';
 
@@ -19,34 +18,7 @@ export class GameListingContainer {
 		this.gamesCount = payload.gamesCount;
 		this.perPage = payload.perPage;
 
-		this.currentPage = route.query.page ? parseInt(route.query.page, 10) : 1;
+		this.currentPage = route.query.page ? parseInt(route.query.page + '', 10) : 1;
 		this.section = route.params.section || 'hot';
-	}
-
-	setAdTargeting(route: Route, adUnit?: string) {
-		const genre = route.params.category || undefined;
-		const channel = route.params.channel || undefined;
-
-		let mat: string | undefined;
-		let paid: string | undefined;
-		if (this.filteringContainer) {
-			const priceFilter = this.filteringContainer.getFilter('price');
-			mat = this.filteringContainer.getFilter('maturity');
-			paid = priceFilter ? (priceFilter !== 'free' ? 'y' : 'n') : undefined;
-		}
-
-		const adSettings = new AdSettingsContainer();
-		adSettings.targeting = {
-			mat,
-			genre,
-			channel,
-			paid,
-		};
-
-		if (adUnit) {
-			adSettings.adUnit = adUnit;
-		}
-
-		Ads.setPageSettings(adSettings);
 	}
 }
