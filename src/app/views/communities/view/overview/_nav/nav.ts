add7c0b6b2daf1d9602e9d7ff73641599957ccbb
hworld <hworld@holo-world.com>
2019-01-18 16:19:33 -0600

    3-col layout for communities (#256)

---
+++ src/app/views/communities/view/overview/_nav/nav.ts
@@ -0,0 +1,60 @@
+import View from '!view!./nav.html?style=./nav.styl';
+import { Community } from 'game-jolt-frontend-lib/components/community/community.model';
+import { Screen } from 'game-jolt-frontend-lib/components/screen/screen-service';
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+
+type NavItemType = 'general' | 'tag';
+
+class NavItem {
+	constructor(
+		public readonly type: NavItemType,
+		public readonly label: string,
+		public readonly channel: 'featured' | string
+	) {}
+
+	get routeParam() {
+		return this.channel === 'featured' ? undefined : this.channel;
+	}
+}
+
+class NavGroup {
+	constructor(public readonly items: NavItem[]) {}
+}
+
+@View
+@Component({})
+export class AppCommunitiesViewOverviewNav extends Vue {
+	@Prop(Community)
+	community!: Community;
+
+	@Prop(Array)
+	tags!: string[];
+
+	@Prop(String)
+	channel!: string;
+
+	isNavExpanded = false;
+
+	readonly Screen = Screen;
+
+	get groups(): NavGroup[] {
+		return [
+			new NavGroup([
+				new NavItem('general', this.$gettext('Featured'), 'featured'),
+				new NavItem('general', this.$gettext('All Posts'), 'all'),
+			]),
+			new NavGroup(this.tags.map(i => new NavItem('tag', i, i))),
+		];
+	}
+
+	get activeItem() {
+		for (const group of this.groups) {
+			for (const item of group.items) {
+				if (item.channel === this.channel) {
+					return item;
+				}
+			}
+		}
+	}
+}
