1c2c2c36dbf9e3efc6968f80bb01b220f121828d
YLivay <yarivlivay@gmail.com>
2019-05-06 18:30:04 +0300

    Added some analytics to onboarding

--- src/app/views/welcome/welcome.ts
+++ src/app/views/welcome/welcome.ts
@@ -1,14 +1,15 @@
 import { Api } from 'game-jolt-frontend-lib/components/api/api.service';
 import { Auth } from 'game-jolt-frontend-lib/components/auth/auth.service';
 import AppEditableOverlay from 'game-jolt-frontend-lib/components/editable-overlay/editable-overlay.vue';
+import Onboarding from 'game-jolt-frontend-lib/components/onboarding/onboarding.service';
 import {
 	BaseRouteComponent,
 	RouteResolver,
 } from 'game-jolt-frontend-lib/components/route/route-component';
 import { AppThemeSvg } from 'game-jolt-frontend-lib/components/theme/svg/svg';
 import AppUserAvatar from 'game-jolt-frontend-lib/components/user/user-avatar/user-avatar.vue';
+import { AppState, AppStore } from 'game-jolt-frontend-lib/vue/services/app/app-store';
 import { Component } from 'vue-property-decorator';
-import { AppState, AppStore } from '../../../lib/gj-lib-client/vue/services/app/app-store';
 import { UserAvatarModal } from '../../components/user/avatar-modal/avatar-modal.service';
 
 @Component({
@@ -26,6 +27,7 @@ export default class RouteWelcome extends BaseRouteComponent {
 	@AppState
 	user!: AppStore['user'];
 
+	startedOn = Date.now();
 	hasSelectedAvatar = false;
 	hasModifiedBio = false;
 	isInputDisabled = false;
@@ -42,21 +44,48 @@ export default class RouteWelcome extends BaseRouteComponent {
 		return this.hasSelectedAvatar && (this.hasBio || this.hasModifiedBio);
 	}
 
+	routeCreated() {
+		// Since the app section and auth section may run on different ports
+		// the local storage key used to start the onboarding flow might not be available.
+		// This allows us to test in dev.
+		if (GJ_ENVIRONMENT === 'development') {
+			Onboarding.start();
+		}
+
+		if (!Onboarding.isOnboarding) {
+			this.$router.push({ name: 'home' });
+			return;
+		}
+	}
+
 	routeResolved() {
 		if (!this.user) {
-			this.$router.push('home');
+			this.$router.push({ name: 'home' });
 			return;
 		}
 
+		Onboarding.startStep('profile');
+
 		this.hasSelectedAvatar = !!this.user && !!this.user.avatar_media_item;
+		if (this.hasSelectedAvatar) {
+			Onboarding.trackEvent('avatar', 'bootstrap');
+		}
+
+		if (this.hasBio) {
+			Onboarding.trackEvent('bio', 'bootstrap');
+		}
 	}
 
 	async chooseAvatar() {
+		Onboarding.trackEvent('avatar', 'set');
 		await UserAvatarModal.show();
 		this.hasSelectedAvatar = true;
 	}
 
 	onBioChanged() {
+		if (!this.hasModifiedBio) {
+			Onboarding.trackEvent('bio', 'set');
+		}
 		this.hasModifiedBio = true;
 	}
 
@@ -66,6 +95,8 @@ export default class RouteWelcome extends BaseRouteComponent {
 		}
 
 		this.isInputDisabled = true;
+
+		Onboarding.endStep(!this.isNext);
 
 		if (this.hasModifiedBio) {
 			await this.user.$save();
