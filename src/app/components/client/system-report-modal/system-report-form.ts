e186987d95f70d02435030de079111b783845bf1
YLivay <yarivlivay@gmail.com>
2019-07-15 16:20:09 +0300

    Add issue tracker URL to the send system report modal

--- src/app/components/client/system-report-modal/system-report-form.ts
+++ src/app/components/client/system-report-modal/system-report-form.ts
@@ -3,7 +3,7 @@ import { Api } from 'game-jolt-frontend-lib/components/api/api.service';
 import { BaseForm, FormOnSubmit } from 'game-jolt-frontend-lib/components/form-vue/form.service';
 import { Component } from 'vue-property-decorator';
 import { ClientLogger } from '../../../../_common/client/logger/logger.service';
-
+import { ClientLibraryStore, ClientLibraryState } from '../../../store/client-library';
 
 interface FormModel {
 	description: string;
@@ -13,14 +13,22 @@ interface FormModel {
 
 @Component({})
 export default class FormClientSystemReport extends BaseForm<FormModel> implements FormOnSubmit {
+	@ClientLibraryState
+	games!: ClientLibraryStore['gamesById'];
+
+	@ClientLibraryState
+	packages!: ClientLibraryStore['packagesById'];
+
 	onSubmit() {
 		const log = ClientLogger.getLogInfo();
 
 		this.setField('log_lines', log.logLines.join('\n'));
 		this.setField('os_info', log.osInfo);
+		this.setField('localdb_games', this.games);
+		this.setField('localdb_packages', this.packages);
 
 		return Api.sendRequest('/web/client/logs/submit', this.formModel, {
-			allowComplexData: ['os_info'],
+			allowComplexData: ['os_info', 'localdb_games', 'localdb_packages'],
 		});
 	}
 }
--- src/app/components/client/system-report-modal/system-report-form.vue
+++ src/app/components/client/system-report-modal/system-report-form.vue
@@ -1,5 +1,24 @@
 <template>
 	<app-form name="systemReportForm">
+		<app-form-group name="issue_tracker_url" optional :label="$gettext('Issue Tracker Link')">
+			<p class="help-block">
+				<translate>
+					Paste the link to your ticket here. It's optional, but without it we can't get back to you
+					if we need more info to solve it.
+				</translate>
+				<span class="emoji emoji-huh" />
+			</p>
+
+			<app-form-control
+				type="text"
+				placeholder="https://github.com/gamejolt/issue-tracker/issues/1313"
+				:rules="{
+					max: 255,
+				}"
+			/>
+			<app-form-control-errors />
+		</app-form-group>
+
 		<app-form-group name="description" :label="$gettext('system_report.form.description_label')">
 			<p class="help-block">
 				<translate>system_report.form.description_help</translate>
