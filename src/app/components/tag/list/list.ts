f20552ce1b7729dd1127d7adea5cb43a7fc79213
YLivay <yarivlivay@gmail.com>
2019-07-19 16:29:09 +0300

    Add adult category

--- src/app/components/tag/list/list.ts
+++ src/app/components/tag/list/list.ts
@@ -1,8 +1,8 @@
-import AppScrollScroller from 'game-jolt-frontend-lib/components/scroll/scroller/scroller.vue'
+import AppScrollScroller from 'game-jolt-frontend-lib/components/scroll/scroller/scroller.vue';
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import { TagsInfo } from '../tags-info.service';
-import AppTagThumbnail from '../thumbnail/thumbnail.vue'
+import AppTagThumbnail from '../thumbnail/thumbnail.vue';
 
 const FeaturedTags = [
 	'action',
@@ -11,6 +11,7 @@ const FeaturedTags = [
 	'fangame',
 	'fnaf',
 	'rpg',
+	'adult',
 	'other',
 	'multiplayer',
 	'platformer',
