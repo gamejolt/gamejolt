ae086ae08bf7b8c6206ab00c5ae5f057bc82f1c8
nilllzz <nidresch@googlemail.com>
2019-08-07 18:42:19 +0200

    Inline comment buttons (#305)
    * add controls and pass startup prop
    
    * add analytics
    
    * rename prop

--- src/app/components/event-item/controls/comment-add-placeholder/placeholder.ts
+++ src/app/components/event-item/controls/comment-add-placeholder/placeholder.ts
@@ -1,9 +1,37 @@
+import { GJ_EMOJIS } from 'game-jolt-frontend-lib/components/content/content-editor/schemas/specs/nodes/gj-emoji-nodespec';
+import { AppTooltip } from 'game-jolt-frontend-lib/components/tooltip/tooltip';
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
-@Component({})
+@Component({
+	directives: {
+		AppTooltip,
+	},
+})
 export default class AppEventItemControlsCommentAddPlaceholder extends Vue {
-	onClick() {
-		this.$emit('click');
+	emoji = 'huh'; // Gets set to a random one at created
+
+	get emojis() {
+		return GJ_EMOJIS;
+	}
+
+	created() {
+		this.setRandomEmoji();
+	}
+
+	private setRandomEmoji() {
+		const prev = this.emoji;
+		do {
+			const emojiIndex = Math.floor(Math.random() * GJ_EMOJIS.length);
+			this.emoji = GJ_EMOJIS[emojiIndex];
+		} while (prev === this.emoji);
+	}
+
+	onMouseEnter() {
+		this.setRandomEmoji();
+	}
+
+	onClick(type: string) {
+		this.$emit('click', type);
 	}
 }
