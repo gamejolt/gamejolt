3cde8566e498a79148178f717fab379f72037ffb
Nils <nidresch@googlemail.com>
2019-10-03 23:18:00 +0200

    fix blips being invisible

--- src/app/components/shell/cbar/cbar.vue
+++ src/app/components/shell/cbar/cbar.vue
@@ -1,15 +1,17 @@
 <template>
 	<div id="shell-cbar" v-if="shouldShow">
 		<app-scroll-scroller class="-scroller" overlay hide-scrollbar :inview-throttle="0">
-			<transition-group name="-items">
-				<app-shell-cbar-item
-					v-for="community of communities"
-					:key="community.id"
-					:community="community"
-				/>
+			<div class="-inner">
+				<transition-group name="-items">
+					<app-shell-cbar-item
+						v-for="community of communities"
+						:key="community.id"
+						:community="community"
+					/>
 
-				<app-shell-cbar-add-item key="add" />
-			</transition-group>
+					<app-shell-cbar-add-item key="add" />
+				</transition-group>
+			</div>
 		</app-scroll-scroller>
 	</div>
 </template>
@@ -23,7 +25,6 @@
 	change-bg('darkest')
 	position: fixed
 	width: $shell-cbar-width
-	padding: 15px $cbar-h-padding
 
 .-items-move
 	transition: transform 0.3s
@@ -33,6 +34,9 @@
 	position: relative
 	width: 100%
 	height: 100%
+
+.-inner
+	padding: 15px $cbar-h-padding
 </style>
 
 <script lang="ts" src="./cbar"></script>
