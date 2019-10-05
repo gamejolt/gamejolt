bbb6057e6770c2d2a5ce54fc7e3a5bc519b18d5c
Nils <nidresch@googlemail.com>
2019-10-05 06:22:07 +0200

    add verified tick component

---
+++ src/_common/community/verified-tick/verified-tick.vue
@@ -0,0 +1,23 @@
+<template>
+	<app-jolticon
+		v-if="community.is_verified"
+		:class="{
+			'-small': this.small,
+		}"
+		icon="verified"
+		:big="big"
+		v-app-tooltip="tooltip"
+	/>
+</template>
+
+<style lang="stylus" scoped>
+@require '~styles/variables'
+
+$jolticon-size = 16px
+
+.-small
+	margin: 0 2px
+	font-size: $jolticon-size * 0.85
+</style>
+
+<script lang="ts" src="./verified-tick"></script>
