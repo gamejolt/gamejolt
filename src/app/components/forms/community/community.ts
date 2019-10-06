95cb7897cfc9acc94dc5f9af81ed65adae8c5363
Nils <nidresch@googlemail.com>
2019-10-06 05:50:53 +0200

    add theme to community details form

--- src/app/components/forms/community/community.ts
+++ src/app/components/forms/community/community.ts
@@ -1,16 +1,29 @@
 import { Component } from 'vue-property-decorator';
 import { Action } from 'vuex-class';
 import { Community } from '../../../../_common/community/community.model';
+import AppFormControlTheme from '../../../../_common/form-vue/control/theme/theme.vue';
 import { BaseForm, FormOnSubmitSuccess } from '../../../../_common/form-vue/form.service';
+import { Theme } from '../../../../_common/theme/theme.model';
+import { ThemeMutation, ThemeState, ThemeStore } from '../../../../_common/theme/theme.store';
 import { Store } from '../../../store';
 
-@Component({})
+@Component({
+	components: {
+		AppFormControlTheme,
+	},
+})
 export default class FormCommunity extends BaseForm<Community> implements FormOnSubmitSuccess {
 	modelClass = Community;
 	warnOnDiscard = false;
 
 	@Action
 	joinCommunity!: Store['joinCommunity'];
+
+	@ThemeMutation
+	setFormTheme!: ThemeStore['setFormTheme'];
+
+	@ThemeState
+	userTheme!: ThemeStore['userTheme'];
 
 	onSubmitSuccess(response: any) {
 		const community = new Community(response.community);
@@ -21,5 +34,11 @@ export default class FormCommunity extends BaseForm<Community> implements FormOn
 		this.joinCommunity(community);
 
 		this.$router.push(community.routeEditLocation);
+	}
+
+	onThemeChanged() {
+		// Default theme would be the user theme. Don't want to fallback to page theme otherwise
+		// when clearing theme it'll show the page theme.
+		this.setFormTheme(this.formModel.theme || this.userTheme || new Theme());
 	}
 }
