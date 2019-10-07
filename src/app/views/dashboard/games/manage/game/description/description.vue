0ca1b9e9f925861eb3a8a235176a3697f0e68201
Yariv <YLivay@users.noreply.github.com>
2019-10-07 22:57:25 +0300

    Game description improve (#326)
    * Allow better customization of form control labels with slots
    
    * Improve game description page

--- src/app/views/dashboard/games/manage/game/description/description.vue
+++ src/app/views/dashboard/games/manage/game/description/description.vue
@@ -1,18 +1,19 @@
 <template>
 	<div class="row">
-		<div class="col-sm-4 col-sm-push-8">
+		<div class="hidden-xs col-sm-4 col-sm-push-8">
 			<div class="page-help">
+				<h4>Writing a Good Description</h4>
 				<p>
 					<translate>
-						You can edit your description whenever you want and add to it over time. A good
-						description generally contains things like a gameplay summary, control overview, story,
-						and credits.
+						A good description generally contains things like a gameplay summary, control overview,
+						story, credits, etc. You can edit your description whenever you want and add to it over
+						time.
 					</translate>
 				</p>
 				<p>
 					<translate>
-						Upload images and use the markdown embeds in your description as in-context shots for
-						your game or for stylized headings.
+						You can also upload images to use as in-context shots for your game or for stylized
+						headings.
 					</translate>
 				</p>
 			</div>
@@ -23,5 +24,10 @@
 		</div>
 	</div>
 </template>
+
+<style lang="stylus" scoped>
+h4
+	margin-top: 0
+</style>
 
 <script lang="ts" src="./description"></script>
