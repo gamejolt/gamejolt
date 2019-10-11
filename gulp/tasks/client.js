6c986cbcabfd22da66544260bf7fc08d9b1cb059
Yariv Livay <yarivlivay@gmail.com>
2019-10-11 17:06:55 +0300

    Build a staging client using the govendor branch of joltron

--- gulp/tasks/client.js
+++ gulp/tasks/client.js
@@ -392,7 +392,7 @@ module.exports = config => {
 			const gitStatus = 'git -C ' + joltronRepoDir + ' status';
 			let gitClone =
 				'git clone --branch ' +
-				joltronVersion +
+				'govendor' + // joltronVersion +
 				' https://github.com/gamejolt/joltron ' +
 				joltronRepoDir;
 
