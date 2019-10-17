2319432408e1d88a4b4423a421d59c02d22ecf21
nilllzz <nidresch@googlemail.com>
2019-10-17 14:11:59 +0200

    Mobile community sidebar (#357)
    * implementation
    
    * fix to isMobile

---
+++ src/app/components/community/sidebar/modal/modal.service.ts
@@ -0,0 +1,49 @@
+import { asyncComponentLoader } from '../../../../../utils/utils';
+import { Community } from '../../../../../_common/community/community.model';
+import { Modal } from '../../../../../_common/modal/modal.service';
+import { User } from '../../../../../_common/user/user.model';
+
+interface CommunitySidebarModalOptions {
+	community: Community;
+	isEditing: boolean;
+	owner: User | null;
+	knownMembers: User[];
+	knownMemberCount: number;
+	collaborators: User[];
+	hasMoreCollaborators: boolean;
+	initialCollaboratorCount: number;
+}
+
+export class CommunitySidebarModal {
+	static async show(options: CommunitySidebarModalOptions) {
+		const {
+			community,
+			isEditing,
+			owner,
+			knownMembers,
+			knownMemberCount,
+			collaborators,
+			hasMoreCollaborators,
+			initialCollaboratorCount,
+		} = options;
+
+		return await Modal.show<void>({
+			modalId: 'CommunitySidebarModal-' + community.id,
+			component: () =>
+				asyncComponentLoader(
+					import(/* webpackChunkName: "CommunitySidebarModal" */ './modal.vue')
+				),
+			props: {
+				community,
+				isEditing,
+				owner,
+				knownMembers,
+				knownMemberCount,
+				collaborators,
+				hasMoreCollaborators,
+				initialCollaboratorCount,
+			},
+			size: 'sm',
+		});
+	}
+}
