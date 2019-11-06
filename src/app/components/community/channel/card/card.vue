9c10eeb9d98104390a75f339078bf0739e607b02
nilllzz <nidresch@googlemail.com>
2019-11-06 18:16:43 +0100

    Channel customization (#337)
    * some work
    
    * finished
    
    * remove description from form
    
    * card change wip
    
    * remove description from model
    
    * update design, remove description, add featured background
    
    * use media server url
    
    * change endpoint

---
+++ src/app/components/community/channel/card/card.vue
@@ -0,0 +1,32 @@
+<template>
+	<router-link class="-card" :class="{ '-card-active': isActive }" :to="linkTo" :title="title">
+		<div class="-card-bg" v-if="hasBackgroundImage">
+			<div
+				class="-card-bg-img"
+				:class="{ '-card-bg-img-active': isActive }"
+				:style="{
+					'background-image': `url('${backgroundItem.mediaserver_url}')`,
+				}"
+			></div>
+
+			<div class="-card-bg-gradient"></div>
+		</div>
+
+		<div v-if="isActive" class="-card-active-indicator"></div>
+
+		<div class="-card-content">
+			<div
+				class="-card-content-title"
+				:class="{ '-card-content-title-overlay': hasBackgroundImage }"
+			>
+				{{ title }}
+			</div>
+
+			<div v-if="isUnread" class="-card-content-unread" v-app-tooltip="$gettext(`Unread`)"></div>
+		</div>
+	</router-link>
+</template>
+
+<style lang="stylus" src="./card.styl" scoped></style>
+
+<script lang="ts" src="./card"></script>
--- src/app/views/communities/view/overview/edit/channels/channels.vue
+++ src/app/views/communities/view/overview/edit/channels/channels.vue
@@ -24,39 +24,100 @@
 				:channels="community.channels"
 			/>
 
-			<ul class="-channel-list" v-if="community.channels">
-				<draggable v-model="community.channels" @change="saveChannelSort">
-					<li v-for="channel in community.channels" :key="channel.id">
-						<a class="tag">
-							{{ channel.title }}
+			<app-card-list
+				:items="community.channels"
+				:active-item="activeItem"
+				@activate="activeItem = $event"
+			>
+				<app-card-list-item :id="`channel-container-featured`" :item="community">
+					<div class="row">
+						<div class="col-xs-6 col-xs-offset-3 col-sm-2 col-sm-offset-0">
+							<img
+								v-if="community.featured_background"
+								class="-channel-img-preview"
+								:src="community.featured_background.img_url"
+							/>
 
-							<a v-if="canRemoveChannel" @click.stop="onClickRemoveChannel(channel)">
-								<app-jolticon icon="remove" v-app-tooltip="$gettext(`Remove Channel`)" />
-							</a>
-						</a>
-					</li>
-				</draggable>
-			</ul>
+							<br class="visible-xs" />
+						</div>
+						<div class="col-xs-12 col-sm-10">
+							<div class="card-title">
+								<h5><translate>featured</translate></h5>
+							</div>
+						</div>
+					</div>
+					<template slot="body">
+						<form-community-channel-edit-featured
+							:model="community"
+							@save="featuredBackgroundEdited"
+							@clear="featuredBackgroundEdited"
+						/>
+					</template>
+				</app-card-list-item>
+			</app-card-list>
+
+			<app-card-list
+				v-if="community.channels"
+				:items="community.channels"
+				:active-item="activeItem"
+				@activate="activeItem = $event"
+			>
+				<app-card-list-draggable @change="saveChannelSort">
+					<app-card-list-item
+						v-for="channel in community.channels"
+						:key="channel.id"
+						:id="`channel-container-${channel.id}`"
+						:item="channel"
+					>
+						<div class="row">
+							<div class="col-xs-6 col-xs-offset-3 col-sm-2 col-sm-offset-0">
+								<img
+									v-if="channel.background"
+									class="-channel-img-preview"
+									:src="channel.background.img_url"
+								/>
+
+								<br class="visible-xs" />
+							</div>
+							<div class="col-xs-12 col-sm-10">
+								<a
+									v-if="canRemoveChannel"
+									class="card-remove"
+									@click.stop="onClickRemoveChannel(channel)"
+									v-app-tooltip="$gettext(`Remove Channel`)"
+								>
+									<app-jolticon icon="remove" />
+								</a>
+
+								<div class="card-title">
+									<h5>{{ channel.title }}</h5>
+								</div>
+							</div>
+						</div>
+
+						<template slot="body">
+							<form-community-channel-edit
+								:community="community"
+								:model="channel"
+								@save="channelEdited"
+								@clear="channelEdited"
+							/>
+						</template>
+					</app-card-list-item>
+				</app-card-list-draggable>
+			</app-card-list>
 		</app-community-perms>
 	</div>
 </template>
 
 <style lang="stylus" scoped>
 @require '~styles/variables'
+@require '~styles-lib/mixins'
 
-.-channel-list
-	list-style: none
-	padding-left: 0
-
-	li
-		margin-bottom: 10px
-
-	.tag
-		border-radius: 10px
-		font-weight: normal
-		cursor: move !important
-		font-size: $font-size-base
-		padding: 6px
+.-channel-img-preview
+	width: 68px
+	height: 25px
+	rounded-corners()
 
 </style>
 
