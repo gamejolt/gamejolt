64a6c2cbf4b6ea928b018fb402d0bfb93b25c225
Yariv Livay <yarivlivay@gmail.com>
2019-11-26 23:55:49 +0200

    Add playwire terms to privacy and cookie sections

--- src/app/views/legal/privacy/privacy.ts
+++ src/app/views/legal/privacy/privacy.ts
@@ -1,18 +1,33 @@
-import { BaseRouteComponent } from '../../../../_common/route/route-component';
-import { CreateElement } from 'vue';
 import { Component } from 'vue-property-decorator';
+import { BaseRouteComponent } from '../../../../_common/route/route-component';
 
-const template: string = require('../../../../lib/terms/privacy/global.md');
+const templateMd: string = require('../../../../lib/terms/privacy/global.md');
 
 @Component({
 	name: 'RouteLegalPrivacy',
 })
 export default class RouteLegalPrivacy extends BaseRouteComponent {
+	readonly template = templateMd;
+
 	get routeTitle() {
 		return this.$gettext('Privacy Policy');
 	}
 
-	render(h: CreateElement) {
-		return h('div', { domProps: { innerHTML: template } });
+	playwireSetCookie(cname: string, cvalue: string, exdays: number) {
+		const d = new Date();
+		d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+		const expires = 'expires=' + d.toUTCString();
+		document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+	}
+
+	playwireOptOut() {
+		this.playwireSetCookie('_pubcid_optout', '1', 1825);
+
+		// TODO: should probably show some confirmation, yeah?
+		// At this point they should be opted out.
+	}
+
+	playwireShowConsentTool() {
+		(window as any).__cmp('showConsentTool');
 	}
 }
---
+++ src/app/views/legal/privacy/privacy.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<div v-html="template"></div>
+
+		<h3>Common ID Cookie</h3>
+		<p>
+			This site uses cookies and similar tracking technologies such as the Common ID cookie to
+			provide its services. Cookies are important devices for measuring advertising effectiveness
+			and ensuring a robust online advertising industry. The Common ID cookie stores a unique user
+			id in the first party domain and is accessible to our ad partners. This simple ID that can be
+			utilized to improve user matching, especially for delivering ads to iOS and MacOS browsers.
+			Users can opt out of the Common ID tracking cookie by clicking
+			<a @click="playwireOptOut" href="#opt-out">here</a>
+			.
+		</p>
+
+		<h3>Advertising Privacy Settings</h3>
+		<p>
+			When you use our site, pre-selected companies may access and use certain information on your
+			device and about your interests to serve ads or personalized content. You may revisit or
+			change consent-choices at any time by clicking
+			<a href="#cmp" @click="playwireShowConsentTool">here</a>
+			.
+		</p>
+	</div>
+</template>
+
+<script lang="ts" src="./privacy"></script>
