00626d7eda8327a7761f537eb568ec1a09bdf6fa
Hapurubokka <somekid11@gmail.com>
2020-02-29 18:30:39 -0500

    scrollbar stuff

--- src/_common/backdrop/backdrop.service.ts
+++ src/_common/backdrop/backdrop.service.ts
@@ -12,6 +12,7 @@ export class Backdrop {
 
 	static push(options: BackdropOptions = {}) {
 		const el = document.createElement('div');
+		const scrollbarWidth = window.innerWidth - document.body.clientWidth;
 
 		if (!options.context) {
 			document.body.appendChild(el);
@@ -30,17 +31,25 @@ export class Backdrop {
 		this.backdrops.push(backdrop);
 		document.body.classList.add('backdrop-active');
 
+		document.body.style.marginRight = scrollbarWidth + 'px';
+		// setting the max-width to the difference in width from scrollbar
+		document.getElementById('shell-top-nav').style.maxWidth =
+			window.innerWidth - scrollbarWidth + 'px';
+
 		return backdrop;
 	}
 
 	static checkBackdrops() {
 		const active = this.backdrops.filter(i => i.active);
 		if (active.length === 0) {
+			document.body.style.marginRight = '';
 			document.body.classList.remove('backdrop-active');
 		}
 	}
 
 	static remove(backdrop: AppBackdrop) {
+		// setting max-width to 100vw here will make sure it's set at the proper time
+		document.getElementById('shell-top-nav').style.maxWidth = '100vw';
 		backdrop.$destroy();
 		backdrop.$el.parentNode!.removeChild(backdrop.$el);
 		arrayRemove(this.backdrops, i => i === backdrop);
--- src/_common/modal/modals.ts
+++ src/_common/modal/modals.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import { Component, Watch } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
 import { AppModalWrapper } from './modal-wrapper';
 import { Modal } from './modal.service';
 import './modals-global.styl';
@@ -16,18 +16,20 @@ export default class AppModals extends Vue {
 		return Modal.modals;
 	}
 
-	@Watch('activeModals.length')
-	watchModalLength() {
-		// We only count modals that have backdrops. If all the modals don't
-		// have backdrops, then we don't add `modal-open`.
-		const backdropModals = Modal.modals.filter(item => !item.noBackdrop);
+	// Below is a duplicate scroll-disabling backdrop. Breaks the stuff in 'backdrop.service.ts' for some reason.
 
-		if (!GJ_IS_SSR) {
-			if (backdropModals.length) {
-				document.body.classList.add('modal-open');
-			} else {
-				document.body.classList.remove('modal-open');
-			}
-		}
-	}
+	// @Watch('activeModals.length')
+	// watchModalLength() {
+	// 	// We only count modals that have backdrops. If all the modals don't
+	// 	// have backdrops, then we don't add `modal-open`.
+	// 	const backdropModals = Modal.modals.filter(item => !item.noBackdrop);
+
+	// 	if (!GJ_IS_SSR) {
+	// 		if (backdropModals.length) {
+	// 			document.body.classList.add('modal-open');
+	// 		} else {
+	// 			document.body.classList.remove('modal-open');
+	// 		}
+	// 	}
+	// }
 }
