433cdc029e77bb2898d01067b83e630777af316c
nilllzz <nidresch@googlemail.com>
2020-03-06 21:59:06 +0100

    Update game media modal (#434)

--- src/app/components/forms/game/video/video.ts
+++ src/app/components/forms/game/video/video.ts
@@ -1,14 +1,17 @@
+import { Component, Prop, Watch } from 'vue-property-decorator';
 import { BaseForm, FormOnInit } from '../../../../../_common/form-vue/form.service';
 import { Game } from '../../../../../_common/game/game.model';
 import { GameVideo } from '../../../../../_common/game/video/video.model';
-import { Component, Prop, Watch } from 'vue-property-decorator';
+import AppVideoEmbed from '../../../../../_common/video/embed/embed.vue';
 
 type FormModel = GameVideo & {
 	_url: string;
 };
 
 @Component({
-	components: {},
+	components: {
+		AppVideoEmbed,
+	},
 })
 export default class FormGameVideo extends BaseForm<FormModel> implements FormOnInit {
 	@Prop(Game) game!: Game;
@@ -18,6 +21,24 @@ export default class FormGameVideo extends BaseForm<FormModel> implements FormOn
 	warnOnDiscard = false;
 
 	GameVideo = GameVideo;
+
+	get hasValidVideoUrl() {
+		return !!this.videoData;
+	}
+
+	get videoData() {
+		const url = this.formModel._url;
+		if (url) {
+			const youtubeMatch = url.match(GameVideo.REGEX.YOUTUBE);
+			const vimeoMatch = url.match(GameVideo.REGEX.VIMEO);
+
+			if (youtubeMatch) {
+				return { id: youtubeMatch[4], type: GameVideo.TYPE_YOUTUBE };
+			} else if (vimeoMatch) {
+				return { id: vimeoMatch[4], type: GameVideo.TYPE_VIMEO };
+			}
+		}
+	}
 
 	onInit() {
 		this.setField('game_id', this.game.id);
@@ -37,15 +58,10 @@ export default class FormGameVideo extends BaseForm<FormModel> implements FormOn
 		// Check if we need to scrub out anything from the URL.
 		// Will be the case if they entered in a full URL such as http://www.youtube.com/watch?v=something, etc.
 		if (url) {
-			const youtubeMatch = url.match(GameVideo.REGEX.YOUTUBE);
-			const vimeoMatch = url.match(GameVideo.REGEX.VIMEO);
-
-			if (youtubeMatch) {
-				this.setField('type', GameVideo.TYPE_YOUTUBE);
-				this.setField('url', youtubeMatch[4]);
-			} else if (vimeoMatch) {
-				this.setField('type', GameVideo.TYPE_VIMEO);
-				this.setField('url', vimeoMatch[4]);
+			const videoData = this.videoData;
+			if (!!videoData) {
+				this.setField('type', videoData!.type);
+				this.setField('url', videoData!.id);
 			}
 		}
 	}
