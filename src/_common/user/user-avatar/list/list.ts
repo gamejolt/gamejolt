aeef1d5dd27bcd6983daab87c9cdf78bf085b457
Hapurubokka <somekid11@gmail.com>
2020-03-17 20:06:23 -0400

    removed optional props to change sizing and flex

--- src/_common/user/user-avatar/list/list.ts
+++ src/_common/user/user-avatar/list/list.ts
@@ -18,10 +18,4 @@ import AppUserAvatar from '../user-avatar.vue';
 export default class AppUserAvatarList extends Vue {
 	@Prop(Array)
 	users!: User[];
-
-	@Prop(Boolean)
-	sm?: boolean;
-
-	@Prop(Boolean)
-	inline?: boolean;
 }
