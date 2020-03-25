c1b232803fa4a58cfcffb0ba94ee1eefeb42102e
Hapurubokka <somekid11@gmail.com>
2020-03-25 14:17:56 -0400

    replaced AppExpand with vue transition

--- src/app/components/event-item/controls/user-follow/user-follow.ts
+++ src/app/components/event-item/controls/user-follow/user-follow.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import { Component, Emit, Prop } from 'vue-property-decorator';
 import { propOptional, propRequired } from '../../../../../utils/vue';
-import AppExpand from '../../../../../_common/expand/expand.vue';
 import { FiresidePost } from '../../../../../_common/fireside/post/post-model';
 import { Screen } from '../../../../../_common/screen/screen-service';
 import AppUserFollowWidget from '../../../../../_common/user/follow/widget.vue';
@@ -9,7 +8,6 @@ import AppUserFollowWidget from '../../../../../_common/user/follow/widget.vue';
 @Component({
 	components: {
 		AppUserFollowWidget,
-		AppExpand,
 	},
 })
 export default class AppEventItemControlsUserFollow extends Vue {
