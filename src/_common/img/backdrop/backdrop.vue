2db88817de35bb655c59264e17b5f293c9665916
Hapurubokka <somekid11@gmail.com>
2020-03-29 17:01:36 -0400

    revisions

--- src/_common/img/backdrop/backdrop.vue
+++ src/_common/img/backdrop/backdrop.vue
@@ -1,23 +1,20 @@
 <template>
-	<div class="-app-img-backdrop">
-		<div class="-bg-color-styling" :style="wrapperStyling" />
+	<div class="img-backdrop" :style="wrapperStyling">
 		<slot />
 	</div>
 </template>
 
 <style lang="stylus" scoped>
-.-app-img-backdrop
+@require '~styles/variables'
+@require '~styles-lib/mixins'
+
+.img-backdrop
+	rounded-corners-lg()
+	overflow: hidden
 	position: relative
 	width: inherit
 	height: 100%
-
-	img
-		position: relative
-
-.-bg-color-styling
-	position: absolute
-	width: 100%
-	height: 100%
+	overflow: hidden
 	background-clip: content-box
 </style>
 
