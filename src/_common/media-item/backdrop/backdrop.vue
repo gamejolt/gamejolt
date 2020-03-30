243011318f837d5c42c94eb277788c425bf67e7e
Hapurubokka <somekid11@gmail.com>
2020-03-30 16:55:01 -0400

    moved location, changed styling for better borders

---
+++ src/_common/media-item/backdrop/backdrop.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="img-backdrop" :class="radiusClass">
+		<div class="-color" :class="radiusClass" :style="wrapperStyling" />
+		<slot />
+	</div>
+</template>
+
+<style lang="stylus" scoped>
+@require '~styles/variables'
+@require '~styles-lib/mixins'
+
+
+.img-backdrop
+	display: flex
+	overflow: hidden
+	width: inherit
+	height: 100%
+	position: relative
+
+	> *
+		position: relative
+
+.-color
+	position: absolute
+	top: 1px
+	right: 1px
+	bottom: 1px
+	left: 1px
+	width: auto
+	background-color: white !important
+
+	.spoiler &
+		opacity: 0
+
+.-sm
+	rounded-corners-sm()
+
+.-md
+	rounded-corners()
+
+.-lg
+	rounded-corners-lg()
+
+.-full
+	border-radius: 50%
+
+</style>
+
+<script lang="ts" src="./backdrop"></script>
