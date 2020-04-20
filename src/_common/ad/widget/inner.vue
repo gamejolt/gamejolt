46e07b0b9eaaadcb999e5989280a09657a6ccff7
David DeCarmine <hworld@holo-world.com>
2020-04-20 15:37:05 -0700

    Fix ad styling.
    It had wrapped the ads in flex, but didn't set the inner content to take up the full width, so then video ads didn't render properly.

--- src/_common/ad/widget/inner.vue
+++ src/_common/ad/widget/inner.vue
@@ -1,6 +1,6 @@
 <template>
 	<!-- We completely regenerate it when the slot ID changes -->
-	<div v-if="slotId" :key="slotId">
+	<div v-if="slotId" :key="slotId" class="ad-widget-inner">
 		<!-- Load the ad component for the currently running ad adapter -->
 		<component :is="adapter.component(adSlot)" :ad-slot="adSlot" :adapter="adapter" />
 	</div>
--- src/_common/ad/widget/widget.vue
+++ src/_common/ad/widget/widget.vue
@@ -1,13 +1,13 @@
 <template>
 	<div
 		v-if="shouldShow"
-		class="-container"
+		class="ad-widget"
 		:class="{
 			'-size-leaderboard': adSlot.size === 'leaderboard',
 			'-size-rectangle': adSlot.size === 'rectangle',
 		}"
 	>
-		<div class="-inner">
+		<div class="-content">
 			<app-ad-widget-inner :ad-slot="adSlot" />
 		</div>
 	</div>
@@ -17,23 +17,27 @@
 @require '~styles/variables'
 @require '~styles-lib/mixins'
 
-.-container
+.ad-widget
 	text-align: center
 
-.-inner
-	display: flex
-	align-items: center
-	justify-content: center
-	margin: 0 auto
+	.-content
+		display: flex
+		align-items: center
+		justify-content: center
+		margin: 0 auto
 
-// We reserve some extra space for the "report ad" link below the ad itself.
-.-size-leaderboard
-	.-inner
-		min-height: 115px
+		// Make sure the ad is able to take up the full width.
+		> .ad-widget-inner
+			flex: auto
 
-.-size-rectangle
-	.-inner
-		min-height: 275px
+	// We reserve some extra space for the "report ad" link below the ad itself.
+	&.-size-leaderboard
+		.-content
+			min-height: 115px
+
+	&.-size-rectangle
+		.-content
+			min-height: 275px
 </style>
 
 <script lang="ts" src="./widget"></script>
