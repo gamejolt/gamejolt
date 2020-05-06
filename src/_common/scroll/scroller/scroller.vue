c9797f7ebd4b7feb0f9f4737c2d98bc72bf81534
Hapurubokka <somekid11@gmail.com>
2020-05-06 19:55:50 -0400

    was accidentally overriding mobile scrollbars

--- src/_common/scroll/scroller/scroller.vue
+++ src/_common/scroll/scroller/scroller.vue
@@ -27,45 +27,48 @@ $-modal-track = var(--theme-bg)
 
 .scroll-scroller
 	scrollable()
-	// Firefox
-	scrollbar-color: $-default-thumb $-default-track
-
-	// Other browsers
-	&::-webkit-scrollbar
-		background-color: $-default-track
-
-		&-thumb
-			background-color: $-default-thumb
 
 	&.-horizontal
 		scrollable-x()
 
-	&.-thin
-		scrollbar-width: thin
-
-		&::-webkit-scrollbar
-			width: $-scrollbar-size
-			height: $-scrollbar-size
-
-			&-thumb
-				border-radius: $-thumb-radius
-
 	&.-hide-scrollbar
+		// Firefox
 		scrollbar-width: none
 
+		// Other browsers
 		&::-webkit-scrollbar
 			display: none
 
-	// Override colors so transparency doesn't look weird
-	// with body background or others in full-screen modals.
-	&.modal
-		scrollbar-color: $-modal-thumb $-modal-track
+	/* mouse, touch pad, and stylus-based screens */
+	@media (pointer: fine)
+		scrollbar-color: $-default-thumb $-default-track
 
 		&::-webkit-scrollbar
-			background-color: $-modal-track
+			background-color: $-default-track
 
 			&-thumb
-				background-color: $-modal-thumb
+				background-color: $-default-thumb
+
+		&.-thin
+			scrollbar-width: thin
+
+			&::-webkit-scrollbar
+				width: $-scrollbar-size
+				height: $-scrollbar-size
+
+				&-thumb
+					border-radius: $-thumb-radius
+
+		// Override colors so transparency doesn't look weird
+		// with body background or others in full-screen modals.
+		&.modal
+			scrollbar-color: $-modal-thumb $-modal-track
+
+			&::-webkit-scrollbar
+				background-color: $-modal-track
+
+				&-thumb
+					background-color: $-modal-thumb
 </style>
 
 <script lang="ts" src="./scroller"></script>
