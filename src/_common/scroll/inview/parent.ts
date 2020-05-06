e62dae59c825ee722367298080963596ce482c61
Hapurubokka <somekid11@gmail.com>
2020-05-06 13:07:51 -0400

    better typing for AppScrollInviewParent

--- src/_common/scroll/inview/parent.ts
+++ src/_common/scroll/inview/parent.ts
@@ -7,7 +7,7 @@ import { ScrollInviewContainer } from './container';
 export class AppScrollInviewParent extends Vue {
 	// If this is a child of AppScrollScroller, we need to get it as a prop so
 	// we can use that scroll element as the root context.
-	@Prop(propOptional(HTMLElement)) scroller?: HTMLElement;
+	@Prop(propOptional(HTMLElement, null)) scroller!: null | HTMLElement;
 
 	// We need to create a new container for each unique "margin" that our
 	// AppScrollInview children need. This is a map of margin => container.
@@ -22,7 +22,7 @@ export class AppScrollInviewParent extends Vue {
 	getContainer(margin: string) {
 		const container = this.containers.get(margin);
 		if (!container) {
-			const root = this.scroller || null;
+			const root = this.scroller;
 			const newContainer = new ScrollInviewContainer(root, margin);
 			this.containers.set(margin, newContainer);
 			return newContainer;
