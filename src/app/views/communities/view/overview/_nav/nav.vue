dabcb5c5f808dd369fe03b08503077498ef55801
Hapurubokka <somekid11@gmail.com>
2020-05-08 13:14:51 -0400

    add AppScrollScroller to communities card nav

--- src/app/views/communities/view/overview/_nav/nav.vue
+++ src/app/views/communities/view/overview/_nav/nav.vue
@@ -1,40 +1,42 @@
 <template>
 	<nav>
-		<app-community-channel-card
-			:community="community"
-			path="featured"
-			label="Featured"
-			:background-item="community.featured_background"
-			:is-active="activeChannelTitle === 'featured'"
-			:is-unread="isChannelUnread('featured')"
-		/>
-		<app-community-channel-card
-			:community="community"
-			path="all"
-			label="All Posts"
-			:background-item="community.all_background"
-			sort="hot"
-			:is-active="activeChannelTitle === 'all'"
-			:is-unread="isChannelUnread('all')"
-		/>
-
-		<h5 v-if="Screen.isDesktop" class="-channels-header">
-			<translate>Channels</translate>
-		</h5>
-
-		<template v-if="community.channels">
+		<app-scroll-scroller :horizontal="Screen.isMobile" thin>
 			<app-community-channel-card
-				v-for="channel of community.channels"
-				:key="channel.id"
 				:community="community"
-				:path="channel.title"
-				:label="channel.title"
-				:background-item="channel.background"
-				:is-active="activeChannelTitle === channel.title"
-				:is-unread="isChannelUnread(channel.title)"
-				:is-locked="isChannelLocked(channel)"
+				path="featured"
+				label="Featured"
+				:background-item="community.featured_background"
+				:is-active="activeChannelTitle === 'featured'"
+				:is-unread="isChannelUnread('featured')"
 			/>
-		</template>
+			<app-community-channel-card
+				:community="community"
+				path="all"
+				label="All Posts"
+				:background-item="community.all_background"
+				sort="hot"
+				:is-active="activeChannelTitle === 'all'"
+				:is-unread="isChannelUnread('all')"
+			/>
+
+			<h5 v-if="Screen.isDesktop" class="-channels-header">
+				<translate>Channels</translate>
+			</h5>
+
+			<template v-if="community.channels">
+				<app-community-channel-card
+					v-for="channel of community.channels"
+					:key="channel.id"
+					:community="community"
+					:path="channel.title"
+					:label="channel.title"
+					:background-item="channel.background"
+					:is-active="activeChannelTitle === channel.title"
+					:is-unread="isChannelUnread(channel.title)"
+					:is-locked="isChannelLocked(channel)"
+				/>
+			</template>
+		</app-scroll-scroller>
 	</nav>
 </template>
 
