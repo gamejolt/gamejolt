f4f79d6d0d98eded456f8b363f121f2c1e0057c4
David DeCarmine <hworld@holo-world.com>
2020-05-09 12:39:56 -0700

    Fix scroll-inview-parent to not check concrete type of HTMLElement.
    This should fix SSR.

--- src/_common/scroll/inview/parent.ts
+++ src/_common/scroll/inview/parent.ts
@@ -7,7 +7,7 @@ import { ScrollInviewContainer } from './container';
 export class AppScrollInviewParent extends Vue {
 	// If this is a child of AppScrollScroller, we need to get it as a prop so
 	// we can use that scroll element as the root context.
-	@Prop(propOptional(HTMLElement, null)) scroller!: null | HTMLElement;
+	@Prop(propOptional(Object, null)) scroller!: null | HTMLElement;
 
 	// We need to create a new container for each unique "margin" that our
 	// AppScrollInview children need. This is a map of margin => container.
