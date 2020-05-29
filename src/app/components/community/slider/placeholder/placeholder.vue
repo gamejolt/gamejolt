a33091685a9315624ff3f718c459fb17e5375b4d
Hapurubokka <somekid11@gmail.com>
2020-05-29 19:00:21 -0400

    add display: flex to collapse whitespace

--- src/app/components/community/slider/placeholder/placeholder.vue
+++ src/app/components/community/slider/placeholder/placeholder.vue
@@ -1,18 +1,20 @@
 <template>
 	<div class="-list">
 		<app-scroll-scroller class="-scroller" thin horizontal>
-			<template v-for="n in num">
-				<div class="-container" :key="n">
-					<div class="-item" />
-					<div class="-text" />
-				</div>
-			</template>
+			<div class="-flex">
+				<template v-for="n in num">
+					<div class="-container" :key="n">
+						<div class="-item" />
+						<div class="-text" />
+					</div>
+				</template>
 
-			<div v-if="withAddButton" class="-container">
-				<div class="-item" />
-			</div>
-			<div class="-container">
-				<div class="-item -last" />
+				<div v-if="withAddButton" class="-container">
+					<div class="-item" />
+				</div>
+				<div class="-container">
+					<div class="-item -last" />
+				</div>
 			</div>
 		</app-scroll-scroller>
 	</div>
--- src/app/components/community/slider/slider.vue
+++ src/app/components/community/slider/slider.vue
@@ -1,15 +1,17 @@
 <template>
 	<div class="-list" v-if="communities.length || showAddButton">
 		<app-scroll-scroller class="-scroller" horizontal thin>
-			<app-community-slider-item
-				v-for="community of communities"
-				:key="community.id"
-				:community="community"
-				:event-cat="eventCat"
-			/>
+			<div class="-flex">
+				<app-community-slider-item
+					v-for="community of communities"
+					:key="community.id"
+					:community="community"
+					:event-cat="eventCat"
+				/>
 
-			<app-community-slider-discover-item key="discover" />
-			<app-community-slider-add-item key="add" v-if="showAddButton" />
+				<app-community-slider-discover-item key="discover" />
+				<app-community-slider-add-item key="add" v-if="showAddButton" />
+			</div>
 		</app-scroll-scroller>
 	</div>
 </template>
