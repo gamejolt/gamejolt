0e4fa4d3855a1742422bc6d2b0949dc729cdd18e
Hapurubokka <somekid11@gmail.com>
2020-06-04 11:35:43 -0400

    lots of changes for how popper is used

--- src/app/components/forms/post/_community-pill/selector/selector.ts
+++ src/app/components/forms/post/_community-pill/selector/selector.ts
@@ -5,6 +5,7 @@ import { CommunityChannel } from '../../../../../../_common/community/channel/ch
 import { Community } from '../../../../../../_common/community/community.model';
 import AppCommunityThumbnailImg from '../../../../../../_common/community/thumbnail/img/img.vue';
 import AppCommunityVerifiedTick from '../../../../../../_common/community/verified-tick/verified-tick.vue';
+import AppJolticon from '../../../../../../_common/jolticon/jolticon.vue';
 import { Popper } from '../../../../../../_common/popper/popper.service';
 import AppPopper from '../../../../../../_common/popper/popper.vue';
 
@@ -13,6 +14,7 @@ import AppPopper from '../../../../../../_common/popper/popper.vue';
 		AppPopper,
 		AppCommunityThumbnailImg,
 		AppCommunityVerifiedTick,
+		AppJolticon,
 	},
 })
 export default class AppFormPostCommunityPillSelector extends Vue {
@@ -22,6 +24,12 @@ export default class AppFormPostCommunityPillSelector extends Vue {
 	@Prop(propOptional(Community, null))
 	initialCommunity!: Community | null;
 
+	$refs!: {
+		header: HTMLElement;
+	};
+
+	// State for what page to be on
+	selectingChannel = true;
 	selectedCommunity: Community | null = null;
 
 	@Emit('select-community') emitSelectCommunity(_community: Community) {}
@@ -32,13 +40,42 @@ export default class AppFormPostCommunityPillSelector extends Vue {
 		return this.selectedCommunity?.postableChannels;
 	}
 
+	get isInitial() {
+		return this.selectedCommunity === this.initialCommunity;
+	}
+
 	created() {
 		this.resetSelections();
 	}
 
+	async scrollToHeader() {
+		await this.$nextTick();
+		// const parent = findVueParent(this, AppScrollScroller);
+		// parent?.$el.scrollTo({
+		// 	top: -500,
+		// 	behavior: 'smooth',
+		// });
+		this.$refs.header.scrollIntoView(
+			false
+			// 	{
+			// 	behavior: 'smooth',
+			// }
+		);
+	}
+
+	unselectCommunity() {
+		if (this.selectedCommunity === this.initialCommunity) {
+			return;
+		}
+
+		this.selectingChannel = false;
+	}
+
 	selectCommunity(community: Community) {
+		this.selectingChannel = true;
 		this.selectedCommunity = community;
 		this.emitSelectCommunity(community);
+		this.scrollToHeader();
 	}
 
 	selectChannel(channel: CommunityChannel) {
--- src/app/components/forms/post/_community-pill/selector/selector.vue
+++ src/app/components/forms/post/_community-pill/selector/selector.vue
@@ -1,40 +1,62 @@
 <template>
-	<app-popper hide-on-state-change @hide="resetSelections">
+	<app-popper popover-class="fill-bg" hide-on-state-change @hide="resetSelections">
 		<slot />
 
-		<div slot="popover">
-			<div class="-container">
-				<div class="-slider" :class="{ '-slide-to-channels': !!selectedCommunity }">
-					<div class="-communities list-group">
-						<a
-							class="-community-item list-group-item"
-							v-for="community of communities"
-							:key="community.id"
-							@click="selectCommunity(community)"
-						>
-							<app-community-thumbnail-img
-								slot="img"
-								class="-community-img"
-								:community="community"
-							/>
+		<div class="-container" slot="popover">
+			<div
+				class="-slider"
+				:class="{ '-slide-to-channels': !!selectedCommunity && selectingChannel }"
+			>
+				<div class="-communities list-group">
+					<a
+						class="-community-item list-group-item"
+						v-for="community of communities"
+						:key="community.id"
+						@click="selectCommunity(community)"
+					>
+						<app-community-thumbnail-img slot="img" class="-community-img" :community="community" />
 
+						<span class="-text">
 							{{ community.name }}
-							<app-community-verified-tick class="-tick" :community="community" small />
-						</a>
-					</div>
+						</span>
+						<app-community-verified-tick class="-tick" :community="community" small />
+					</a>
+				</div>
 
-					<div class="-channels list-group">
-						<template v-if="channels">
-							<a
-								class="-channel-item list-group-item"
-								v-for="channel of channels"
-								:key="channel.id"
-								@click="selectChannel(channel)"
-							>
+				<div v-if="selectedCommunity" class="-channels list-group">
+					<a
+						ref="header"
+						class="-community-item -selected list-group-item"
+						:class="{ '-initial': isInitial }"
+						@click="unselectCommunity"
+					>
+						<div slot="img" class="-community-img">
+							<app-community-thumbnail-img :community="selectedCommunity" />
+							<app-jolticon v-if="!isInitial" class="-back" icon="arrow-left" />
+						</div>
+						<span class="-text">
+							{{ selectedCommunity.name }}
+						</span>
+						<app-community-verified-tick class="-tick" :community="selectedCommunity" small />
+						<!-- <app-button
+							v-if="selectedCommunity !== initialCommunity"
+							class="-back"
+							icon="arrow-left"
+							@click="unselectCommunity"
+						/> -->
+					</a>
+					<template v-if="channels">
+						<a
+							class="-channel-item list-group-item"
+							v-for="channel of channels"
+							:key="channel.id"
+							@click="selectChannel(channel)"
+						>
+							<span class="-text">
 								{{ channel.title }}
-							</a>
-						</template>
-					</div>
+							</span>
+						</a>
+					</template>
 				</div>
 			</div>
 		</div>
