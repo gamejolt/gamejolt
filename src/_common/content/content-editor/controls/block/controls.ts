eb3f94c6d20757d9e341b85780fda1e667b3bb94
Nils <nidresch@googlemail.com>
2020-06-19 12:51:00 +0200

    fix backspace issue by referencing the same package version of prosemirror-state

--- src/_common/content/content-editor/controls/block/controls.ts
+++ src/_common/content/content-editor/controls/block/controls.ts
@@ -61,7 +61,7 @@ export default class AppContentEditorBlockControls extends Vue {
 					}
 				}
 
-				const box = this.$refs.container.offsetParent.getBoundingClientRect();
+				const box = this.$refs.container.offsetParent!.getBoundingClientRect();
 				this.top = start.top - box.top - 8 + 'px';
 				this.left = '-32px';
 
