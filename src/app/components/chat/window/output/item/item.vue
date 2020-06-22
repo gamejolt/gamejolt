9e81d008022a346aa1de0db87c4d7027693b69c9
Nils <nidresch@googlemail.com>
2020-06-22 10:30:14 +0200

    slight improvement to text styling for message split

--- src/app/components/chat/window/output/item/item.vue
+++ src/app/components/chat/window/output/item/item.vue
@@ -10,21 +10,16 @@
 			'-chat-message-new': isNew,
 		}"
 	>
-		<router-link
-			v-if="!message.combine"
-			class="chat-window-message-avatar"
-			:to="message.user.url"
-		>
+		<router-link v-if="!message.combine" class="chat-window-message-avatar" :to="message.user.url">
 			<img class="img-responsive" :src="message.user.img_avatar" alt="" />
 		</router-link>
 
 		<div class="chat-window-message-container">
 			<div class="chat-window-message-byline" v-if="!message.combine">
 				<router-link class="chat-window-message-user link-unstyled" :to="message.user.url">
-					{{ message.user.display_name }}
-				</router-link>
-				<span class="chat-window-message-username">@{{ message.user.username }}</span>
-				<span class="chat-window-message-time">
+					{{ message.user.display_name }} </router-link
+				><span class="chat-window-message-username"> @{{ message.user.username }}</span
+				><span class="chat-window-message-time">
 					<span v-app-tooltip="loggedOn">
 						{{ message.logged_on | date('shortTime') }}
 					</span>
