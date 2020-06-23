fd7658e18caeef360f655bc8485ce66dc45c0e88
Nils <nidresch@googlemail.com>
2020-06-23 23:44:12 +0200

    update notification growl and system message

--- src/app/components/chat/notification-growl/notification-growl.vue
+++ src/app/components/chat/notification-growl/notification-growl.vue
@@ -1,17 +1,26 @@
 <template>
 	<div class="-container">
 		<app-user-avatar class="-avatar" :user="message.user" />
-		<div>
-			<h4 class="-header">{{ message.user.display_name }}</h4>
+		<div class="-content">
+			<h4 class="-header">
+				<app-jolticon icon="user-messages" />
+				{{ message.user.display_name }}
+			</h4>
 
 			<app-fade-collapse :collapse-height="100">
 				<app-content-viewer :source="message.content" />
 			</app-fade-collapse>
+
+			<!-- Used to block any interaction with the content inside the viewer. -->
+			<div class="-content-overlay"></div>
 		</div>
 	</div>
 </template>
 
 <style lang="stylus" scoped>
+@require '~styles/variables'
+@require '~styles-lib/mixins'
+
 .-container
 	display: flex
 
@@ -19,8 +28,23 @@
 	width: 44px
 	margin-right: 16px
 	flex-shrink: 0
-	// To visually align with the top of the h4
-	margin-top: 6px
+
+.-content
+	position: relative
+	min-width: 0
+	width: 100%
+
+	&-overlay
+		position: absolute
+		top: 0
+		left: 0
+		right: 0
+		bottom: 0
+
+.-header
+	text-overflow()
+	margin-bottom: 8px
+
 </style>
 
 <script lang="ts" src="./notification-growl"></script>
