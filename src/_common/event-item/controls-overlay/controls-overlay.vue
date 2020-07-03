d2562261459b2d76b52ff007b80d0d76221d67d3
hapurubokka <somekid11@gmail.com>
2020-07-03 12:27:36 -0400

    Fix EventItemControlsOverlay padding issues (#567)

--- src/_common/event-item/controls-overlay/controls-overlay.vue
+++ src/_common/event-item/controls-overlay/controls-overlay.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="-container">
+	<div class="event-item-controls-overlay">
 		<div class="-overlay" :class="{ '-end': end }" />
 		<slot />
 	</div>
@@ -11,28 +11,28 @@
 @require '~styles/variables'
 @require '~styles-lib/mixins'
 
-.-container
+.event-item-controls-overlay
 	position: relative
 	// Needs to be higher than the z-index of AppStickerTarget
 	z-index: 1
 
-.-overlay
-	change-bg('bg')
-	position: absolute
-	top: -4px
-	right: 0
-	bottom: 4px
-	left: 0
-	opacity: 0.9
-	z-index: -1
-	// We want to expand the overlay out to the edges of the post container.
-	margin-left: -($grid-gutter-width-xs / 2)
-	margin-right: -($grid-gutter-width-xs / 2)
+	.-overlay
+		change-bg('bg')
+		position: absolute
+		top: -4px
+		right: 0
+		bottom: 4px
+		left: 0
+		opacity: 0.9
+		z-index: -1
+		// We want to expand the overlay out to the edges of the post container.
+		margin-left: -($grid-gutter-width-xs / 2)
+		margin-right: -($grid-gutter-width-xs / 2)
 
-	@media $media-sm-up
-		margin-left: -($grid-gutter-width / 2) + $border-width-base + 0.5px
-		margin-right: -($grid-gutter-width / 2) + $border-width-base + 0.5px
+		@media $media-sm-up
+			margin-left: -($grid-gutter-width / 2) + $border-width-base + 0.5px
+			margin-right: -($grid-gutter-width / 2) + $border-width-base + 0.5px
 
-	&.-end
-		bottom: -4px
+		&.-end
+			bottom: -4px
 </style>
