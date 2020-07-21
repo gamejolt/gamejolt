b57a5e7da64b13e6c04093544c4cd7ab2df154f3
David DeCarmine <hworld@holo-world.com>
2020-07-21 10:59:30 -0700

    Integrate eslint with TS and vue file support.

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		parser: '@typescript-eslint/parser',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:vue/vue3-recommended',
+	],
+	rules: {
+		'no-empty': ['error', { allowEmptyCatch: true }],
+		'no-undef': 'off',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-unsafe-assignment': 'off',
+		'@typescript-eslint/no-unsafe-call': 'off',
+		'@typescript-eslint/no-unsafe-member-access': 'off',
+		'@typescript-eslint/no-unsafe-return': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+		'vue/html-indent': ['error', 'tab'],
+		'vue/html-self-closing': [
+			'warn',
+			{
+				html: {
+					void: 'always',
+					normal: 'always',
+					component: 'always',
+				},
+				svg: 'always',
+				math: 'always',
+			},
+		],
+		'vue/max-attributes-per-line': 'off',
+		'vue/singleline-html-element-content-newline': 'off',
+	},
+};
