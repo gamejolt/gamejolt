993598a69471d402d7f33bcb6f99ae8ab6bac8cf
Hapurubokka <somekid11@gmail.com>
2020-07-28 12:23:40 -0400

    fix race condition with channel changes and context pane

--- src/app/components/community/channel/card/card.vue
+++ src/app/components/community/channel/card/card.vue
@@ -1,44 +1,47 @@
 <template>
-	<router-link
-		v-app-observe-dimensions="updateCardHeight"
-		class="community-channel-card sheet sheet-no-full-bleed sheet-full"
-		:class="{ '-active': isActive, 'theme-dark': backgroundItem }"
-		:to="linkTo"
-		:title="label"
-		:style="{ height }"
-		@click.native.capture="onChannelClick"
-	>
-		<div v-if="backgroundItem" class="-card-bg">
-			<app-media-item-backdrop :media-item="backgroundItem">
-				<div
-					class="-card-bg-img"
-					:style="{
-						'background-image': `url('${backgroundItem.mediaserver_url}')`,
-					}"
-				/>
-				<div class="-overlay" />
-			</app-media-item-backdrop>
-		</div>
-
-		<div class="-card-content">
-			<div class="-card-content-title">
-				<app-jolticon
-					v-if="isLocked"
-					v-app-tooltip.left="
-						$gettext(`You do not have permissions to post to this channel.`)
-					"
-					icon="lock"
-				/>
-				{{ label }}
+	<div @click.capture="onChannelClick">
+		<router-link
+			v-app-observe-dimensions="updateCardHeight"
+			class="community-channel-card sheet sheet-no-full-bleed sheet-full"
+			:class="{ '-active': isActive, 'theme-dark': backgroundItem }"
+			:to="linkTo"
+			:title="label"
+			:style="{ height }"
+		>
+			<div v-if="backgroundItem" class="-card-bg">
+				<app-media-item-backdrop :media-item="backgroundItem">
+					<div
+						class="-card-bg-img"
+						:style="{
+							'background-image': `url('${backgroundItem.mediaserver_url}')`,
+						}"
+					/>
+					<div class="-overlay" />
+				</app-media-item-backdrop>
 			</div>
 
-			<div
-				v-if="isUnread"
-				v-app-tooltip="$gettext(`There are new posts since you last viewed this channel`)"
-				class="-card-content-unread"
-			/>
-		</div>
-	</router-link>
+			<div class="-card-content">
+				<div class="-card-content-title">
+					<app-jolticon
+						v-if="isLocked"
+						v-app-tooltip.left="
+							$gettext(`You do not have permissions to post to this channel.`)
+						"
+						icon="lock"
+					/>
+					{{ label }}
+				</div>
+
+				<div
+					v-if="isUnread"
+					v-app-tooltip="
+						$gettext(`There are new posts since you last viewed this channel`)
+					"
+					class="-card-content-unread"
+				/>
+			</div>
+		</router-link>
+	</div>
 </template>
 
 <script lang="ts" src="./card"></script>
