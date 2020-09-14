6d94b3679f0385986b404e6b21b0889a6e82953d
Hapurubokka <somekid11@gmail.com>
2020-09-13 20:07:53 -0400

    Revert "remove AppActivityFeedDevlogPostText"
    This reverts commit c7e7226d37829a7b981a49f8155d74b33b9d90ff.

---
+++ src/app/components/activity/feed/devlog-post/text/text.ts
@@ -0,0 +1,82 @@
+import AppContentViewer from '../../../../../../_common/content/content-viewer/content-viewer.vue';
+import AppFadeCollapse from '../../../../../../_common/fade-collapse/fade-collapse.vue';
+import { FiresidePost } from '../../../../../../_common/fireside/post/post-model';
+import { Screen } from '../../../../../../_common/screen/screen-service';
+import { Scroll } from '../../../../../../_common/scroll/scroll.service';
+import Vue from 'vue';
+import { Component, Inject, Prop } from 'vue-property-decorator';
+import { ActivityFeedItem } from '../../item-service';
+import { ActivityFeedView } from '../../view';
+
+@Component({
+	components: {
+		AppFadeCollapse,
+		AppContentViewer,
+	},
+})
+export default class AppActivityFeedDevlogPostText extends Vue {
+	@Inject()
+	feed!: ActivityFeedView;
+
+	@Prop(ActivityFeedItem)
+	item!: ActivityFeedItem;
+
+	@Prop(FiresidePost)
+	post!: FiresidePost;
+
+	isToggling = false;
+
+	$el!: HTMLDivElement;
+
+	get isHydrated() {
+		return this.feed.isItemHydrated(this.item);
+	}
+
+	get isOpen() {
+		return this.feed.isItemOpen(this.item);
+	}
+
+	async mounted() {
+		await this.$nextTick();
+		this.$emit('content-bootstrapped');
+	}
+
+	async toggleFull() {
+		if (this.isToggling) {
+			return;
+		}
+
+		this.isToggling = true;
+		this.$emit('expanded');
+
+		// If we collapsed.
+		if (!this.isOpen) {
+			this.expand();
+		} else {
+			this.collapse();
+		}
+	}
+
+	expand() {
+		this.feed.setItemOpen(this.item, true);
+		this.isToggling = false;
+	}
+
+	collapse() {
+		// We will scroll to the bottom of the element minus some extra padding.
+		// This keeps the element in view a bit.
+		const elementOffset = Scroll.getElementOffsetTopFromContext(this.$el);
+		const scrollTo = elementOffset - Screen.windowHeight * 0.25;
+
+		// Only if we're past where we would scroll.
+		if (Scroll.getScrollTop() > elementOffset) {
+			Scroll.to(scrollTo, { animate: false });
+		}
+
+		this.feed.setItemOpen(this.item, false);
+
+		setTimeout(() => {
+			this.isToggling = false;
+		}, 1000);
+	}
+}
---
+++ src/app/components/activity/feed/devlog-post/text/text.vue
@@ -0,0 +1,49 @@
+<template>
+	<div
+		class="devlog-post-text"
+		:class="{
+			'-hydrated': isHydrated,
+		}"
+	>
+		<template v-if="isOpen">
+			<div class="page-cut" />
+
+			<app-content-viewer
+				:source="post.article_content"
+				/>
+		</template>
+
+		<div class="page-cut">
+			<app-button
+				:trans="!isToggling"
+				:solid="isToggling"
+				:primary="isToggling"
+				@click.stop="toggleFull()"
+			>
+				<translate v-if="!isOpen">read article</translate>
+				<translate v-else>less</translate>
+			</app-button>
+		</div>
+	</div>
+</template>
+
+<style lang="stylus" scoped>
+@require '../../variables'
+@require '~styles-lib/mixins'
+
+.page-cut
+	@media $media-sm-up
+		margin-left: -($-item-padding-container)
+		margin-right: -($-item-padding-container)
+
+.devlog-post-text
+	// Hide images and widgets until we are hydrated.
+	>>> img, >>> iframe
+		visibility: hidden
+
+	&.-hydrated
+		>>> img, >>> iframe
+			visibility: visible
+</style>
+
+<script lang="ts" src="./text"></script>
