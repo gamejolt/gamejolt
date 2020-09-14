b1063e041d3164a66967debcc0198af9753e83b4
Hapurubokka <somekid11@gmail.com>
2020-09-13 21:11:11 -0400

    fix post view placeholder

--- src/app/views/post/_page-placeholder/page-placeholder.vue
+++ src/app/views/post/_page-placeholder/page-placeholder.vue
@@ -1,17 +1,44 @@
 <script lang="ts" src="./page-placeholder"></script>
 
 <template>
-	<section class="section">
-		<div class="container-xl" style="max-width: 650px">
-			<app-lazy-placeholder>
-				<div class="lazy-placeholder lazy-placeholder-h2" />
-				<div class="lazy-placeholder lazy-placeholder-h2" style="width: 40%" />
+	<section class="-section section-thin">
+		<div class="container-xl">
+			<div style="max-width: 650px; margin: 0 auto">
+				<div class="-user-info">
+					<div class="-avatar" />
+					<div class="-name">
+						<div class="-top lazy-placeholder" />
+						<div class="-bottom lazy-placeholder" />
+					</div>
+
+					<div class="-controls">
+						<div class="-follow lazy-placeholder" />
+					</div>
+				</div>
 
 				<span class="lazy-placeholder" />
 				<span class="lazy-placeholder" />
 				<span class="lazy-placeholder" />
 				<span class="lazy-placeholder" style="width: 40%" />
-			</app-lazy-placeholder>
+			</div>
 		</div>
 	</section>
 </template>
+
+<style lang="stylus" scoped>
+@import '../_page/page'
+
+.-top
+	width: 80px
+	height: $font-size-small
+	margin-bottom: 4px
+
+.-bottom
+	width: 64px
+	height: $font-size-tiny
+
+.-follow
+	width: 88px
+	height: 36px
+	margin-bottom: 0
+</style>
