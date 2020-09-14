cd1c915b84ec4c625d2ec814043b2211e4f8a2a9
Hapurubokka <somekid11@gmail.com>
2020-09-13 20:28:19 -0400

    change route path from '/post/:slug' to '/p/:slug'

--- src/app/views/post/post.route.ts
+++ src/app/views/post/post.route.ts
@@ -2,6 +2,6 @@ import { RouteConfig } from 'vue-router';
 
 export const routePostView: RouteConfig = {
 	name: 'post',
-	path: '/post/:slug',
+	path: '/p/:slug',
 	component: () => import(/* webpackChunkName: "routePost" */ './post.vue'),
 };
