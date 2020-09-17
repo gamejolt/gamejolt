f3fe5542feae34213b0379ebc8b04417f8f86682
Hapurubokka <somekid11@gmail.com>
2020-09-17 15:20:57 -0400

    fix styling alignment issues

--- src/app/components/activity/feed/devlog-post/text/text.vue
+++ src/app/components/activity/feed/devlog-post/text/text.vue
@@ -8,12 +8,14 @@
 		}"
 	>
 		<template v-if="isOpen">
-			<div class="page-cut" />
+			<div class="page-cut">
+				<div class="page-cut-content-placeholder" />
+			</div>
 
 			<app-content-viewer :source="post.article_content" />
 		</template>
 
-		<div class="page-cut">
+		<div class="page-cut page-cut-bottom">
 			<div class="page-cut-content">
 				<app-loading
 					v-if="isLoading"
@@ -41,13 +43,20 @@
 		margin-left: -($-item-padding-container)
 		margin-right: -($-item-padding-container)
 
+	&-bottom
+		margin-bottom: ($line-height-computed / 2)
+
 .page-cut-content
 	display: inline-block
-	height: $button-md-line-height
 	overflow: hidden
+
+	&
+	&-placeholder
+		height: $button-md-line-height
 
 .-loading
 	margin: 0
+	padding: 0 ($grid-gutter-width-xs / 2)
 
 .devlog-post-text
 	// Hide images and widgets until we are hydrated.
