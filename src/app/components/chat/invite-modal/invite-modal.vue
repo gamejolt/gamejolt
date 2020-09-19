afd92b17ba15ace344dc25e1da8467b9b9f64086
Hapurubokka <somekid11@gmail.com>
2020-09-18 21:42:32 -0400

    invite button as a modal footer, change radio colors

--- src/app/components/chat/invite-modal/invite-modal.vue
+++ src/app/components/chat/invite-modal/invite-modal.vue
@@ -1,57 +1,67 @@
 <script lang="ts" src="./invite-modal"></script>
 <template>
 	<app-modal>
-		<div class="modal-controls">
-			<app-button @click="modal.dismiss()">
-				<translate>Close</translate>
-			</app-button>
-		</div>
+		<template #default>
+			<div class="modal-controls">
+				<app-button @click="modal.dismiss()">
+					<translate>Close</translate>
+				</app-button>
+			</div>
 
-		<div class="modal-header">
-			<h2 class="modal-title">
-				<translate>Invite Friends</translate>
-			</h2>
-		</div>
+			<div class="modal-header">
+				<h2 class="modal-title">
+					<translate>Invite Friends</translate>
+				</h2>
+			</div>
 
-		<div class="modal-body">
-			<div class="friend-select-widget">
-				<div class="nav-controls">
-					<input
-						v-model="filterQuery"
-						text="search"
-						class="form-control"
-						placeholder="Filter..."
-					/>
-				</div>
+			<div class="modal-body">
+				<div class="friend-select-widget">
+					<div class="nav-controls">
+						<input
+							v-model="filterQuery"
+							text="search"
+							class="form-control"
+							placeholder="Filter..."
+						/>
+					</div>
 
-				<app-scroll-scroller>
-					<div class="user-list">
-						<div v-for="user of filteredUsers" :key="user.id" class="user-list-item">
-							<div class="-avatar">
-								<app-user-avatar-img :user="user" />
-							</div>
-
-							<div class="-label">
-								<div class="-name">
-									{{ user.display_name }}
+					<app-scroll-scroller>
+						<div class="-user-list">
+							<div
+								v-for="user of filteredUsers"
+								:key="user.id"
+								class="-user-list-item"
+								@click="toggle(user)"
+							>
+								<div class="-avatar">
+									<app-user-avatar-img :user="user" />
 								</div>
-								<div class="-username">@{{ user.username }}</div>
-							</div>
-							<div>
-								<a @click="toggle(user)">
+
+								<div class="-label">
+									<div class="-name">
+										{{ user.display_name }}
+									</div>
+									<div class="-username">@{{ user.username }}</div>
+								</div>
+								<div class="-radio" :class="{ '-active': selected(user) }">
 									<app-jolticon
 										:icon="selected(user) ? 'checkbox' : 'box-empty'"
 									/>
-								</a>
+								</div>
 							</div>
 						</div>
-					</div>
-				</app-scroll-scroller>
+					</app-scroll-scroller>
+				</div>
+			</div>
+		</template>
+
+		<template #footer>
+			<div class="-footer">
 				<app-button primary block :disabled="selectedUsers.length < 1" @click="invite">
 					<translate>Invite</translate>
 				</app-button>
 			</div>
-		</div>
+		</template>
 	</app-modal>
 </template>
 
