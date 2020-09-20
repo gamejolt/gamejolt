e49ad01acb8fe4c1285f344c16b5c25885b14eda
David DeCarmine <hworld@holo-world.com>
2020-09-19 18:05:20 -0700

    some nicer styling

--- src/app/components/chat/invite-modal/invite-modal.vue
+++ src/app/components/chat/invite-modal/invite-modal.vue
@@ -1,4 +1,5 @@
 <script lang="ts" src="./invite-modal"></script>
+
 <template>
 	<app-modal>
 		<template #default>
@@ -10,20 +11,17 @@
 
 			<div class="modal-header">
 				<h2 class="modal-title">
-					<translate>Invite Friends</translate>
+					<translate>Choose Friends</translate>
 				</h2>
 			</div>
 
 			<div class="modal-body">
 				<div class="friend-select-widget">
-					<div class="nav-controls">
-						<input
-							v-model="filterQuery"
-							text="search"
-							class="form-control"
-							placeholder="Filter..."
-						/>
-					</div>
+					<input
+						v-model="filterQuery"
+						class="-filter form-control"
+						placeholder="Filter..."
+					/>
 
 					<app-scroll-scroller>
 						<div class="-user-list">
@@ -77,4 +75,71 @@
 	</app-modal>
 </template>
 
-<style lang="stylus" src="./invite-modal.styl" scoped></style>
+<style lang="stylus" scoped>
+@import '~styles/variables'
+@import '~styles-lib/mixins'
+
+$-v-padding = 15px
+$-h-padding = 20px
+$-height = 40px
+
+.-filter
+	margin-bottom: 8px
+
+.-user-list-item
+	theme-prop('border-bottom-color', 'bg-subtle')
+	display: flex
+	align-items: center
+	padding: $-v-padding 0
+	height: $-height + $-v-padding * 2
+	overflow: hidden
+	border-bottom-width: $border-width-small
+	border-bottom-style: solid
+	cursor: pointer
+
+	&:last-child
+		border-bottom: 0
+
+	&:hover
+		.-radio
+			color: var(--theme-link-hover)
+
+.-radio
+	color: var(--theme-bg-subtle)
+
+	&.-active
+		color: var(--theme-highlight)
+
+.-avatar
+	flex: none
+	width: $-height
+	margin-right: $-h-padding
+
+.-label
+	flex: auto
+	overflow: hidden
+
+.-name
+.-username
+	text-overflow()
+
+.-name
+	font-weight: bold
+
+.-username
+	theme-prop('color', 'fg-muted')
+	font-size: $font-size-small
+
+.-button
+	flex: none
+	margin-left: $-h-padding
+
+.-selected-users
+	display: flex
+	justify-content: center
+	margin-top: -10px
+	margin-bottom: 4px
+
+.-bottom
+	padding-bottom: $line-height-computed
+</style>
