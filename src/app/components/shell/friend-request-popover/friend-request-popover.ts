18602f5af9130d615e38b6a149245fac79ca4eb0
nilllzz <nidresch@googlemail.com>
2020-09-30 16:40:52 +0200

    Paginate friend requests (#575)

--- src/app/components/shell/friend-request-popover/friend-request-popover.ts
+++ src/app/components/shell/friend-request-popover/friend-request-popover.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import { Mutation, State } from 'vuex-class/lib/bindings';
+import { Api } from '../../../../_common/api/api.service';
 import { Connection } from '../../../../_common/connection/connection-service';
 import AppLoading from '../../../../_common/loading/loading.vue';
 import AppPopper from '../../../../_common/popper/popper.vue';
@@ -35,8 +36,12 @@ export default class AppShellFriendRequestPopover extends Vue {
 
 	isShowing = false;
 	isLoading = false;
+	perPage = 50;
+	isBootstrapped = false;
 
 	activeTab: Tab = 'requests';
+	requestCount = 0;
+	pendingCount = 0;
 	incoming: UserFriendship[] = [];
 	outgoing: UserFriendship[] = [];
 
@@ -46,32 +51,85 @@ export default class AppShellFriendRequestPopover extends Vue {
 		return this.activeTab === 'requests' ? this.incoming : this.outgoing;
 	}
 
+	get isAtEnd() {
+		return (
+			(this.activeTab === 'requests' ? this.requestCount : this.pendingCount) ===
+			this.requests.length
+		);
+	}
+
 	async onShow() {
 		if (this.isLoading) {
 			return;
 		}
 
+		// Reset to empty.
+		this.incoming = [];
+		this.outgoing = [];
+
 		this.isShowing = true;
 		this.isLoading = true;
 
-		const { requests, pending } = await UserFriendship.fetchRequests();
-		this.incoming = requests;
-		this.setFriendRequestCount(this.incoming.length);
-		this.outgoing = pending;
+		// We only need to do this request once.
+		if (!this.isBootstrapped) {
+			const payload = await Api.sendRequest('/web/dash/friends/requests', null, {
+				detach: true,
+			});
+			this.requestCount = payload.requestCount;
+			this.pendingCount = payload.pendingCount;
+			this.perPage = payload.perPage;
+			this.isBootstrapped = true;
+
+			this.setFriendRequestCount(this.requestCount);
+		}
+
+		await this.loadTab();
+
 		this.isLoading = false;
+	}
+
+	private async loadTab() {
+		let url = '/web/dash/friends/requests/' + this.activeTab;
+		const lastRequest =
+			this.requests.length > 0 ? this.requests[this.requests.length - 1] : null;
+		if (lastRequest) {
+			url += '?from=' + lastRequest.created_on;
+		}
+
+		const payload = await Api.sendRequest(url, null, { detach: true });
+		const requests = UserFriendship.populate(payload.requests) as UserFriendship[];
+
+		this.requests.push(...requests);
 	}
 
 	onHide() {
 		this.isShowing = false;
 	}
 
-	setActiveTab(tab: Tab) {
+	async setActiveTab(tab: Tab) {
 		this.activeTab = tab;
+
+		// Load tab the first time it's opened.
+		if (this.requests.length === 0) {
+			const count = this.activeTab === 'pending' ? this.pendingCount : this.requestCount;
+			if (count > 0) {
+				this.isLoading = true;
+				await this.loadTab();
+				this.isLoading = false;
+			}
+		}
+	}
+
+	async loadMore() {
+		this.isLoading = true;
+		await this.loadTab();
+		this.isLoading = false;
 	}
 
 	async acceptRequest(request: UserFriendship) {
 		await UserFriendshipHelper.acceptRequest(request);
 		this.removeRequest(request);
+		this.requestCount--;
 	}
 
 	async rejectRequest(request: UserFriendship) {
@@ -79,6 +137,7 @@ export default class AppShellFriendRequestPopover extends Vue {
 			return;
 		}
 		this.removeRequest(request);
+		this.requestCount--;
 	}
 
 	async cancelRequest(request: UserFriendship) {
@@ -86,6 +145,7 @@ export default class AppShellFriendRequestPopover extends Vue {
 			return;
 		}
 		this.removeRequest(request);
+		this.pendingCount--;
 	}
 
 	private removeRequest(request: UserFriendship) {
