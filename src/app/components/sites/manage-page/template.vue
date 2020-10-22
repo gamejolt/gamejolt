a2bbf9c373048d0b9dbc9972254f8bb8e3b2e263
David DeCarmine <hworld@holo-world.com>
2020-10-21 17:21:42 -0700

    some more tweaks

--- src/app/components/sites/manage-page/template.vue
+++ src/app/components/sites/manage-page/template.vue
@@ -1,6 +1,8 @@
+<script lang="ts" src="./template"></script>
+
 <template>
 	<div>
-		<div class="well fill-offset text-center" v-if="!enabled">
+		<div v-if="!enabled" class="well fill-offset text-center">
 			<div class="row">
 				<div class="col-xs-8 col-centered">
 					<p class="lead">
@@ -9,17 +11,7 @@
 						</translate>
 					</p>
 
-					<app-button
-						primary
-						@click="enable()"
-						v-app-tooltip="
-							staticEnabled
-								? $gettext(
-										'This will disable your static site and use a customizable template instead.'
-								  )
-								: undefined
-						"
-					>
+					<app-button v-app-tooltip="enableTooltip" primary @click="enable()">
 						<translate>Turn On</translate>
 					</app-button>
 				</div>
@@ -27,23 +19,19 @@
 		</div>
 		<div v-else>
 			<div class="alert alert-notice visible-xs full-bleed-xs">
-				<p><translate>The site editor doesn't work on mobile devices at this time.</translate></p>
+				<p>
+					<translate>
+						The site editor doesn't work on mobile devices at this time.
+					</translate>
+				</p>
 			</div>
 
 			<div class="hidden-xs">
-				<app-button
-					class="anim-fade-in no-animate-leave stagger"
-					:href="themeEditorLocation"
-					target="_blank"
-				>
+				<app-button :href="themeEditorLocation" target="_blank">
 					<translate>Customize Theme</translate>
 				</app-button>
 
-				<app-button
-					class="anim-fade-in no-animate-leave stagger"
-					:href="contentEditorLocation"
-					target="_blank"
-				>
+				<app-button :href="contentEditorLocation" target="_blank">
 					<translate>Edit Content</translate>
 				</app-button>
 			</div>
@@ -56,5 +44,3 @@
 		</div>
 	</div>
 </template>
-
-<script lang="ts" src="./template"></script>
