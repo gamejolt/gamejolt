cea7a30b5c0422c2dadaade2d5bd8a1b34ffe4a3
David DeCarmine <hworld@holo-world.com>
2020-10-30 10:38:40 -0700

    shaka should always play inline

--- src/_common/video/player/shaka.vue
+++ src/_common/video/player/shaka.vue
@@ -2,7 +2,7 @@
 
 <template>
 	<div class="-shaka">
-		<video ref="video" :poster="player.poster" :autoplay="shouldAutoplay" loop />
+		<video ref="video" :poster="player.poster" :autoplay="shouldAutoplay" loop playsinline />
 	</div>
 </template>
 
