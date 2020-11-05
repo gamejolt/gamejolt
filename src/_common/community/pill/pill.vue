ba9cf526653d05cd659aa66894d450cc3087097c
hapurubokka <somekid11@gmail.com>
2020-11-04 16:18:11 -0800

    Fix alignment of community-pill featured jolticon

--- src/_common/community/pill/pill.vue
+++ src/_common/community/pill/pill.vue
@@ -1,26 +1,34 @@
+<script lang="ts" src="./pill"></script>
+
 <template>
 	<app-pill v-if="!channel" :to="toCommunity">
-		<app-community-thumbnail-img slot="img" :community="community" />
+		<template #img>
+			<app-community-thumbnail-img :community="community" />
+		</template>
 
-		{{ community.name }}
-		<app-community-verified-tick class="-tick" :community="community" small />
+		<template #default>
+			{{ community.name }}
+			<app-community-verified-tick class="-tick" :community="community" small />
+		</template>
 	</app-pill>
 	<app-pill-bi v-else :left-to="toCommunity" :right-to="toChannel">
-		<app-community-thumbnail-img slot="img" :community="community" />
+		<template #img>
+			<app-community-thumbnail-img:community="community" />
+		</template>
 
-		<template slot="left">
+		<template #left>
 			{{ community.name }}
 			<app-community-verified-tick class="-tick" :community="community" small />
 		</template>
 
-		<template slot="right">
+		<template #right>
 			{{ channel.title }}
 
 			<app-jolticon
 				v-if="communityLink.isFeatured"
+				v-app-tooltip="$gettext(`Featured`)"
 				class="-featured"
 				icon="star"
-				v-app-tooltip="$gettext(`Featured`)"
 			/>
 		</template>
 	</app-pill-bi>
@@ -31,8 +39,6 @@
 	margin-left: 5px
 
 .-featured
-	margin: 0 0 3px 5px
+	margin: 0 0 0 5px
 	font-size: 12px
 </style>
-
-<script lang="ts" src="./pill"></script>
