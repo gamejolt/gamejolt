54f38be2228b0f7b96e602db6c693784c9bff538
hapurubokka <somekid11@gmail.com>
2020-11-18 11:57:43 -0800

    Fix modals cropping popup content

--- src/_common/sticker/controls-overlay/controls-overlay.vue
+++ src/_common/sticker/controls-overlay/controls-overlay.vue
@@ -2,7 +2,7 @@
 
 <template>
 	<div class="sticker-controls-overlay">
-		<div class="-overlay" :class="{ '-end': end }" />
+		<div class="-overlay" :class="{ '-end': end, '-wider': wider }" />
 		<slot />
 	</div>
 </template>
@@ -17,6 +17,7 @@
 	z-index: 1
 
 	.-overlay
+		rounded-corners-lg()
 		change-bg('bg')
 		// We want to expand the overlay out to the edges of the post container.
 		margin-left: -($grid-gutter-width-xs / 2)
@@ -24,15 +25,14 @@
 		position: absolute
 		top: -4px
 		bottom: 4px
-		// Bleed a little bit extra to account for any invisible borders.
-		left: -4px
+		left: 0
 		right: @left
 		opacity: 0.9
 		z-index: -1
 
 		@media $media-sm-up
-			margin-left: -($grid-gutter-width / 2) + $border-width-base + 0.5px
-			margin-right: -($grid-gutter-width / 2) + $border-width-base + 0.5px
+			margin-left: -($grid-gutter-width / 2)
+			margin-right: -($grid-gutter-width / 2)
 
 		&.-end
 			bottom: -4px
