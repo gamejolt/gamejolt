71726a0ac6bb16e427e8b9597a8ebc8352b7e106
nilllzz <nidresch@googlemail.com>
2021-02-07 07:08:24 +0100

    fix default route paths

--- src/app/views/communities/view/edit/channels/edit/competition/overview/overview.route.ts
+++ src/app/views/communities/view/edit/channels/edit/competition/overview/overview.route.ts
@@ -2,7 +2,7 @@ import { RouteConfig } from 'vue-router';
 
 export const routeCommunitiesViewEditChannelsCompetitionOverview: RouteConfig = {
 	name: 'communities.view.edit.channels.competition.overview',
-	path: '',
+	path: '/c/:path/edit/:id(\\d+)/channels/:channel/jam',
 	component: () =>
 		import(
 			/* webpackChunkName: "routeCommunitiesViewEditChannelCompetitions" */ './overview.vue'
--- src/app/views/communities/view/edit/channels/edit/overview/overview.route.ts
+++ src/app/views/communities/view/edit/channels/edit/overview/overview.route.ts
@@ -2,7 +2,7 @@ import { RouteConfig } from 'vue-router';
 
 export const routeCommunitiesViewEditChannelsOverview: RouteConfig = {
 	name: 'communities.view.edit.channels.overview',
-	path: '',
+	path: '/c/:path/edit/:id(\\d+)/channels/:channel',
 	component: () =>
 		import(/* webpackChunkName: "routeCommunitiesViewEditChannel" */ './overview.vue'),
 };
