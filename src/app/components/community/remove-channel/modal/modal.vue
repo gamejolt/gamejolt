46c4e5618b272fd0e26b1b5e24831315a13a77ae
nilllzz <nidresch@googlemail.com>
2021-02-09 01:06:50 +0100

    add warning when removing jam channel

--- src/app/components/community/remove-channel/modal/modal.vue
+++ src/app/components/community/remove-channel/modal/modal.vue
@@ -1,3 +1,5 @@
+<script lang="ts" src="./modal"></script>
+
 <template>
 	<app-modal>
 		<div class="modal-controls">
@@ -15,6 +17,24 @@
 		</div>
 
 		<div class="modal-body">
+			<template v-if="channel.type === 'competition'">
+				<div class="alert">
+					<h4 class="sans-margin-top"><translate>Removing a Jam Channel</translate></h4>
+					<p>
+						<span v-translate>
+							This channel contains a <b>Jam</b>, which gets removed when this channel
+							gets removed. That includes all entries, votes, awards and results that
+							belong to the jam.
+						</span>
+					</p>
+					<p>
+						<span v-translate class="-jam-warning">
+							<b>This action is irreversible!</b>
+						</span>
+					</p>
+				</div>
+			</template>
+
 			<app-community-remove-channel
 				:community="community"
 				:channel="channel"
@@ -24,4 +44,15 @@
 	</app-modal>
 </template>
 
-<script lang="ts" src="./modal"></script>
+<style lang="stylus" scoped>
+@import '~styles/variables'
+@import '~styles-lib/mixins'
+
+.-jam-warning
+	display: inline-block
+	padding: 4px
+	rounded-corners()
+	background-color: var(--theme-notice)
+	color: var(--theme-notice-fg)
+	margin-top: 4px
+</style>
