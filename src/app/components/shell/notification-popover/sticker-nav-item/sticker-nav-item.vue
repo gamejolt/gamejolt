21c525bb40e0721f165411207dbe95018229d050
David DeCarmine <hworld@holo-world.com>
2021-04-14 11:50:09 -0700

    Use number for sticker nav item

--- src/app/components/shell/notification-popover/sticker-nav-item/sticker-nav-item.vue
+++ src/app/components/shell/notification-popover/sticker-nav-item/sticker-nav-item.vue
@@ -14,7 +14,7 @@
 					<div class="timeline-list-item-title timeline-list-item-title-small -title">
 						<translate
 							:translate-n="stickerCount"
-							:translate-params="{ count: fuzzynumber(stickerCount) }"
+							:translate-params="{ count: number(stickerCount) }"
 							translate-plural="You have %{ count } stickers."
 						>
 							You have %{ count } sticker.
