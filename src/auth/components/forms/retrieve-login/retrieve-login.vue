69f751cddcc83c24cfd92eb91891199dbbde6024
hworld <hworld@holo-world.com>
2021-04-20 12:05:17 -0700

    Removed oauth besides google from our auth forms. (#774)
    * Removed oauth besides google from our auth forms.
    Modify the auth flow to allow retrieving login credentials through username.
    
    * slight change to messaging

--- src/auth/components/forms/retrieve-login/retrieve-login.vue
+++ src/auth/components/forms/retrieve-login/retrieve-login.vue
@@ -1,15 +1,20 @@
+<script lang="ts" src="./retrieve-login"></script>
+
 <template>
 	<app-form name="retrieveLoginForm">
 		<fieldset :disabled="Connection.isClientOffline">
-			<app-form-group name="email" :label="$gettext('auth.forgot.email_label')" :hide-label="true">
+			<app-form-group name="email" :label="$gettext('Email or Username')" hide-label>
 				<p class="help-block">
-					<translate>auth.forgot.email_help</translate>
+					<translate>
+						Enter the email address or username on your account and we'll send you a
+						link to get into your account.
+					</translate>
 				</p>
 
 				<app-form-control
-					type="email"
+					type="text"
 					:validate-on="['blur']"
-					:placeholder="$gettext('auth.forgot.email_label')"
+					:placeholder="$gettext('Email or Username')"
 					@changed="onChanged"
 				/>
 
@@ -19,22 +24,19 @@
 			<br />
 
 			<div
-				class="alert alert-notice anim-fade-in-enlarge no-animate-leave"
-				v-if="state.invalidEmail"
+				v-if="invalidEmail"
 				v-translate
+				class="alert alert-notice anim-fade-in-enlarge no-animate-leave"
 			>
 				<p>
-					Hmm, that email address is not in our system. Maybe you didn't
-					<a href="/join">sign up yet</a>
-					?
+					Hmm, we couldn't find you in our system. Maybe you didn't
+					<a href="/join">sign up yet</a>?
 				</p>
 			</div>
 
 			<app-form-button block>
-				<translate>auth.forgot.submit_button</translate>
+				<translate>Send Login Link</translate>
 			</app-form-button>
 		</fieldset>
 	</app-form>
 </template>
-
-<script lang="ts" src="./retrieve-login"></script>
