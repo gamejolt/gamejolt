caece0126ea992ed81325bf30a12170094d092ff
nilllzz <nidresch@googlemail.com>
2021-05-16 05:18:52 +0200

    add new content rule to embiggen emojis

--- src/_common/content/content-editor/content-rules.ts
+++ src/_common/content/content-editor/content-rules.ts
@@ -1,7 +1,10 @@
 export class ContentRules {
 	public maxMediaWidth: number | null = null;
 	public maxMediaHeight: number | null = null;
+	/** Truncates links to a max length. */
 	public truncateLinks = false;
+	/** Embiggens emojis when the entire content is just 1 emoji. */
+	public biggifyEmojis = false;
 
 	constructor(data?: Partial<ContentRules>) {
 		Object.assign(this, data);
