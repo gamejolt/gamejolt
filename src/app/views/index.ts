f760e776203b7c5815db551c799be485512701d9
David DeCarmine <hworld@holo-world.com>
2021-06-09 16:36:24 -0700

    remove store route

--- src/app/views/index.ts
+++ src/app/views/index.ts
@@ -16,7 +16,6 @@ import { routePost } from './post/post.route';
 import { routeProfile } from './profile/profile.route';
 import { routeSearch } from './search/search.route';
 import { routeSettings } from './settings/settings.route';
-import { routeStore } from './store/store.route';
 import { routeStyleguide } from './styleguide/styleguide.route';
 import { routeTimeout } from './timeout/timeout.route';
 import { routeWelcome } from './welcome/welcome.route';
@@ -25,7 +24,6 @@ import { routeWeplay } from './weplay/weplay.route';
 const routes = [
 	routeHome,
 	routeDiscover,
-	routeStore,
 	routeDownload,
 	routeCommunitiesView,
 	routeLanding,
