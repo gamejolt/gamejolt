986ab5f205582c6147e001978b13865e1973ca81
nilllzz <nidresch@googlemail.com>
2021-07-15 19:38:25 +0200

    work

--- src/app/views/discover/firesides/firesides.route.ts
+++
@@ -1,7 +0,0 @@
-import { RouteConfig } from 'vue-router';
-
-export const routeDiscoverFiresides: RouteConfig = {
-	name: 'discover.firesides',
-	path: '/firesides',
-	component: () => import(/* webpackChunkName: "routeDiscoverFiresides" */ './firesides.vue'),
-};
