df786d715fd41005199763cd92190103d3ca4620
hapurubokka <somekid11@gmail.com>
2021-07-15 12:15:35 -0700

    Fix resizing template-columns

--- src/app/views/post/_page/recommendations/recommendations.vue
+++ src/app/views/post/_page/recommendations/recommendations.vue
@@ -24,12 +24,14 @@
 @import '~styles/variables'
 @import '~styles-lib/mixins'
 
+$-grid-gap = 16px
+
 .post-page-recommendations
 	@media $media-lg-up
 		margin-left: 50px
 
 	.-posts
 		display: grid
-		grid-template-columns: repeat(auto-fill, minmax(max(50%, 120px), 1fr))
-		grid-gap: 16px
+		grid-template-columns: 'repeat(auto-fill, minmax(calc(max(50% - %s, 120px)), 1fr))' % ($-grid-gap / 2)
+		grid-gap: $-grid-gap
 </style>
