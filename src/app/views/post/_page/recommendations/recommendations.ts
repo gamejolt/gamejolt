e422a99a11d7ffa84135852f86b6bd6a5209e0d7
hapurubokka <somekid11@gmail.com>
2021-07-15 14:35:49 -0700

    Allow scrollable, improve display over smaller breakpoints

--- src/app/views/post/_page/recommendations/recommendations.ts
+++ src/app/views/post/_page/recommendations/recommendations.ts
@@ -4,6 +4,8 @@ import { Prop } from 'vue-property-decorator';
 import { propRequired } from '../../../../../utils/vue';
 import { Api } from '../../../../../_common/api/api.service';
 import { FiresidePost } from '../../../../../_common/fireside/post/post-model';
+import { Screen } from '../../../../../_common/screen/screen-service';
+import AppScrollScroller from '../../../../../_common/scroll/scroller/scroller.vue';
 import AppPostCardPlaceholder from '../../../../components/fireside/post/card/card-placeholder.vue';
 import AppPostCard from '../../../../components/fireside/post/card/card.vue';
 
@@ -11,12 +13,25 @@ import AppPostCard from '../../../../components/fireside/post/card/card.vue';
 	components: {
 		AppPostCard,
 		AppPostCardPlaceholder,
+		AppScrollScroller,
 	},
 })
 export default class AppPostPageRecommendations extends Vue {
 	@Prop(propRequired(FiresidePost)) post!: FiresidePost;
 
 	posts: FiresidePost[] | null = null;
+
+	get shouldScroll() {
+		return Screen.isXs;
+	}
+
+	get usablePosts() {
+		if (Screen.isSm && !this.shouldScroll) {
+			return this.posts?.slice(0, 8);
+		}
+
+		return this.posts;
+	}
 
 	async created() {
 		const payload = await Api.sendRequest(
--- src/app/views/post/_page/recommendations/recommendations.vue
+++ src/app/views/post/_page/recommendations/recommendations.vue
@@ -5,18 +5,47 @@
 		<h4 class="section-header">
 			<translate>Next up</translate>
 		</h4>
-		<div class="-posts">
-			<template v-if="!posts">
-				<div v-for="i of 4" :key="i" class="-post">
-					<app-post-card-placeholder />
-				</div>
-			</template>
-			<template v-else>
-				<div v-for="recommendedPost of posts" :key="recommendedPost.id" class="-post">
-					<app-post-card :post="recommendedPost" with-user />
-				</div>
-			</template>
-		</div>
+		<component
+			:is="shouldScroll ? 'app-scroll-scroller' : 'div'"
+			:class="{ '-scrollable': shouldScroll }"
+			:horizontal="shouldScroll"
+			:thin="shouldScroll"
+		>
+			<div class="-posts">
+				<template v-if="!usablePosts">
+					<template v-for="i of 4">
+						<div :key="i" class="-post">
+							<app-post-card-placeholder />
+						</div>
+
+						<div
+							v-if="shouldScroll"
+							:key="'spacer-' + i"
+							:class="{
+								'-spacer': i < usablePosts.length,
+								'-spacer-large': i === usablePosts.length,
+							}"
+						/>
+					</template>
+				</template>
+				<template v-else>
+					<template v-for="(recommendedPost, i) of usablePosts">
+						<div :key="recommendedPost.id" class="-post">
+							<app-post-card :post="recommendedPost" with-user />
+						</div>
+
+						<div
+							v-if="shouldScroll"
+							:key="'spacer-' + i"
+							:class="{
+								'-spacer': i + 1 < usablePosts.length,
+								'-spacer-large': i + 1 === usablePosts.length,
+							}"
+						/>
+					</template>
+				</template>
+			</div>
+		</component>
 	</div>
 </template>
 
@@ -32,6 +61,45 @@ $-grid-gap = 16px
 
 	.-posts
 		display: grid
-		grid-template-columns: 'repeat(auto-fill, minmax(calc(max(50% - %s, 120px)), 1fr))' % ($-grid-gap / 2)
-		grid-gap: $-grid-gap
+
+		@media $media-sm
+			grid-template-columns: repeat(4, 1fr)
+			grid-gap: $-grid-gap
+
+		@media $media-md-up
+			grid-template-columns: 'repeat(auto-fill, minmax(calc(max(50% - %s, 110px)), 1fr))' % $-grid-gap
+			grid-gap: $-grid-gap
+
+.-scrollable
+	@media $media-xs
+		full-bleed-xs()
+		padding-left: $grid-gutter-width-xs * 0.5
+		padding-right: @padding-left
+
+	@media $media-sm-up
+		margin-left: -($grid-gutter-width)
+		margin-right: @margin-left
+		padding-left: $grid-gutter-width
+		padding-right: @padding-left
+
+	.-posts
+		display: flex
+		padding-bottom: 8px
+		grid-gap: 0
+
+		.-post
+			min-width: 'calc(min(40vw - %s, 60vh * (10 / 16)))' % $-grid-gap
+
+.-spacer
+	flex: none
+	width: $-grid-gap
+
+	&-large
+		flex: none
+
+		@media $media-xs
+			width: $grid-gutter-width-xs * 0.5
+
+		@media $media-sm-up
+			width: $grid-gutter-width
 </style>
