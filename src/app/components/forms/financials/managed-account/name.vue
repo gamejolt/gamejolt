3d17d7fe1b4ee92f28e3de8e346ae89051f2a6a8
David DeCarmine <hworld@holo-world.com>
2021-08-12 18:56:37 -0700

    fix template requiring a directive

--- src/app/components/forms/financials/managed-account/name.vue
+++ src/app/components/forms/financials/managed-account/name.vue
@@ -1,13 +1,15 @@
+<script lang="ts" src="./name"></script>
+
 <template>
 	<div>
-		<div class="row" v-if="!parent.getStripeField(namePrefix + '.first_name')">
-			<div class="col-sm-6" v-if="parent.requiresField(namePrefix + '.first_name')">
+		<div v-if="!parent.getStripeField(namePrefix + '.first_name')" class="row">
+			<div v-if="parent.requiresField(namePrefix + '.first_name')" class="col-sm-6">
 				<app-form-group :name="`${namePrefix}.first_name`" :label="$gettext('First Name')">
 					<app-form-control />
 					<app-form-control-errors />
 				</app-form-group>
 			</div>
-			<div class="col-sm-6" v-if="parent.requiresField(namePrefix + '.last_name')">
+			<div v-if="parent.requiresField(namePrefix + '.last_name')" class="col-sm-6">
 				<app-form-group :name="`${namePrefix}.last_name`" :label="$gettext('Last Name')">
 					<app-form-control />
 					<app-form-control-errors />
@@ -18,46 +20,46 @@
 		<div
 			v-if="
 				!parent.getStripeField(namePrefix + '.relationship.title') &&
-					parent.requiresField(namePrefix + '.relationship.title')
+				parent.requiresField(namePrefix + '.relationship.title')
 			"
 		>
-			<app-form-group :name="`${namePrefix}.relationship.title`" :label="$gettext('Job Title')">
+			<app-form-group
+				:name="`${namePrefix}.relationship.title`"
+				:label="$gettext('Job Title')"
+			>
 				<app-form-control />
 				<app-form-control-errors />
 			</app-form-group>
 		</div>
 
 		<div
-			class="form-horizontal"
 			v-if="
 				parent.getStripeField(namePrefix + '.first_name') ||
-					parent.getStripeField(namePrefix + '.title')
+				parent.getStripeField(namePrefix + '.title')
 			"
+			class="form-horizontal"
 		>
-			<div class="form-group" v-if="parent.getStripeField(namePrefix + '.first_name')">
+			<div v-if="parent.getStripeField(namePrefix + '.first_name')" class="form-group">
 				<label class="control-label col-sm-4">
 					<translate>Name</translate>
 				</label>
 				<div class="form-static col-sm-8">
-					<template>
-						{{ parent.getStripeField(namePrefix + '.first_name') }}
-						{{ parent.getStripeField(namePrefix + '.last_name') }}
-					</template>
+					{{ parent.getStripeField(namePrefix + '.first_name') }}
+					{{ parent.getStripeField(namePrefix + '.last_name') }}
 				</div>
 			</div>
 
-			<div class="form-group" v-if="parent.getStripeField(namePrefix + '.relationship.title')">
+			<div
+				v-if="parent.getStripeField(namePrefix + '.relationship.title')"
+				class="form-group"
+			>
 				<label class="control-label col-sm-4">
 					<translate>Job Title</translate>
 				</label>
 				<div class="form-static col-sm-8">
-					<template>
-						{{ parent.getStripeField(namePrefix + '.relationship.title') }}
-					</template>
+					{{ parent.getStripeField(namePrefix + '.relationship.title') }}
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
-
-<script lang="ts" src="./name"></script>
