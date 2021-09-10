3f70a04bc73f351f9cb19225d3a0b84a605273df
hapurubokka <somekid11@gmail.com>
2021-09-10 15:08:28 -0700

    Move output device outside of advanced settings.

--- src/app/views/fireside/_stream-setup/stream-setup.vue
+++ src/app/views/fireside/_stream-setup/stream-setup.vue
@@ -132,11 +132,54 @@
 						</p>
 					</app-form-group>
 
+					<app-form-group
+						name="tempSelectedGroupAudioDeviceId"
+						:label="$gettext('Output Device')"
+					>
+						<template v-if="!hasSpeakerPermissions">
+							<translate>
+								To hear the other people streaming with you, we'll need access to
+								your output device so that we can pipe their beautiful voices into
+								your earholes.
+							</translate>
+
+							<app-button
+								v-if="!speakerPermissionsWerePrompted"
+								@click="onClickPromptSpeakerPermissions"
+							>
+								<translate>Request Permission</translate>
+							</app-button>
+							<translate v-else>
+								Click the lock icon next to the URL in your address bar to enable.
+								You may need to open this window again or refresh the page to renew
+								your devices.
+							</translate>
+						</template>
+						<template v-else>
+							<app-form-control-select :disabled="producer.isBusy">
+								<option
+									v-for="speaker of speakers"
+									:key="speaker.deviceId"
+									:value="speaker.deviceId"
+								>
+									{{ speaker.label }}
+								</option>
+							</app-form-control-select>
+
+							<p class="help-block">
+								<translate>
+									Make sure you choose an output device that is not captured by
+									your desktop audio input device.
+								</translate>
+							</p>
+						</template>
+					</app-form-group>
+
 					<fieldset>
 						<app-form-legend
 							v-if="shouldShowAdvanced || !isInvalidConfig"
 							compact
-							expandable
+							:expandable="!hasDesktopAudio"
 							:expanded="shouldShowAdvanced"
 							@click.native="onToggleAdvanced"
 						>
@@ -222,51 +265,6 @@
 										</div>
 									</app-expand>
 								</app-form-group>
-
-								<app-expand :when="shouldShowAdvanced">
-									<app-form-group
-										name="tempSelectedGroupAudioDeviceId"
-										:label="$gettext('Chat Output Device')"
-									>
-										<template v-if="!hasSpeakerPermissions">
-											<translate>
-												To hear the other people streaming with you, we'll
-												need access to your output device so that we can
-												pipe their beautiful voices into your earholes.
-											</translate>
-
-											<app-button
-												v-if="!speakerPermissionsWerePrompted"
-												@click="onClickPromptSpeakerPermissions"
-											>
-												<translate>Request Permission</translate>
-											</app-button>
-											<translate v-else>
-												Click the lock icon next to the URL in your address
-												bar to enable. You may need to open this window
-												again or refresh the page to renew your devices.
-											</translate>
-										</template>
-										<template v-else>
-											<app-form-control-select :disabled="producer.isBusy">
-												<option
-													v-for="speaker of speakers"
-													:key="speaker.deviceId"
-													:value="speaker.deviceId"
-												>
-													{{ speaker.label }}
-												</option>
-											</app-form-control-select>
-
-											<p class="help-block">
-												<translate>
-													Make sure you choose an output device that is
-													not captured by your desktop audio input device.
-												</translate>
-											</p>
-										</template>
-									</app-form-group>
-								</app-expand>
 							</div>
 						</app-expand>
 					</fieldset>
