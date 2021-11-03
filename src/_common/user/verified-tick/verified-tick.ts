6ff0be60e4d6ff24fbcab6f12572f2470846ab6b
nilsgj <nils@gamejolt.com>
2021-11-03 07:04:50 +0100

    show "Game Jolt Staff" instead of "Verified Account" for staff

--- src/_common/user/verified-tick/verified-tick.ts
+++ src/_common/user/verified-tick/verified-tick.ts
@@ -27,18 +27,22 @@ export default class AppUserVerifiedTick extends Vue {
 	verticalAlign!: boolean;
 
 	get shouldShow() {
-		return this.user.is_verified;
+		return this.user.is_verified || this.user.permission_level > 0;
 	}
 
 	get icon() {
-		if (this.user.is_verified) {
+		if (this.user.permission_level > 0) {
+			return this.big ? 'bolt-unfilled' : 'bolt-filled';
+		} else if (this.user.is_verified) {
 			return 'verified';
 		}
 	}
 
 	get tooltip() {
-		if (this.user.is_verified) {
-			return this.$gettext('Verified Account');
+		if (this.user.permission_level > 0) {
+			return this.$gettext(`Game Jolt Staff`);
+		} else if (this.user.is_verified) {
+			return this.$gettext(`Verified Account`);
 		}
 	}
 }
