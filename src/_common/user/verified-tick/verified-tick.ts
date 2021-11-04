a7da47ee91de95c71ca7d91ea7d8fa6b763735e8
nilsgj <nils@gamejolt.com>
2021-11-04 11:45:23 +0100

    revert changes to verified-tick.ts

--- src/_common/user/verified-tick/verified-tick.ts
+++ src/_common/user/verified-tick/verified-tick.ts
@@ -27,21 +27,17 @@ export default class AppUserVerifiedTick extends Vue {
 	verticalAlign!: boolean;
 
 	get shouldShow() {
-		return this.user.is_verified || this.user.permission_level > 0;
+		return this.user.is_verified;
 	}
 
 	get icon() {
-		if (this.user.permission_level > 0) {
-			return this.big ? 'bolt-unfilled' : 'bolt-filled';
-		} else if (this.user.is_verified) {
+		if (this.user.is_verified) {
 			return 'verified';
 		}
 	}
 
 	get tooltip() {
-		if (this.user.permission_level > 0) {
-			return this.$gettext(`Game Jolt Staff`);
-		} else if (this.user.is_verified) {
+		if (this.user.is_verified) {
 			return this.$gettext(`Verified Account`);
 		}
 	}
