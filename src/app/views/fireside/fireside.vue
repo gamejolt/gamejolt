4018a901be02658e4088d9b303acc440e848a098
hapurubokka <somekid11@gmail.com>
2021-11-11 13:21:05 -0800

    Fix desktop host list sometimes getting cut off

--- src/app/views/fireside/fireside.vue
+++ src/app/views/fireside/fireside.vue
@@ -37,51 +37,64 @@
 						v-app-observe-dimensions="onDimensionsChange"
 						class="-video-container"
 					>
-						<app-sticker-target
-							:controller="c.stickerTargetController"
+						<div
 							class="-video-inner"
-							:class="{
-								'-unsupported': GJ_IS_CLIENT,
-							}"
 							:style="{
 								width: videoWidth + 'px',
 								height: videoHeight + 'px',
 							}"
 						>
-							<template v-if="GJ_IS_CLIENT">
-								<app-illustration
-									v-if="shouldShowHosts && shortestSide > 700"
-									src="~img/ill/no-comments.svg"
-								/>
-
-								<p class="-unsupported-text">
-									<translate>
-										Oh no... Fireside streams don't work on the Client yet.
-									</translate>
-								</p>
-
-								<app-button @click="onClickOpenBrowser()">
-									<translate>Open Fireside in Browser</translate>
-								</app-button>
-							</template>
-							<template v-else-if="c.rtc && c.rtc.focusedUser">
-								<app-popper trigger="right-click">
-									<app-fireside-stream
-										:rtc-user="c.rtc.focusedUser"
-										:show-overlay-hosts="!shouldShowHosts"
-										:members="overlayChatMembers"
+							<app-sticker-target
+								class="-video-inner"
+								:controller="c.stickerTargetController"
+								:class="{
+									'-unsupported': GJ_IS_CLIENT,
+								}"
+								:style="{
+									top: 0,
+									right: 0,
+									bottom: 0,
+									left: 0,
+								}"
+							>
+								<template v-if="GJ_IS_CLIENT">
+									<app-illustration
+										v-if="shouldShowHosts && shortestSide > 700"
+										src="~img/ill/no-comments.svg"
 									/>
 
-									<template #popover>
-										<div class="list-group">
-											<a class="list-group-item" @click="toggleVideoStats()">
-												<translate>Toggle Video Stats</translate>
-											</a>
-										</div>
-									</template>
-								</app-popper>
-							</template>
-						</app-sticker-target>
+									<p class="-unsupported-text">
+										<translate>
+											Oh no... Fireside streams don't work on the Client yet.
+										</translate>
+									</p>
+
+									<app-button @click="onClickOpenBrowser()">
+										<translate>Open Fireside in Browser</translate>
+									</app-button>
+								</template>
+								<template v-else-if="c.rtc && c.rtc.focusedUser">
+									<app-popper trigger="right-click">
+										<app-fireside-stream
+											:rtc-user="c.rtc.focusedUser"
+											:show-overlay-hosts="!shouldShowHosts"
+											:members="overlayChatMembers"
+										/>
+
+										<template #popover>
+											<div class="list-group">
+												<a
+													class="list-group-item"
+													@click="toggleVideoStats()"
+												>
+													<translate>Toggle Video Stats</translate>
+												</a>
+											</div>
+										</template>
+									</app-popper>
+								</template>
+							</app-sticker-target>
+						</div>
 					</div>
 				</div>
 
