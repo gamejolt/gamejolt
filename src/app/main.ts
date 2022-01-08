87fa560495af5c8e7318d92e31371a7d2bdf5602
David DeCarmine <hworld@holo-world.com>
2022-01-07 16:58:19 -0800

    remove webpack and related packages

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,3 @@
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
-import { Analytics } from '../_common/analytics/analytics.service';
 import { createApp } from './bootstrap';
 
 const { app, router, store } = createApp();
@@ -12,28 +10,29 @@ router.isReady().then(() => {
 	app.mount('#app');
 });
 
-if (GJ_BUILD_TYPE === 'production' && navigator.serviceWorker) {
-	OfflinePluginRuntime.install({
-		onUpdating() {
-			Analytics.trackEvent('sw', 'updating');
-		},
+// TODO(vue3): make sure this won't break out build when we push to prod since we're removing the service worker
+// if (GJ_BUILD_TYPE === 'production' && navigator.serviceWorker) {
+// 	OfflinePluginRuntime.install({
+// 		onUpdating() {
+// 			Analytics.trackEvent('sw', 'updating');
+// 		},
 
-		onUpdateReady() {
-			// Tells to new SW to take control immediately
-			OfflinePluginRuntime.applyUpdate();
-			Analytics.trackEvent('sw', 'update-ready');
-		},
+// 		onUpdateReady() {
+// 			// Tells to new SW to take control immediately
+// 			OfflinePluginRuntime.applyUpdate();
+// 			Analytics.trackEvent('sw', 'update-ready');
+// 		},
 
-		onUpdated() {
-			Analytics.trackEvent('sw', 'updated');
+// 		onUpdated() {
+// 			Analytics.trackEvent('sw', 'updated');
 
-			// TODO: Hook into how we update based on site-api version.
-			// Reload the webpage to load into the new version
-			// window.location.reload();
-		},
+// 			// TODO: Hook into how we update based on site-api version.
+// 			// Reload the webpage to load into the new version
+// 			// window.location.reload();
+// 		},
 
-		onUpdateFailed() {
-			Analytics.trackEvent('sw', 'update-failed');
-		},
-	});
-}
+// 		onUpdateFailed() {
+// 			Analytics.trackEvent('sw', 'update-failed');
+// 		},
+// 	});
+// }
