7949b9993366454db7dc11935d5715c72b0ca54a
David DeCarmine <hworld@holo-world.com>
2022-01-16 15:46:36 -0800

    Fix route components with cached content

--- typings/router.d.ts
+++ typings/router.d.ts
@@ -1,16 +1,10 @@
-import {
-	RouteResolverOptions,
-	RouteStoreOptions,
-	RouteStoreResolveCallback,
-} from '../src/_common/route/route-component';
+import { RouteResolverOptions } from '../src/_common/route/route-component';
 
 declare module '@vue/runtime-core' {
 	interface ComponentCustomOptions {
 		__RESOLVER__?: any;
 		routeResolverOptions?: RouteResolverOptions & {
 			hasResolver?: boolean;
-			resolveStore?: RouteStoreResolveCallback;
 		};
-		routeStoreOptions?: RouteStoreOptions;
 	}
 }
