0d2843600080b323f0d4987559d5391fdd92ad43
nilsgj <nils@gamejolt.com>
2022-03-27 01:42:33 +0100

    add quest-stage-description content context and capabilities

--- src/_common/content/content-context.ts
+++ src/_common/content/content-context.ts
@@ -13,7 +13,8 @@ export type ContentContext =
 	| 'community-description'
 	| 'chat-message'
 	| 'community-channel-description'
-	| 'fireside-chat-message';
+	| 'fireside-chat-message'
+	| 'quest-stage-description';
 
 enum ContextCapabilityType {
 	TextBold,
@@ -207,6 +208,7 @@ export class ContextCapabilities {
 					ContextCapabilityType.TextBold,
 					ContextCapabilityType.TextItalic,
 					ContextCapabilityType.TextLink,
+
 					ContextCapabilityType.TextCode,
 					ContextCapabilityType.TextStrike,
 					ContextCapabilityType.CustomLink,
@@ -270,6 +272,29 @@ export class ContextCapabilities {
 					ContextCapabilityType.Gif,
 					ContextCapabilityType.Media,
 					ContextCapabilityType.Sticker,
+				]);
+			case 'quest-stage-description':
+				return new ContextCapabilities([
+					ContextCapabilityType.TextBold,
+					ContextCapabilityType.TextItalic,
+					ContextCapabilityType.TextLink,
+					ContextCapabilityType.TextCode,
+					ContextCapabilityType.TextStrike,
+					ContextCapabilityType.CustomLink,
+					ContextCapabilityType.Media,
+					ContextCapabilityType.EmbedVideo,
+					ContextCapabilityType.EmbedMusic,
+					ContextCapabilityType.EmbedModel,
+					ContextCapabilityType.CodeBlock,
+					ContextCapabilityType.Blockquote,
+					ContextCapabilityType.Emoji,
+					ContextCapabilityType.List,
+					ContextCapabilityType.HorizontalRule,
+					ContextCapabilityType.Spoiler,
+					ContextCapabilityType.Tag,
+					ContextCapabilityType.Heading,
+					ContextCapabilityType.Mention,
+					ContextCapabilityType.Gif,
 				]);
 
 			default:
