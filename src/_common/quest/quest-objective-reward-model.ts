b93115944e4084f910676c4bb92481c9847010c0
hapurubokka <somekid11@gmail.com>
2022-05-03 15:17:12 -0700

    Add Background type for quest rewards

--- src/_common/quest/quest-objective-reward-model.ts
+++ src/_common/quest/quest-objective-reward-model.ts
@@ -2,12 +2,20 @@ import { MediaItem } from '../media-item/media-item-model';
 import { Model } from '../model/model.service';
 import { SiteTrophy } from '../site/trophy/trophy.model';
 import { StickerReward } from '../sticker/sticker-reward-model';
+import { Background } from '../background/background.model';
 
 const QuestRewardTypes = {
 	exp: 0,
 	sticker: 1,
 	siteTrophy: 2,
 	randomSticker: 3,
+
+	/**
+	 * Should just use the fallback values so we can display it however backend
+	 * wants to.
+	 */
+	custom: 4,
+	background: 5,
 } as const;
 
 export class QuestObjectiveReward extends Model {
@@ -21,6 +29,10 @@ export class QuestObjectiveReward extends Model {
 		if (data.trophy) {
 			this.trophy = new SiteTrophy(data.trophy);
 		}
+
+		if (data.background) {
+			this.background = new Background(data.background);
+		}
 	}
 
 	declare objective_id: number;
@@ -30,6 +42,7 @@ export class QuestObjectiveReward extends Model {
 	declare type: number;
 	declare stickers: StickerReward[];
 	declare trophy?: SiteTrophy;
+	declare background?: Background;
 
 	declare fallback_name: string;
 	declare fallback_amount: number;
@@ -49,6 +62,10 @@ export class QuestObjectiveReward extends Model {
 		return this.type === QuestRewardTypes.siteTrophy;
 	}
 
+	get isBackground() {
+		return this.type === QuestRewardTypes.background;
+	}
+
 	get name() {
 		let result = this.fallback_name;
 		switch (this.type) {
@@ -62,6 +79,10 @@ export class QuestObjectiveReward extends Model {
 
 			case QuestRewardTypes.siteTrophy:
 				result = 'Trophy';
+				break;
+
+			case QuestRewardTypes.background:
+				result = 'Background';
 				break;
 		}
 
