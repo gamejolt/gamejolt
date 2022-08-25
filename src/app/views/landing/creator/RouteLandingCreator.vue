be4171968cc553b26a996e9dede9e570b8416968
hapurubokka <somekid11@gmail.com>
2022-08-25 13:57:26 -0700

    change shadow for creator card

--- src/app/views/landing/creator/RouteLandingCreator.vue
+++ src/app/views/landing/creator/RouteLandingCreator.vue
@@ -1055,7 +1055,7 @@ function getRandomStickers(count = 3) {
 
 .-who-card-names
 	text-overflow()
-	filter: drop-shadow(0px 1px 8px rgba(0, 0, 0, 0.8))
+	overlay-text-shadow()
 
 .-who-card-displayname
 	font-weight: bold
