535a9815c3432628bd3a613ec45d30b05c5d5f63
hapurubokka <somekid11@gmail.com>
2022-09-20 13:29:05 -0700

    Fix incorrect FiresidePostRealm fields

--- src/_common/fireside/post/realm/realm.model.ts
+++ src/_common/fireside/post/realm/realm.model.ts
@@ -1,19 +1,13 @@
 import { Model } from '../../../model/model.service';
 import { Realm } from '../../../realm/realm-model';
-import { FiresidePost } from '../post-model';
 
 export class FiresidePostRealm extends Model {
-	// TODO(realm-posting) double-check
-	fireside_post!: FiresidePost;
+	fireside_post_id!: number;
 	realm!: Realm;
 	added_on!: number;
 
 	constructor(data: any = {}) {
 		super(data);
-
-		if (data.fireside_post) {
-			this.fireside_post = new FiresidePost(data.fireside_post);
-		}
 
 		if (data.realm) {
 			this.realm = new Realm(data.realm);
