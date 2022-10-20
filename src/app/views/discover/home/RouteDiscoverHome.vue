468f76cd1282ddb88f00839b9590a0c131dd992c
hapurubokka <somekid11@gmail.com>
2022-10-20 12:42:54 -0700

    Remove web_guest_home_discover test

--- src/app/views/discover/home/RouteDiscoverHome.vue
+++ src/app/views/discover/home/RouteDiscoverHome.vue
@@ -1,11 +1,9 @@
 <script lang="ts">
-import { computed, ref } from 'vue';
+import { computed, ref, shallowRef } from 'vue';
 import { useRoute } from 'vue-router';
 import { arrayShuffle } from '../../../../utils/array';
-import { trackExperimentEngagement } from '../../../../_common/analytics/analytics.service';
 import { Api } from '../../../../_common/api/api.service';
 import { Community } from '../../../../_common/community/community.model';
-import { configGuestHomeDiscover } from '../../../../_common/config/config.service';
 import { Environment } from '../../../../_common/environment/environment.service';
 import { Fireside } from '../../../../_common/fireside/fireside.model';
 import { FiresidePost } from '../../../../_common/fireside/post/post-model';
@@ -42,16 +40,9 @@ const featuredItem = ref<FeaturedItem>();
 const featuredCommunities = ref<Community[]>([]);
 const featuredFireside = ref<Fireside>();
 const featuredRealms = ref<Realm[]>([]);
-const heroPosts = ref<FiresidePost[]>([]);
-const creatorPosts = ref<FiresidePost[]>([]);
 
-const homeView = computed(() => {
-	if (user.value) {
-		return 'discover';
-	}
-
-	return configGuestHomeDiscover.value ? 'discover' : 'slider';
-});
+const heroPosts = shallowRef<FiresidePost[]>([]);
+const creatorPosts = shallowRef<FiresidePost[]>([]);
 
 const { isBootstrapped } = createAppRoute({
 	routeTitle: computed(() => (user.value ? $gettext(`Discover`) : null)),
@@ -116,10 +107,6 @@ const { isBootstrapped } = createAppRoute({
 				: [];
 			HistoryCache.store(route, creatorPosts.value, CachedCreatorsKey);
 		}
-
-		if (!user.value) {
-			trackExperimentEngagement(configGuestHomeDiscover);
-		}
 	},
 });
 </script>
@@ -128,9 +115,9 @@ const { isBootstrapped } = createAppRoute({
 	<div v-if="!userBootstrapped" class="-load-container">
 		<AppLoading stationary hide-label />
 	</div>
-	<AppHomeSlider v-if="homeView === 'slider'" :posts="heroPosts" />
+
 	<AppHomeDefault
-		v-else
+		v-if="user"
 		:is-bootstrapped="isBootstrapped"
 		:featured-item="featuredItem"
 		:featured-communities="featuredCommunities"
@@ -138,6 +125,7 @@ const { isBootstrapped } = createAppRoute({
 		:featured-realms="featuredRealms"
 		:creator-posts="creatorPosts"
 	/>
+	<AppHomeSlider v-else :posts="heroPosts" />
 </template>
 
 <style lang="stylus" scoped>
