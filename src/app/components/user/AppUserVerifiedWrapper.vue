720b900747e681649b4aeb48957ab35e806240e8
hapurubokka <somekid11@gmail.com>
2023-01-02 12:48:17 -0800

    formatFuzzynumberOverThreshold, VerifiedWrapper

---
+++ src/app/components/user/AppUserVerifiedWrapper.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import { PropType } from 'vue';
+import { User } from '../../../_common/user/user.model';
+import AppUserVerifiedTick from '../../../_common/user/verified-tick/AppUserVerifiedTick.vue';
+import { ChatUser } from '../chat/user';
+
+defineProps({
+	user: {
+		type: Object as PropType<User | ChatUser>,
+		required: true,
+	},
+	small: {
+		type: Boolean,
+	},
+	tiny: {
+		type: Boolean,
+	},
+});
+</script>
+
+<template>
+	<div class="user-verified-wrapper">
+		<slot name="default" class="-inner" />
+
+		<AppUserVerifiedTick class="-floating-tick" :user="user" :small="small" :tiny="tiny" />
+	</div>
+</template>
+
+<style lang="stylus" scoped>
+.user-verified-wrapper
+	position: relative
+
+.-inner
+	z-index: 1
+
+.-floating-tick
+	img-circle()
+	background-color: var(--theme-bg-actual)
+	position: absolute
+	right: 0px
+	bottom: 0px
+	transform: translate(20%, 20%)
+	margin: 0
+	z-index: 2
+	color: var(--theme-fg)
+	padding: 1px
+</style>
