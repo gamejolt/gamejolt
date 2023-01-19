175b9cce492346cba760d44e31b8af7d8b5d54cf
YLivay <yarivlivay@gmail.com>
2023-01-20 01:50:12 +0200

    Revert "attempt 2 col"
    This reverts commit 2a8862e6354e7d83cab27519484b2c7844665f4b.

--- src/app/views/realms/view/RouteRealmsView.vue
+++ src/app/views/realms/view/RouteRealmsView.vue
@@ -1,11 +1,9 @@
 <script lang="ts">
 import { computed, provide, ref } from 'vue';
-import { RouterView, useRoute, useRouter } from 'vue-router';
+import { RouterView, useRouter } from 'vue-router';
 import { getAbsoluteLink } from '../../../../utils/router';
 import { Api } from '../../../../_common/api/api.service';
 import AppButton from '../../../../_common/button/AppButton.vue';
-import { Fireside } from '../../../../_common/fireside/fireside.model';
-import AppLoadingFade from '../../../../_common/loading/AppLoadingFade.vue';
 import AppRealmFollowButton from '../../../../_common/realm/AppRealmFollowButton.vue';
 import AppRealmFullCard from '../../../../_common/realm/AppRealmFullCard.vue';
 import { Realm } from '../../../../_common/realm/realm-model';
@@ -15,27 +13,23 @@ import AppScrollAffix from '../../../../_common/scroll/AppScrollAffix.vue';
 import AppShareCard from '../../../../_common/share/card/AppShareCard.vue';
 import { ShareModal } from '../../../../_common/share/card/_modal/modal.service';
 import AppSpacer from '../../../../_common/spacer/AppSpacer.vue';
+import AppTranslate from '../../../../_common/translate/AppTranslate.vue';
 import { User } from '../../../../_common/user/user.model';
-import { kLineHeightComputed } from '../../../../_styles/variables';
-import AppFiresideAvatarAdd from '../../../components/fireside/avatar/AppFiresideAvatarAdd.vue';
 import AppPageContainer from '../../../components/page-container/AppPageContainer.vue';
 import AppShellPageBackdrop from '../../../components/shell/AppShellPageBackdrop.vue';
 import AppUserKnownFollowers from '../../../components/user/known-followers/AppUserKnownFollowers.vue';
-import { routeRealmsViewFiresides } from './firesides/firesides.route';
-import { routeRealmsOverview } from './overview/overview.route';
 import { createRealmRouteStore, RealmRouteStoreKey } from './view.store';
 
 export default {
 	...defineAppRouteOptions({
 		resolver: async ({ route }) => Api.sendRequest('/web/realms/' + route.params.path),
 	}),
-	components: { RouterView, AppFiresideAvatarAdd, AppLoadingFade },
+	components: { RouterView },
 };
 </script>
 
 <script lang="ts" setup>
 const router = useRouter();
-const route = useRoute();
 
 const routeStore = createRealmRouteStore();
 provide(RealmRouteStoreKey, routeStore);
@@ -49,27 +43,11 @@ const shareLink = computed(() =>
 		: undefined
 );
 
-const isBootstrapped = ref(false);
-const firesides = ref<Fireside[]>([]);
-
-const shouldShowFiresidePreview = computed(() => route.name === routeRealmsOverview.name);
-
-// How many firesides to show in the preview row, including the "add a fireside".
-const firesidesGridColumns = 3;
-
-const displayablePreviewFiresides = computed(() => {
-	// -1 to leave room for the "add a fireside"
-	const perRow = firesidesGridColumns - 1;
-	return Object.freeze(firesides.value.slice(0, perRow));
-});
-
 createAppRoute({
 	onResolved({ payload }) {
-		isBootstrapped.value = true;
 		routeStore.realm.value = new Realm(payload.realm);
 		knownFollowers.value = User.populate(payload.knownFollowers);
 		knownFollowerCount.value = payload.knownFollowerCount || 0;
-		firesides.value = Fireside.populate(payload.activeFiresides);
 	},
 });
 
@@ -108,7 +86,7 @@ function onShareClick() {
 								trans
 								@click="onShareClick"
 							>
-								{{ $gettext(`Share`) }}
+								<AppTranslate>Share</AppTranslate>
 							</AppButton>
 						</h1>
 
@@ -133,55 +111,16 @@ function onShareClick() {
 							:count="knownFollowerCount"
 						/>
 					</div>
-
+				</AppScrollAffix>
+			</template>
+			<template #right>
+				<AppScrollAffix :disabled="!Screen.isLg">
 					<AppShareCard
 						v-if="shareLink && Screen.isDesktop"
 						resource="realm"
 						:url="shareLink"
 					/>
 				</AppScrollAffix>
-			</template>
-			<template v-if="shouldShowFiresidePreview" #right>
-				<div
-					:style="{
-						display: `flex`,
-						justifyContent: `space-between`,
-					}"
-				>
-					<h4 class="section-header">
-						{{ $gettext(`Firesides`) }}
-					</h4>
-
-					<AppButton
-						trans
-						:to="{
-							name: routeRealmsViewFiresides.name,
-							params: { path: routeStore.realm.value.path },
-						}"
-					>
-						{{ $gettext(`View All`) }}
-					</AppButton>
-				</div>
-
-				<AppLoadingFade :is-loading="!isBootstrapped">
-					<div
-						:style="{
-							gridTemplateColumns: `repeat(${firesidesGridColumns}, 1fr)`,
-							display: `grid`,
-							gridGap: `${kLineHeightComputed}px`,
-							marginBottom: `${kLineHeightComputed}px`,
-						}"
-					>
-						<AppFiresideAvatarAdd :realms="[routeStore.realm.value]" />
-
-						<AppFiresideAvatarAdd
-							v-for="fireside in displayablePreviewFiresides"
-							:key="fireside.id"
-							:fireside="fireside"
-							hide-realm
-						/>
-					</div>
-				</AppLoadingFade>
 			</template>
 			<template #default>
 				<RouterView />
--- src/app/views/realms/view/overview/RouteRealmsOverview.vue
+++ src/app/views/realms/view/overview/RouteRealmsOverview.vue
@@ -2,16 +2,24 @@
 import { computed, Ref, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { Api } from '../../../../../_common/api/api.service';
+import AppButton from '../../../../../_common/button/AppButton.vue';
+import { Fireside } from '../../../../../_common/fireside/fireside.model';
 import { FiresidePost } from '../../../../../_common/fireside/post/post-model';
+import AppLoadingFade from '../../../../../_common/loading/AppLoadingFade.vue';
 import {
 	createAppRoute,
 	defineAppRouteOptions,
 } from '../../../../../_common/route/route-component';
+import AppTranslate from '../../../../../_common/translate/AppTranslate.vue';
 import { $gettextInterpolate } from '../../../../../_common/translate/translate.service';
+import { kLineHeightComputed } from '../../../../../_styles/variables';
 import { ActivityFeedService } from '../../../../components/activity/feed/feed-service';
 import { ActivityFeedView } from '../../../../components/activity/feed/view';
+import AppFiresideAvatar from '../../../../components/fireside/avatar/AppFiresideAvatar.vue';
+import AppFiresideAvatarAdd from '../../../../components/fireside/avatar/AppFiresideAvatarAdd.vue';
 import { AppActivityFeedLazy } from '../../../../components/lazy';
 import AppPostAddButton from '../../../../components/post/add-button/AppPostAddButton.vue';
+import { routeRealmsViewFiresides } from '../firesides/firesides.route';
 import { useRealmRouteStore } from '../view.store';
 
 export default {
@@ -33,7 +41,17 @@ const router = useRouter();
 const route = useRoute();
 
 const feed = ref(null) as Ref<ActivityFeedView | null>;
+const firesides = ref<Fireside[]>([]);
 const isBootstrapped = ref(false);
+
+// How many firesides to show in the preview row, including the "add a fireside".
+const firesidesGridColumns = 5;
+
+const displayablePreviewFiresides = computed(() => {
+	// -1 to leave room for the "add a fireside"
+	const perRow = firesidesGridColumns - 1;
+	return Object.freeze(firesides.value.slice(0, perRow));
+});
 
 const appRoute = createAppRoute({
 	routeTitle: computed(() =>
@@ -61,6 +79,9 @@ const appRoute = createAppRoute({
 			payload.items,
 			fromCache
 		);
+
+		// TODO(fireside-realms) this does not exist on this payload. need a new overview endpoint.
+		firesides.value = []; // Fireside.populate(payload.activeFiresides);
 	},
 });
 
@@ -77,6 +98,47 @@ function onPostAdded(post: FiresidePost) {
 
 <template>
 	<div>
+		<div
+			:style="{
+				display: `flex`,
+				justifyContent: `space-between`,
+			}"
+		>
+			<h4 class="section-header">
+				<AppTranslate>Firesides</AppTranslate>
+			</h4>
+
+			<AppButton
+				trans
+				:to="{
+					name: routeRealmsViewFiresides.name,
+					params: { path: routeStore.realm.value.path },
+				}"
+			>
+				<AppTranslate>View All</AppTranslate>
+			</AppButton>
+		</div>
+
+		<AppLoadingFade :is-loading="!isBootstrapped">
+			<div
+				:style="{
+					gridTemplateColumns: `repeat(${firesidesGridColumns}, 1fr)`,
+					display: `grid`,
+					gridGap: `${kLineHeightComputed}px`,
+					marginBottom: `${kLineHeightComputed}px`,
+				}"
+			>
+				<AppFiresideAvatarAdd :realms="[routeStore.realm.value]" />
+
+				<AppFiresideAvatar
+					v-for="fireside in displayablePreviewFiresides"
+					:key="fireside.id"
+					:fireside="fireside"
+					hide-realm
+				/>
+			</div>
+		</AppLoadingFade>
+
 		<AppPostAddButton
 			:realm="routeStore.realm.value"
 			:placeholder="
