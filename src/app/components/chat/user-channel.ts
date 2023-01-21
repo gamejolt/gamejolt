055757c974522b8ae16abfa06bea873e5f269de7
David DeCarmine <hworld@holo-world.com>
2023-01-20 16:48:02 -0800

    small tweaks

--- src/app/components/chat/user-channel.ts
+++ src/app/components/chat/user-channel.ts
@@ -20,7 +20,6 @@ import { ChatMessage } from './message';
 import { ChatNotificationGrowl } from './notification-growl/notification-growl.service';
 import { ChatRoom } from './room';
 import { ChatUser } from './user';
-import { ChatUserCollection } from './user-collection';
 
 const TabLeaderLazy = importNoSSR(async () => await import('../../../utils/tab-leader'));
 
