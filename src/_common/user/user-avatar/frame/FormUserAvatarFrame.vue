a6c03fd220a6f0ff3c5990a6948a6e8bd3e12d0d
hapurubokka <somekid11@gmail.com>
2023-02-01 15:29:16 -0800

    rewrite avatar frame form

--- src/_common/user/user-avatar/frame/FormUserAvatarFrame.vue
+++ src/_common/user/user-avatar/frame/FormUserAvatarFrame.vue
@@ -1,38 +1,18 @@
 <script lang="ts" setup>
-import { computed, onUnmounted, ref } from 'vue';
+import { ref } from 'vue';
 import { illNoCommentsSmall } from '../../../../app/img/ill/illustrations';
-import {
-	styleBorderRadiusBase,
-	styleBorderRadiusCircle,
-	styleBorderRadiusLg,
-} from '../../../../_styles/mixins';
-import {
-	kBorderRadiusLg,
-	kBorderWidthLg,
-	kFontSizeSmall,
-	kStrongEaseOut,
-} from '../../../../_styles/variables';
 import { Api } from '../../../api/api.service';
-import AppAspectRatio from '../../../aspect-ratio/AppAspectRatio.vue';
-import AppAvatarFrame from '../../../avatar/AppAvatarFrame.vue';
-import { shorthandReadableTime } from '../../../filters/duration';
 import AppForm, { createForm, FormController } from '../../../form-vue/AppForm.vue';
 import AppFormButton from '../../../form-vue/AppFormButton.vue';
+import AppFormGroup from '../../../form-vue/AppFormGroup.vue';
 import AppFormStickySubmit from '../../../form-vue/AppFormStickySubmit.vue';
 import { showErrorGrowl } from '../../../growls/growls.service';
 import AppIllustration from '../../../illustration/AppIllustration.vue';
 import AppSpacer from '../../../spacer/AppSpacer.vue';
 import { useCommonStore } from '../../../store/common-store';
-import {
-	kThemeBgOffset,
-	kThemeBgSubtle,
-	kThemeDark,
-	kThemeFg10,
-	kThemeGjOverlayNotice,
-} from '../../../theme/variables';
 import { $gettext } from '../../../translate/translate.service';
-import AppUserAvatarImg from '../AppUserAvatarImg.vue';
 import { UserAvatarFrame } from './frame.model';
+import AppUserAvatarFrameSelector from './_selector/AppUserAvatarFrameSelector.vue';
 
 interface FormModel {
 	avatar_frame: number | undefined;
@@ -40,24 +20,15 @@ interface FormModel {
 
 const { user } = useCommonStore();
 
-const placeholderFrames = [null, null];
-
-const expiryInfoKey = ref(-1);
 const availableFrames = ref<UserAvatarFrame[]>([]);
-
-const displayFrames = computed(() =>
-	availableFrames.value.length ? [null, ...availableFrames.value] : []
-);
-
-const myFrameId = computed(() => user.value?.avatar_frame?.id || 0);
 
 const form: FormController<FormModel> = createForm({
 	loadUrl: '/web/dash/profile/save',
-	onInit() {
-		form.formModel.avatar_frame = myFrameId.value;
-	},
 	onLoad(payload) {
 		availableFrames.value = UserAvatarFrame.populate(payload.userAvatarFrames);
+
+		form.formModel.avatar_frame =
+			availableFrames.value.find(i => i.is_active)?.avatar_frame.id || 0;
 	},
 	async onSubmit() {
 		return Api.sendRequest(`/web/dash/profile/save`, {
@@ -71,146 +42,25 @@ const form: FormController<FormModel> = createForm({
 		user.value?.processUpdate(response, 'user');
 	},
 });
-
-const interval = setInterval(() => (expiryInfoKey.value *= -1), 1_000);
-
-onUnmounted(() => {
-	clearInterval(interval);
-});
-
-function pickFrame(frameId: number) {
-	if (!form.isLoaded) {
-		return;
-	}
-
-	form.formModel.avatar_frame = frameId;
-	form.changed = form.formModel.avatar_frame !== myFrameId.value;
-}
-
-function isSelected(data: UserAvatarFrame | null) {
-	const frameId = data?.avatar_frame.id || 0;
-	return frameId === form.formModel.avatar_frame;
-}
 </script>
 
 <template>
+	<!-- FormUserAvatarFrame -->
 	<AppForm :controller="form">
-		<template v-if="form.isLoaded && !displayFrames.length">
-			<AppIllustration :asset="illNoCommentsSmall" sm>
-				{{ $gettext(`You have no available avatar frames.`) }}
-			</AppIllustration>
-
-			<AppSpacer vertical :scale="4" />
-		</template>
-		<template v-else>
-			<div
-				:style="{
-					display: `grid`,
-					gap: `8px`,
-					gridTemplateColumns: `repeat(auto-fill, minmax(120px, 1fr))`,
-				}"
-			>
-				<div
-					v-for="(data, index) of form.isLoaded ? displayFrames : placeholderFrames"
-					:key="data?.avatar_frame.id ?? `no-frame-${index}`"
-					:style="[
-						form.isLoaded
-							? {
-									cursor: data?.isExpired ? 'normal' : 'pointer',
-									backgroundColor: isSelected(data)
-										? kThemeBgOffset
-										: `transparent`,
-							  }
-							: {
-									backgroundColor: kThemeBgSubtle,
-							  },
-						{
-							position: `relative`,
-							padding: `24px`,
-							borderRadius: kBorderRadiusLg.px,
-							transition: `background-color 300ms ${kStrongEaseOut}`,
-							...styleBorderRadiusLg,
-							border: `${kBorderWidthLg.px} solid ${kThemeFg10}`,
-						},
-					]"
-					@click="data?.isExpired ? undefined : pickFrame(data?.avatar_frame.id || 0)"
-				>
-					<AppAspectRatio
-						:style="{
-							width: `100%`,
-						}"
-						:ratio="1"
-						show-overflow
-					>
-						<template v-if="form.isLoaded">
-							<div
-								:style="{
-									position: `absolute`,
-									left: 0,
-									top: 0,
-									right: 0,
-									bottom: 0,
-									display: `grid`,
-									alignItems: `center`,
-								}"
-							>
-								<div
-									v-if="!data"
-									:style="{
-										textAlign: `center`,
-										fontWeight: `bold`,
-										fontSize: kFontSizeSmall.px,
-									}"
-								>
-									{{ $gettext(`No frame`) }}
-								</div>
-								<AppAvatarFrame v-else :frame="data.avatar_frame">
-									<AppAspectRatio :ratio="1">
-										<Transition name="fade">
-											<AppUserAvatarImg
-												v-if="isSelected(data)"
-												:style="{
-													backgroundColor: kThemeDark,
-													...styleBorderRadiusCircle,
-												}"
-												:user="user"
-											/>
-										</Transition>
-									</AppAspectRatio>
-								</AppAvatarFrame>
-							</div>
-						</template>
-					</AppAspectRatio>
-
-					<div
-						v-if="data && data.expires_on"
-						:key="expiryInfoKey"
-						:style="{
-							position: `absolute`,
-							top: `8px`,
-							left: `8px`,
-							padding: `2px 6px`,
-							backgroundColor: `rgba(0, 0, 0, 0.54)`,
-							color: data.isExpired ? kThemeGjOverlayNotice : `white`,
-							fontWeight: `bold`,
-							fontSize: kFontSizeSmall.px,
-							...styleBorderRadiusBase,
-							zIndex: 3,
-							pointerEvents: `none`,
-						}"
-					>
-						{{
-							data.isExpired
-								? $gettext(`Expired`)
-								: shorthandReadableTime(data.expires_on, {
-										allowFuture: true,
-										precision: 'rough',
-										nowText: $gettext(`Expired`),
-								  })
-						}}
-					</div>
-				</div>
-			</div>
+		<AppFormGroup
+			:style="{
+				marginBottom: 0,
+			}"
+			name="avatar_frame"
+			hide-label
+		>
+			<AppUserAvatarFrameSelector :frames="availableFrames">
+				<template #no-items>
+					<AppIllustration :asset="illNoCommentsSmall" sm>
+						{{ $gettext(`You have no available avatar frames.`) }}
+					</AppIllustration>
+				</template>
+			</AppUserAvatarFrameSelector>
 
 			<AppSpacer vertical :scale="8" />
 
@@ -219,16 +69,6 @@ function isSelected(data: UserAvatarFrame | null) {
 					{{ $gettext(`Save avatar frame`) }}
 				</AppFormButton>
 			</AppFormStickySubmit>
-		</template>
+		</AppFormGroup>
 	</AppForm>
 </template>
-
-<style lang="stylus" scoped>
-.fade-enter-active
-.fade-leave-active
-	transition: opacity 200ms $strong-ease-out
-
-.fade-enter-from
-.fade-leave-to
-	opacity: 0
-</style>
