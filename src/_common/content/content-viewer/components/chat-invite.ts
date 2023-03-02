0f7d37a88e35dbcded8dbc97bd8386d0a6c32876
nilsgj <nils@gamejolt.com>
2023-03-02 22:20:25 +0100

    fix build

--- src/_common/content/content-viewer/components/chat-invite.ts
+++ src/_common/content/content-viewer/components/chat-invite.ts
@@ -8,12 +8,13 @@ export class AppContentViewerChatInvite extends Vue {
 
 	render() {
 		if (GJ_SECTION === 'app') {
+			// Can't include this directly in the import tag, otherwise builds
+			// will fail.
+			const path = '../../../../app/components/content/components/AppContentChatInvite.vue';
+
 			// Only render this component in the app section.
 			// It interacts with grid/chat and the cbar, which are app-exclusive.
-			const c = defineAsyncComponent(
-				() =>
-					import('../../../../app/components/content/components/AppContentChatInvite.vue')
-			);
+			const c = defineAsyncComponent(() => import(/* @vite-ignore */ path));
 			return h(c, {
 				key: this.contentData.attrs.id,
 				inviteId: this.contentData.attrs.id,
