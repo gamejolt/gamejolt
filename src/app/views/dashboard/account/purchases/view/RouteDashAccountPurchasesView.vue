28ceee03af42e63b12da473ba26aecd1170549de
hapurubokka <somekid11@gmail.com>
2023-05-22 11:02:05 -0700

    rewrite purchase view

--- src/app/views/dashboard/account/purchases/view/RouteDashAccountPurchasesView.vue
+++ src/app/views/dashboard/account/purchases/view/RouteDashAccountPurchasesView.vue
@@ -1,100 +1,106 @@
 <script lang="ts">
-import { setup } from 'vue-class-component';
-import { Options } from 'vue-property-decorator';
+import { Ref, computed, ref } from 'vue';
 import { Api } from '../../../../../../_common/api/api.service';
 import { formatCurrency } from '../../../../../../_common/filters/currency';
 import { formatDate } from '../../../../../../_common/filters/date';
-import AppGameThumbnailImg from '../../../../../../_common/game/thumbnail/AppGameThumbnailImg.vue';
+import { GamePackage } from '../../../../../../_common/game/package/package.model';
 import { Geo } from '../../../../../../_common/geo/geo.service';
+import AppMicrotransactionItem from '../../../../../../_common/microtransaction/AppMicrotransactionItem.vue';
+import { MicrotransactionProduct } from '../../../../../../_common/microtransaction/product.model';
 import { Order } from '../../../../../../_common/order/order.model';
 import { OrderPayment } from '../../../../../../_common/order/payment/payment.model';
 import {
-	BaseRouteComponent,
-	OptionsForRoute,
+	createAppRoute,
+	defineAppRouteOptions,
 } from '../../../../../../_common/route/route-component';
 import { Screen } from '../../../../../../_common/screen/screen-service';
 import { $gettext } from '../../../../../../_common/translate/translate.service';
 import { useAccountRouteController } from '../../RouteDashAccount.vue';
 import AppGamePackagePreview from './AppGamePackagePreview.vue';
 
-@Options({
-	name: 'RouteDashAccountPurchasesView',
-	components: {
-		AppGameThumbnailImg,
-		AppGamePackagePreview,
+export default {
+	...defineAppRouteOptions({
+		deps: { params: ['hash'] },
+		resolver: ({ route }) => Api.sendRequest('/web/dash/purchases/order/' + route.params.hash),
+	}),
+};
+</script>
+
+<script lang="ts" setup>
+const { heading } = useAccountRouteController()!;
+
+const order = ref(null) as Ref<Order | null>;
+
+const firstRefund = computed(() => {
+	if (
+		order.value &&
+		order.value._is_refunded &&
+		order.value.payments &&
+		order.value.payments[0] &&
+		order.value.payments[0].refunds
+	) {
+		return order.value.payments[0].refunds[0];
+	}
+	return null;
+});
+
+const billingAddress = computed(() => order.value?.billing_address);
+
+const hasVisiblePayment = computed(() => {
+	if (!order.value || !order.value.payments || !order.value.payments.length) {
+		return false;
+	}
+
+	return order.value.payments.some(
+		i => i.method !== OrderPayment.METHOD_CC_STRIPE || !!i.stripe_payment_source
+	);
+});
+
+const { isBootstrapped } = createAppRoute({
+	routeTitle: heading,
+	onInit() {
+		heading.value = $gettext(`Order Details`);
 	},
-})
-@OptionsForRoute({
-	deps: { params: ['hash'] },
-	resolver: ({ route }) => Api.sendRequest('/web/dash/purchases/order/' + route.params.hash),
-})
-export default class RouteDashAccountPurchasesView extends BaseRouteComponent {
-	routeStore = setup(() => useAccountRouteController()!);
+	onResolved({ payload }) {
+		order.value = new Order(payload.order);
+	},
+});
 
-	order: Order = null as any;
+function isGamePackage(
+	resource: GamePackage | MicrotransactionProduct | null
+): resource is GamePackage {
+	return resource instanceof GamePackage;
+}
 
-	readonly Geo = Geo;
-	readonly OrderPayment = OrderPayment;
-	readonly formatDate = formatDate;
-	readonly formatCurrency = formatCurrency;
-	readonly Screen = Screen;
-
-	get routeTitle() {
-		return this.routeStore.heading;
-	}
-
-	get firstRefund() {
-		if (
-			this.order._is_refunded &&
-			this.order.payments &&
-			this.order.payments[0] &&
-			this.order.payments[0].refunds
-		) {
-			return this.order.payments[0].refunds[0];
-		}
-		return null;
-	}
-
-	get billingAddress() {
-		return this.order.billing_address!;
-	}
-
-	routeCreated() {
-		this.routeStore.heading = $gettext(`Order Details`);
-	}
-
-	routeResolved($payload: any) {
-		this.order = new Order($payload.order);
-	}
+function isMicrotransactionProduct(resource: any): resource is MicrotransactionProduct {
+	return resource instanceof MicrotransactionProduct;
 }
 </script>
 
 <template>
-	<div v-if="isRouteBootstrapped">
+	<div v-if="isBootstrapped && order">
 		<p class="text-muted">
-			<AppTranslate :translate-params="{ orderId: order.id }">
-				Order #%{orderId}
-			</AppTranslate>
+			{{ $gettextInterpolate(`Order #%{ orderId }`, { orderId: order.id }) }}
 
 			<span class="dot-separator" />
 
-			<AppTranslate :translate-params="{ date: formatDate(order.completed_on, 'medium') }">
-				Ordered on %{date}
-			</AppTranslate>
+			{{
+				$gettextInterpolate(`Ordered on %{ date }`, {
+					date: formatDate(order.completed_on, 'medium'),
+				})
+			}}
 		</p>
 
 		<!--
 			If the order was canceled but without a refund (just disabled), then we can't show
 			this.
-		-->
-		<div v-if="order._is_refunded && firstRefund" class="alert alert-notice">
-			<AppTranslate
-				:translate-params="{
+		 -->
+		<div v-if="order && order._is_refunded && firstRefund" class="alert alert-notice">
+			{{
+				$gettextInterpolate(`This order was refunded on %{ date }.`, {
 					date: formatDate(firstRefund.created_on, 'medium'),
-				}"
-			>
-				This order was refunded on %{ date }.
-			</AppTranslate>
+				})
+			}}
 		</div>
 
 		<hr />
@@ -102,7 +108,7 @@ export default class RouteDashAccountPurchasesView extends BaseRouteComponent {
 		<div class="row">
 			<div v-if="billingAddress" class="col-sm-4">
 				<h4 class="section-header">
-					<AppTranslate>Billing</AppTranslate>
+					{{ $gettext(`Billing`) }}
 				</h4>
 
 				<div v-if="billingAddress.fullname">
@@ -137,19 +143,22 @@ export default class RouteDashAccountPurchasesView extends BaseRouteComponent {
 				</div>
 				<br />
 			</div>
-			<div class="col-sm-4">
+			<div v-if="hasVisiblePayment" class="col-sm-4">
 				<h4 :class="{ 'section-header': !Screen.isXs }">
-					<AppTranslate>Payment</AppTranslate>
+					{{ $gettext(`Payment`) }}
 				</h4>
 				<div v-for="payment of order.payments" :key="payment.id">
-					<template v-if="payment.method === OrderPayment.METHOD_CC_STRIPE">
-						<template v-if="payment.stripe_payment_source">
-							<span class="tag">
-								{{ payment.stripe_payment_source.brand }}
-							</span>
-							****
-							{{ payment.stripe_payment_source.last4 }}
-						</template>
+					<template
+						v-if="
+							payment.method === OrderPayment.METHOD_CC_STRIPE &&
+							payment.stripe_payment_source
+						"
+					>
+						<span class="tag">
+							{{ payment.stripe_payment_source.brand }}
+						</span>
+						****
+						{{ payment.stripe_payment_source.last4 }}
 					</template>
 					<template v-else-if="payment.method === OrderPayment.METHOD_PAYPAL">
 						<span class="tag"> PayPal </span>
@@ -163,23 +172,31 @@ export default class RouteDashAccountPurchasesView extends BaseRouteComponent {
 			</div>
 			<div class="col-sm-4">
 				<h4 :class="{ 'section-header': !Screen.isXs }">
-					<AppTranslate>Summary</AppTranslate>
+					{{ $gettext(`Summary`) }}
 				</h4>
 
 				<table class="-summary-table">
 					<tbody>
 						<tr>
-							<th><AppTranslate>Subtotal</AppTranslate></th>
-							<td>{{ formatCurrency(order.amount) }}</td>
+							<th>
+								{{ $gettext(`Subtotal`) }}
+							</th>
+							<td>
+								{{ formatCurrency(order.amount) }}
+							</td>
 						</tr>
 						<tr v-if="order.tax_amount">
-							<th><AppTranslate>Tax</AppTranslate></th>
+							<th>
+								{{ $gettext(`Tax`) }}
+							</th>
 							<td>{{ formatCurrency(order.tax_amount) }}</td>
 						</tr>
 					</tbody>
 					<tfoot>
 						<tr>
-							<th><AppTranslate>Total</AppTranslate></th>
+							<th>
+								{{ $gettext(`Total`) }}
+							</th>
 							<td>{{ formatCurrency(order.total_amount) }}</td>
 						</tr>
 					</tfoot>
@@ -192,19 +209,22 @@ export default class RouteDashAccountPurchasesView extends BaseRouteComponent {
 		<div v-for="item of order.items" :key="item.id">
 			<h4>
 				<span v-if="item.is_refunded" class="tag tag-notice">
-					<AppTranslate>Refunded</AppTranslate>
+					{{ $gettext(`Refunded`) }}
 				</span>
 				{{ item.sellable.title }}
 				&mdash;
 				<small>{{ formatCurrency(item.amount) }}</small>
 			</h4>
 
-			<template v-if="item.sellable.resource_type === 'Game_Package'">
+			<template v-if="isGamePackage(item.sellable.resource_model)">
 				<AppGamePackagePreview
+					v-if="item.sellable.resource_model.game"
 					:game="item.sellable.resource_model.game"
 					:game-package="item.sellable.resource_model"
 				/>
-				<!-- TODO: MTX product visualization -->
+			</template>
+			<template v-else-if="isMicrotransactionProduct(item.sellable.resource_model)">
+				<AppMicrotransactionItem :item="item.sellable.resource_model" />
 			</template>
 		</div>
 	</div>
--- src/app/views/dashboard/account/purchases/view/view.route.ts
+++ src/app/views/dashboard/account/purchases/view/view.route.ts
@@ -3,5 +3,5 @@ import { RouteRecordRaw } from 'vue-router';
 export const routeDashAccountPurchasesView: RouteRecordRaw = {
 	name: 'dash.account.purchases.view',
 	path: 'purchases/view/:hash',
-	component: () => import('./view.vue'),
+	component: () => import('./RouteDashAccountPurchasesView.vue'),
 };
