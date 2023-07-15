6b0785add5ade85dcb620ee6952ffb70c55ab3f3
hapurubokka <somekid11@gmail.com>
2023-07-14 17:26:08 -0700

    Remove AppScrollAffix from vending vance face

--- src/app/components/vending-machine/modal/modal.service.ts
+++ src/app/components/vending-machine/modal/modal.service.ts
@@ -1,11 +1,10 @@
-import { computed, defineAsyncComponent } from 'vue';
+import { defineAsyncComponent } from 'vue';
 import { showModal } from '../../../../_common/modal/modal.service';
-import { Screen } from '../../../../_common/screen/screen-service';
 
 export async function showVendingMachineModal() {
 	return await showModal<void>({
 		modalId: 'VendingMachine',
 		component: defineAsyncComponent(() => import('./AppVendingMachineModal.vue')),
-		size: computed(() => (Screen.height < 1080 ? 'sm' : 'lg')),
+		size: 'lg',
 	});
 }
