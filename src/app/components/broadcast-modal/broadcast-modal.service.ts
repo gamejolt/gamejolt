ce5f7f9929285ab43bb668dca91e526cd0cc45d7
ramagj <93935871+ramagj@users.noreply.github.com>
2023-09-08 02:04:43 +0800

    Simplify broadcasts
    -Simplify broadcasts by showing just showing the latest post
    -convert static functions into non static functions

--- src/app/components/broadcast-modal/broadcast-modal.service.ts
+++ src/app/components/broadcast-modal/broadcast-modal.service.ts
@@ -8,42 +8,40 @@ import { commonStore } from '../../../_common/store/common-store';
 
 const STORAGE_KEY_PREFIX = 'broadcast-modal:date:';
 
-export class BroadcastModal {
-	private static _key() {
-		return STORAGE_KEY_PREFIX + commonStore.user.value!.id;
+function _key() {
+	return STORAGE_KEY_PREFIX + commonStore.user.value!.id;
+}
+
+async function _show(posts: FiresidePostModel[]) {
+	await showModal({
+		modalId: 'Broadcast',
+		component: defineAsyncComponent(() => import('./AppBroadcastModal.vue')),
+		props: { posts },
+		noBackdropClose: true,
+	});
+}
+
+export async function checkBroadcastModal() {
+	const { user } = commonStore;
+	if (!user.value || !SettingBroadcastModal.get() || import.meta.env.SSR || Screen.isXs) {
+		return;
 	}
 
-	static async check() {
-		const { user } = commonStore;
-		if (!user.value || !SettingBroadcastModal.get() || import.meta.env.SSR || Screen.isXs) {
-			return;
-		}
-
-		const localStorage = window.localStorage as any;
-		if (!localStorage[this._key()]) {
-			localStorage[this._key()] = Date.now();
-		}
-
-		const payload = await Api.sendRequest(
-			'/web/broadcasts?from=' + localStorage[this._key()],
-			undefined,
-			{ detach: true }
-		);
-
-		if (payload.broadcasts.length) {
-			const posts = FiresidePostModel.populate(payload.broadcasts);
-			this.show(posts);
-		}
-
-		localStorage[this._key()] = Date.now();
+	const localStorage = window.localStorage as any;
+	if (!localStorage[_key()]) {
+		localStorage[_key()] = Date.now();
 	}
 
-	private static async show(posts: FiresidePostModel[]) {
-		await showModal({
-			modalId: 'Broadcast',
-			component: defineAsyncComponent(() => import('./AppBroadcastModal.vue')),
-			props: { posts },
-			noBackdropClose: true,
-		});
+	const payload = await Api.sendRequest(
+		'/web/broadcasts?from=' + localStorage[_key()],
+		undefined,
+		{ detach: true }
+	);
+
+	if (payload.broadcasts.length) {
+		const posts = FiresidePostModel.populate(payload.broadcasts);
+		_show(posts);
 	}
+
+	localStorage[_key()] = Date.now();
 }
