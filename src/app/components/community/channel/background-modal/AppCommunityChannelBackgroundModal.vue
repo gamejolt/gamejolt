9b462f85b2f66ad74e8b5c121413f40ad7875f12
ramagj <93935871+ramagj@users.noreply.github.com>
2023-10-19 02:39:50 +0800

    AppCommunityChannelBackgroundModal

---
+++ src/app/components/community/channel/background-modal/AppCommunityChannelBackgroundModal.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import { PropType, ref, toRefs } from 'vue';
+import AppButton from '../../../../../_common/button/AppButton.vue';
+import { CommunityChannelModel } from '../../../../../_common/community/channel/channel.model';
+import AppModal from '../../../../../_common/modal/AppModal.vue';
+import { useModal } from '../../../../../_common/modal/modal.service';
+import { $gettext } from '../../../../../_common/translate/translate.service';
+import FormCommunityChannelBackground from '../../../forms/community/channel/background/background.vue';
+
+const props = defineProps({
+	channel: {
+		type: Object as PropType<CommunityChannelModel>,
+		required: true,
+	},
+});
+
+const { channel } = toRefs(props);
+const modal = useModal()!;
+
+const previousBackgroundId = ref<number | null>(null);
+
+if (channel.value.background) {
+	previousBackgroundId.value = channel.value.background.id;
+}
+
+function onSubmit(channel: CommunityChannelModel) {
+	const newBackgroundId = (channel.background && channel.background.id) || null;
+	if (previousBackgroundId.value === newBackgroundId) {
+		modal.resolve(channel);
+	}
+	previousBackgroundId.value = newBackgroundId;
+}
+</script>
+
+<template>
+	<AppModal>
+		<div class="modal-controls">
+			<AppButton @click="modal.dismiss()">
+				{{ $gettext(`Close`) }}
+			</AppButton>
+		</div>
+
+		<div class="modal-body">
+			<FormCommunityChannelBackground :model="channel" @submit="onSubmit" />
+		</div>
+	</AppModal>
+</template>
