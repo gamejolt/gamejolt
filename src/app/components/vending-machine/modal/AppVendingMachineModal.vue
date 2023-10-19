b7d256064bb2d0bd54d6eda54009a993fb7cf96c
hapurubokka <somekid11@gmail.com>
2023-10-19 14:26:22 -0700

    parse sections

--- src/app/components/vending-machine/modal/AppVendingMachineModal.vue
+++ src/app/components/vending-machine/modal/AppVendingMachineModal.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { CSSProperties, Ref, computed, onMounted, ref, toRefs } from 'vue';
+import { CSSProperties, computed, onMounted, ref, toRefs } from 'vue';
 import { trackShopView } from '../../../../_common/analytics/analytics.service';
 import { Api } from '../../../../_common/api/api.service';
 import AppAspectRatio from '../../../../_common/aspect-ratio/AppAspectRatio.vue';
@@ -16,6 +16,7 @@ import { showErrorGrowl } from '../../../../_common/growls/growls.service';
 import AppIllustration from '../../../../_common/illustration/AppIllustration.vue';
 import { illNoCommentsSmall } from '../../../../_common/illustration/illustrations';
 import { InventoryShopProductSaleModel } from '../../../../_common/inventory/shop/inventory-shop-product-sale.model';
+import { InventoryShopSectionModel } from '../../../../_common/inventory/shop/inventory-shop-section.model';
 import AppJolticon from '../../../../_common/jolticon/AppJolticon.vue';
 import AppLoadingFade from '../../../../_common/loading/AppLoadingFade.vue';
 import { showPurchaseMicrotransactionModal } from '../../../../_common/microtransaction/purchase-modal/modal.service';
@@ -36,6 +37,7 @@ import {
 	kThemeBgOffset,
 	kThemeFg,
 	kThemeFg10,
+	kThemeFgMuted,
 } from '../../../../_common/theme/variables';
 import { vAppTooltip } from '../../../../_common/tooltip/tooltip-directive';
 import { $gettext } from '../../../../_common/translate/translate.service';
@@ -59,20 +61,17 @@ import {
 	kFontSizeH2,
 	kStrongEaseOut,
 } from '../../../../_styles/variables';
+import { numberSort } from '../../../../utils/array';
 import { showGetCoinsRedirectModal } from './_get-coins-redirect-modal/modal.service';
 import AppVendingMachineProduct from './_product/AppVendingMachineProduct.vue';
 import { showPurchaseShopProductModal } from './_purchase-modal/modal.service';
 import imageVance from './vance.png';
 
-interface ProductChunks {
-	packs?: ProductChunk;
-	avatarFrames?: ProductChunk;
-	backgrounds?: ProductChunk;
-}
-
-interface ProductChunk {
-	items: InventoryShopProductSaleModel[];
-	type: keyof ProductChunks;
+interface Section {
+	title: string;
+	description: string;
+	sort: number;
+	items: { product: InventoryShopProductSaleModel; sort: number | undefined }[];
 }
 
 const props = defineProps({
@@ -90,19 +89,19 @@ const { coinBalance, joltbuxBalance } = useCommonStore();
 const modal = useModal()!;
 
 const shopOwner = ref<UserModel>();
-const availableProducts = ref({}) as Ref<ProductChunks>;
+const sections = ref(new Map<string, Section>());
+
 const isLoading = ref(false);
 const productProcessing = ref<number>();
 
 const hasProducts = computed(() => {
-	const e = availableProducts.value;
-
-	return (
-		(e.packs?.items.length ||
-			e.avatarFrames?.items.length ||
-			e.backgrounds?.items.length ||
-			0) > 0
-	);
+	const data = sections.value.values();
+	for (const { items } of data) {
+		if (items.length > 0) {
+			return true;
+		}
+	}
+	return false;
 });
 
 const currencyCardData = computed(() => {
@@ -143,40 +142,64 @@ async function init() {
 			? storeModelList(InventoryShopProductSaleModel, payload.sales)
 			: [];
 
-		const productChunks: ProductChunks = {};
+		// TODO(shop-sections) improve
+		const newSections: typeof sections.value = new Map();
 		const missingItems: InventoryShopProductSaleModel[] = [];
+		const sectionItems = storeModelList(InventoryShopSectionModel, payload.sections);
 
-		for (const shopProduct of newProducts) {
-			let key: keyof ProductChunks | null = null;
-
-			if (shopProduct.stickerPack) {
-				key = 'packs';
-			} else if (shopProduct.avatarFrame) {
-				key = 'avatarFrames';
-			} else if (shopProduct.background) {
-				key = 'backgrounds';
-			}
-
-			if (!key) {
-				missingItems.push(shopProduct);
+		for (const product of newProducts) {
+			if (!product.stickerPack && !product.avatarFrame && !product.background) {
+				missingItems.push(product);
 				continue;
 			}
 
-			const items = (productChunks[key]?.items || []).concat(shopProduct);
-			productChunks[key] = { type: key, items };
-		}
-
-		if (missingItems.length > 0) {
-			console.warn(`Got some products that we don't support yet.`, {
-				missingCount: missingItems.length,
-				types: missingItems.reduce((result, i) => {
-					result.set(i.product_type, (result.get(i.product_type) || 0) + 1);
-					return result;
-				}, new Map<string, number>()),
+			let sort: number | undefined = undefined;
+			const section = sectionItems.find(section => {
+				const item = section.resource.items.find(item => item.sale_id === product.id);
+				if (item) {
+					sort = item.sort;
+					return true;
+				}
+				return false;
 			});
+
+			const sectionKey = section?.title || 'Default';
+			let sectionData = newSections.get(sectionKey);
+			if (!sectionData) {
+				sectionData = {
+					title: sectionKey,
+					description: section?.description || '',
+					sort: section?.sort || 100,
+					items: [],
+				};
+			}
+			sectionData.items.push({ product, sort });
+			newSections.set(sectionKey, sectionData);
 		}
 
-		availableProducts.value = productChunks;
+		const sortedSections: [string, Section][] = [];
+		for (const [key, section] of newSections) {
+			sortedSections.push([key, section]);
+		}
+		sortedSections.sort(([, a], [, b]) => numberSort(b.sort, a.sort));
+
+		newSections.clear();
+		for (const [key, section] of sortedSections) {
+			section.items.sort((a, b) => {
+				if (a.sort === undefined && b.sort === undefined) {
+					return 0;
+				} else if (a.sort === undefined) {
+					return 1;
+				} else if (b.sort === undefined) {
+					return -1;
+				}
+				return numberSort(a.sort, b.sort);
+			});
+			newSections.set(key, section);
+		}
+
+		sections.value = newSections;
+
 		if (payload.shopOwner) {
 			shopOwner.value = payload.shopOwner;
 		}
@@ -231,23 +254,6 @@ async function onClickCurrencyCard(currency: Currency) {
 		}
 	} else {
 		console.error('Unknown currency type', currency);
-	}
-}
-
-function getTooltipText(type: ProductChunk['type']) {
-	switch (type) {
-		case 'packs':
-			return $gettext(
-				`Sticker packs contain a random set of stickers which you can collect and place on content throughout Game Jolt.`
-			);
-
-		case 'avatarFrames':
-			return $gettext(`Equip an avatar frame to make yourself stand out in the community.`);
-
-		case 'backgrounds':
-			return $gettext(
-				`Backgrounds can be added to your posts to make your content stand out in the feeds.`
-			);
 	}
 }
 
@@ -495,89 +501,108 @@ const currencyCardTransitionStyles: CSSProperties = {
 						</div>
 
 						<div
-							:style="[
-								styleWhen(!hasProducts, {
-									padding: `12px`,
-								}),
+							:style="
 								styleWhen(!hasProducts && !isLoading, {
+									padding: `12px`,
 									gridTemplateColumns: '1fr',
 									alignContent: 'center',
-								}),
-							]"
+								})
+							"
 						>
-							<div v-if="isLoading && !hasProducts" class="_items">
-								<AppAspectRatio
-									v-for="i in 3"
-									:key="i"
-									:ratio="StickerPackRatio"
-									show-overflow
-								>
+							<div
+								v-if="isLoading && !hasProducts"
+								class="fill-offset"
+								:style="{
+									...styleBorderRadiusLg,
+									...styleElevate(1),
+									padding: `8px`,
+									marginLeft: `8px`,
+									marginRight: `8px`,
+									marginBottom: `12px`,
+								}"
+							>
+								<div
+									:style="{
+										...styleBorderRadiusLg,
+										height: kFontSizeH2.px,
+										margin: 0,
+										padding: `8px 12px 0`,
+										marginBottom: `12px`,
+										backgroundColor: kThemeFgMuted,
+										width: `25%`,
+									}"
+								/>
+
+								<div class="_items">
 									<div
+										v-for="i in 2"
+										:key="i"
 										:style="{
 											...styleBorderRadiusLg,
 											...styleChangeBg('bg-subtle'),
-											...styleElevate(1),
-											width: `100%`,
-											height: `100%`,
 										}"
-									/>
-								</AppAspectRatio>
+									>
+										<AppAspectRatio
+											:ratio="(StickerPackRatio + 1) / 2"
+											show-overflow
+										/>
+										<AppSpacer vertical :scale="12" />
+									</div>
+								</div>
 							</div>
 							<template v-else-if="hasProducts">
-								<template v-for="chunk in availableProducts" :key="chunk.type">
-									<template v-if="chunk && chunk.items.length">
+								<template v-for="[label, section] in sections" :key="label">
+									<div
+										v-if="section.items.length"
+										class="fill-offset"
+										:style="{
+											...styleBorderRadiusLg,
+											...styleElevate(1),
+											padding: `8px`,
+											marginLeft: `8px`,
+											marginRight: `8px`,
+											marginBottom: `12px`,
+										}"
+									>
 										<h2
 											:style="{
 												display: `flex`,
 												alignItems: `center`,
 												gap: `12px`,
-												marginTop: 0,
-												padding: `12px 16px 0`,
+												margin: 0,
+												padding: `8px 12px 12px`,
 												fontFamily: kFontFamilyDisplay,
 												fontWeight: `bold`,
 												color: kThemeFg,
 											}"
 										>
-											<template v-if="chunk.type === 'packs'">
-												{{ $gettext(`Sticker packs`) }}
-											</template>
-											<template v-else-if="chunk.type === 'avatarFrames'">
-												{{ $gettext(`Avatar frames`) }}
-											</template>
-											<template v-else-if="chunk.type === 'backgrounds'">
-												{{ $gettext(`Backgrounds`) }}
-											</template>
+											{{ label }}
 
 											<AppJolticon
-												v-app-tooltip.touchable="getTooltipText(chunk.type)"
-												icon="help-circle"
+												v-if="section.description"
+												v-app-tooltip.touchable="section.description"
+												icon="info-circle"
 												:style="{
-													fontSize: `inherit`,
 													margin: 0,
+													fontSize: `inherit`,
+													color: kThemeFgMuted,
 												}"
 											/>
 										</h2>
 
-										<AppSpacer vertical :scale="2" />
-
-										<div
-											class="_items"
-											:style="{
-												padding: `0 12px 16px`,
-											}"
-										>
+										<div class="_items">
 											<template
-												v-for="shopProduct in chunk.items"
-												:key="shopProduct.id"
+												v-for="{ product } in section.items"
+												:key="product.id"
 											>
 												<AppVendingMachineProduct
-													:shop-product="shopProduct"
+													:shop-product="product"
 													:disable-purchases="!!productProcessing"
 													@purchase="purchaseProduct($event)"
 												/>
 											</template>
 										</div>
-									</template>
+									</div>
 								</template>
 							</template>
 							<AppIllustration v-else :asset="illNoCommentsSmall">
@@ -586,6 +611,8 @@ const currencyCardTransitionStyles: CSSProperties = {
 								</div>
 							</AppIllustration>
 						</div>
+
+						<AppSpacer vertical :scale="2" />
 					</AppLoadingFade>
 				</AppTheme>
 
@@ -601,7 +628,7 @@ const currencyCardTransitionStyles: CSSProperties = {
 					<div
 						:style="{
 							...styleMaxWidthForOptions({
-								ratio: 1000 / 250,
+								ratio: 4,
 								maxHeight: Math.max(Screen.height * 0.15, 80),
 							}),
 							width: `100%`,
@@ -631,7 +658,6 @@ const currencyCardTransitionStyles: CSSProperties = {
 	--pack-min-width: 200px
 	color: var(--theme-fg)
 	position: relative
-	overflow: hidden
 	display: grid
 	grid-template-columns: repeat(auto-fill, minmax(var(--pack-min-width), 1fr))
 	align-content: start
