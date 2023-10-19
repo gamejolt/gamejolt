8358794ce8c19e636512922956c9cdcaf851135b
ramagj <93935871+ramagj@users.noreply.github.com>
2023-10-20 01:26:37 +0800

    Fix to render screenshots along with images for games

--- src/_common/lightbox/item/AppLightboxItem.vue
+++ src/_common/lightbox/item/AppLightboxItem.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { PropType, computed, nextTick, onMounted, ref, toRef, toRefs, watch } from 'vue';
 import { isInstance } from '../../../utils/utils';
+import { GameScreenshotModel } from '../../game/screenshot/screenshot.model';
 import { GameSketchfabModel } from '../../game/sketchfab/sketchfab.model';
 import { GameVideoModel } from '../../game/video/video.model';
 import AppImgResponsive from '../../img/AppImgResponsive.vue';
@@ -123,7 +124,9 @@ async function calcActive() {
 	<div ref="rootElem" class="media-bar-lightbox-item">
 		<div v-if="isActive || isNext || isPrev" class="-inner">
 			<!-- Media Item (screenshots/gifs/images) -->
-			<template v-if="isInstance(item, MediaItemModel)">
+			<template
+				v-if="isInstance(item, MediaItemModel) || isInstance(item, GameScreenshotModel)"
+			>
 				<div class="-embed">
 					<!-- The min/max will be the actual dimensions for the image thumbnail. -->
 					<AppMediaItemBackdrop
