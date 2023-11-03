44622f4bf45347a0667b20c97316bac0d5460598
ramagj <93935871+ramagj@users.noreply.github.com>
2023-11-04 02:56:45 +0800

    AppGameCollectionThumbnail resolve TODO

--- src/app/components/game/collection/thumbnail/AppGameCollectionThumbnail.vue
+++ src/app/components/game/collection/thumbnail/AppGameCollectionThumbnail.vue
@@ -17,28 +17,21 @@ const props = defineProps({
 
 const { collection } = toRefs(props);
 
-// TODO(component-setup-refactor): Can do this way?
 const tagHighlight = computed(() => {
-	const result = (() => {
-		switch (collection.value.type) {
-			case 'developer':
-				return "Developer's Games";
-			case 'followed':
-				return ' Followed Games';
-			case 'playlist':
-				return ' Playlist';
-			case 'bundle':
-				return ' Bundle';
-			// TODO(component-setup-refactor): 'tag' can be removed?
-			case 'tag':
-				return 'Tag';
-			case 'owned':
-				return 'Owned Games';
-			case 'recommended':
-				return 'Daily Mix';
-		}
-	})();
-	return $gettext(`%{ result }`, result);
+	switch (collection.value.type) {
+		case 'developer':
+			return $gettext("Developer's Games");
+		case 'followed':
+			return $gettext('Followed Games');
+		case 'playlist':
+			return $gettext('Playlist');
+		case 'bundle':
+			return $gettext('Bundle');
+		case 'owned':
+			return $gettext('Owned Games');
+		case 'recommended':
+			return $gettext('Daily Mix');
+	}
 });
 </script>
 
@@ -46,19 +39,6 @@ const tagHighlight = computed(() => {
 	<div class="collection-thumbnail">
 		<span v-if="!hideTag" class="tag tag-highlight">
 			{{ tagHighlight }}
-			<!--TODO(component-setup-refactor): Uncomment/remove after confirming the above TODOS
-				AppTranslate v-if="collection.type === 'developer'">Developer's Games</AppTranslate>
-			<AppTranslate v-else-if="collection.type === 'followed'"> Followed Games </AppTranslate>
-			<AppTranslate v-else-if="collection.type === 'playlist'"> Playlist </AppTranslate>
-			<AppTranslate v-else-if="collection.type === 'bundle'"> Bundle </AppTranslate>
-			<AppTranslate
-				v-else-if="collection.type === 'tag'"
-				translate-comment="The noun for a tag"
-			>
-				Tag
-			</AppTranslate>
-			<AppTranslate v-else-if="collection.type === 'owned'">Owned Games</AppTranslate>
-			<AppTranslate v-else-if="collection.type === 'recommended'">Daily Mix</AppTranslate-->
 		</span>
 
 		<AppJolticon icon="playlist" />
