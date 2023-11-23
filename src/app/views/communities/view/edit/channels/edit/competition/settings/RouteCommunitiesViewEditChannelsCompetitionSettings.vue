ebe55591e87de37f5185e13a0d055f56d44bb78c
ramagj <93935871+ramagj@users.noreply.github.com>
2023-11-24 01:53:00 +0800

    RouteCommunitiesViewEditChannelsCompetitionSettings

---
+++ src/app/views/communities/view/edit/channels/edit/competition/settings/RouteCommunitiesViewEditChannelsCompetitionSettings.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { computed, toRef } from 'vue';
+import {
+	createAppRoute,
+	defineAppRouteOptions,
+} from '../../../../../../../../../_common/route/route-component';
+import FormCommunityCompetitionEdit from '../../../../../../../../components/forms/community/competition/edit/edit.vue';
+import { useCommunityRouteStore } from '../../../../../view.store';
+
+export default {
+	...defineAppRouteOptions({}),
+};
+</script>
+
+<script lang="ts" setup>
+const routeStore = useCommunityRouteStore()!;
+
+const competition = toRef(() => {
+	return routeStore.competition!;
+});
+
+createAppRoute({
+	routeTitle: computed(() => ``),
+});
+</script>
+
+<template>
+	<FormCommunityCompetitionEdit :model="competition" />
+</template>
