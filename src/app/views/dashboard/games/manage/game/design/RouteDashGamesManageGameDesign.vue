6c93979f44a105fd09ae5ef7d46dd89c7a5cab0c
ramagj <93935871+ramagj@users.noreply.github.com>
2023-11-29 03:52:36 +0800

    RouteDashGamesManageGameDesign

---
+++ src/app/views/dashboard/games/manage/game/design/RouteDashGamesManageGameDesign.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+import { computed } from 'vue';
+import { Api } from '../../../../../../../_common/api/api.service';
+import {
+	createAppRoute,
+	defineAppRouteOptions,
+} from '../../../../../../../_common/route/route-component';
+import { useThemeStore } from '../../../../../../../_common/theme/theme.store';
+import { $gettext } from '../../../../../../../_common/translate/translate.service';
+import FormGameDesign from '../../../../../../components/forms/game/design/design.vue';
+import { ManageGameThemeKey, useGameDashRouteController } from '../../manage.store';
+
+export default {
+	...defineAppRouteOptions({
+		deps: {},
+		resolver: ({ route }) =>
+			Api.sendRequest('/web/dash/developer/games/media/' + route.params.id),
+	}),
+};
+</script>
+
+<script lang="ts" setup>
+const themeStore = useThemeStore();
+const routeStore = useGameDashRouteController()!;
+
+const { game } = routeStore;
+
+function onSubmit() {
+	themeStore.setPageTheme({
+		key: ManageGameThemeKey,
+		theme: game.value!.theme ?? null,
+	});
+}
+
+createAppRoute({
+	routeTitle: computed(() => {
+		if (game.value) {
+			return $gettext(`Edit Design for %{ game }`, {
+				game: game.value.title,
+			});
+		}
+		return null;
+	}),
+	onResolved({ payload }) {
+		routeStore.populateMedia(payload.mediaItems || []);
+	},
+});
+</script>
+
+<template>
+	<div class="route-manage-game-design">
+		<div class="row">
+			<div class="col-md-9">
+				<FormGameDesign :model="game!" @submit="onSubmit" />
+			</div>
+		</div>
+	</div>
+</template>
