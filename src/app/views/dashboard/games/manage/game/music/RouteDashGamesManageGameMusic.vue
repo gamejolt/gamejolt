612076319d270b67081f19ce936b0d6fd923895d
ramagj <93935871+ramagj@users.noreply.github.com>
2023-11-29 04:31:05 +0800

    RouteDashGamesManageGameMusic

--- src/app/views/dashboard/games/manage/game/music/RouteDashGamesManageGameMusic.vue
+++ src/app/views/dashboard/games/manage/game/music/RouteDashGamesManageGameMusic.vue
@@ -1,6 +1,5 @@
 <script lang="ts">
-import { setup } from 'vue-class-component';
-import { Options } from 'vue-property-decorator';
+import { computed, ref } from 'vue';
 import { Api } from '../../../../../../../_common/api/api.service';
 import AppCardList from '../../../../../../../_common/card/list/AppCardList.vue';
 import AppCardListAdd from '../../../../../../../_common/card/list/AppCardListAdd.vue';
@@ -11,104 +10,91 @@ import {
 	$saveGameSongSort,
 	GameSongModel,
 } from '../../../../../../../_common/game/song/song.model';
+import AppJolticon from '../../../../../../../_common/jolticon/AppJolticon.vue';
 import AppLoadingFade from '../../../../../../../_common/loading/AppLoadingFade.vue';
 import { showModalConfirm } from '../../../../../../../_common/modal/confirm/confirm-service';
 import {
-	LegacyRouteComponent,
-	OptionsForLegacyRoute,
-} from '../../../../../../../_common/route/legacy-route-component';
+	createAppRoute,
+	defineAppRouteOptions,
+} from '../../../../../../../_common/route/route-component';
+import { $gettext } from '../../../../../../../_common/translate/translate.service';
 import { arrayRemove } from '../../../../../../../utils/array';
 import FormGameSong from '../../../../../../components/forms/game/song/song.vue';
 import AppDashGameWizardControls from '../../../../../../components/forms/game/wizard-controls/AppDashGameWizardControls.vue';
 import { useGameDashRouteController } from '../../manage.store';
 
-@Options({
-	name: 'RouteDashGamesManageGameMusic',
-	components: {
-		FormGameSong,
-		AppCardList,
-		AppCardListDraggable,
-		AppCardListItem,
-		AppCardListAdd,
-		AppDashGameWizardControls,
-		AppLoadingFade,
-	},
-})
-@OptionsForLegacyRoute({
-	deps: {},
-	resolver: ({ route }) => Api.sendRequest('/web/dash/developer/games/music/' + route.params.id),
-})
-export default class RouteDashGamesManageGameMusic extends LegacyRouteComponent {
-	routeStore = setup(() => useGameDashRouteController()!);
+export default {
+	...defineAppRouteOptions({
+		deps: {},
+		resolver: ({ route }) =>
+			Api.sendRequest('/web/dash/developer/games/music/' + route.params.id),
+	}),
+};
+</script>
 
-	get game() {
-		return this.routeStore.game!;
+<script lang="ts" setup>
+const { game } = useGameDashRouteController()!;
+
+const songs = ref<GameSongModel[]>([]);
+const isAdding = ref(false);
+const isProcessing = ref(false);
+const activeItem = ref<GameSongModel | undefined>(undefined);
+
+const currentSort = computed(() => songs.value.map(item => item.id));
+
+function onSongEdited() {
+	activeItem.value = undefined;
+}
+
+function onSongAdded(formModel: GameSongModel) {
+	songs.value.push(new GameSongModel(formModel));
+	isAdding.value = false;
+}
+
+function saveSongSort(newSongs: GameSongModel[]) {
+	songs.value = newSongs;
+	$saveGameSongSort(game.value!.id, currentSort.value);
+}
+
+async function removeSong(song: GameSongModel) {
+	const result = await showModalConfirm($gettext('Are you sure you want to remove this song?'));
+
+	if (!result) {
+		return;
 	}
 
-	songs: GameSongModel[] = [];
-	isAdding = false;
-	isProcessing = false;
-	activeItem: GameSongModel | null = null;
+	isProcessing.value = true;
+	await $removeGameSong(song);
+	arrayRemove(songs.value, i => i.id === song.id);
+	isProcessing.value = false;
+}
 
-	get currentSort() {
-		return this.songs.map(item => item.id);
-	}
-
-	get routeTitle() {
-		if (this.game) {
-			return this.$gettext(`Manage Music for %{ game }`, {
-				game: this.game.title,
+const { isBootstrapped } = createAppRoute({
+	routeTitle: computed(() => {
+		if (game.value) {
+			return $gettext(`Manage Music for %{ game }`, {
+				game: game.value.title,
 			});
 		}
 		return null;
-	}
-
-	routeResolved($payload: any) {
-		this.songs = GameSongModel.populate($payload.songs);
-		this.isAdding = !this.songs.length;
-	}
-
-	onSongEdited() {
-		this.activeItem = null;
-	}
-
-	onSongAdded(formModel: GameSongModel) {
-		this.songs.push(new GameSongModel(formModel));
-		this.isAdding = false;
-	}
-
-	saveSongSort(songs: GameSongModel[]) {
-		this.songs = songs;
-		$saveGameSongSort(this.game.id, this.currentSort);
-	}
-
-	async removeSong(song: GameSongModel) {
-		const result = await showModalConfirm(
-			this.$gettext('Are you sure you want to remove this song?')
-		);
-
-		if (!result) {
-			return;
-		}
-
-		this.isProcessing = true;
-		await $removeGameSong(song);
-		arrayRemove(this.songs, i => i.id === song.id);
-		this.isProcessing = false;
-	}
-}
+	}),
+	onResolved({ payload }) {
+		songs.value = GameSongModel.populate(payload.songs);
+		isAdding.value = !songs.value.length;
+	},
+});
 </script>
 
 <template>
-	<div v-if="isRouteBootstrapped" class="row">
+	<div v-if="isBootstrapped" class="row">
 		<div class="col-sm-4 col-sm-push-8">
 			<div class="page-help">
 				<p>
-					<AppTranslate>
-						Let people enjoy your game's soundtrack whenever they want! Upload MP3s of
-						music from your game and the songs will appear on your game page in a nice
-						little music player. Don't upload copyrighted songs without permission!
-					</AppTranslate>
+					{{
+						$gettext(
+							`Let people enjoy your game's soundtrack whenever they want! Upload MP3s of music from your game and the songs will appear on your game page in a nice little music player. Don't upload copyrighted songs without permission!`
+						)
+					}}
 				</p>
 			</div>
 		</div>
@@ -116,10 +102,11 @@ export default class RouteDashGamesManageGameMusic extends LegacyRouteComponent 
 		<div class="col-sm-8 col-sm-pull-4">
 			<div v-if="!songs.length" class="alert">
 				<p>
-					<AppTranslate>
-						You haven't added any music. Upload some songs from your game! The music
-						player's worth it, trust us!
-					</AppTranslate>
+					{{
+						$gettext(
+							`You haven't added any music. Upload some songs from your game! The music player's worth it, trust us!`
+						)
+					}}
 				</p>
 			</div>
 
