0114603fb8c3672e3b7bfae33778aa07dc63d9e8
ramagj <93935871+ramagj@users.noreply.github.com>
2023-12-07 00:44:46 +0800

    RouteProfileFollowers

--- src/app/views/discover/games/view/followers/followers.route.ts
+++ src/app/views/discover/games/view/followers/followers.route.ts
@@ -3,5 +3,5 @@ import { RouteRecordRaw } from 'vue-router';
 export const routeDiscoverGamesViewFollowers: RouteRecordRaw = {
 	name: 'discover.games.view.followers',
 	path: 'followers',
-	component: () => import('./followers.vue'),
+	component: () => import('./RouteProfileFollowers.vue'),
 };
