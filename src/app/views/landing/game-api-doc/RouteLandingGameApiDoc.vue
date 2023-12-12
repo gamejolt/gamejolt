c31779309b4cfd0e38122bb7547f90f5c4595fd9
ramagj <93935871+ramagj@users.noreply.github.com>
2023-12-13 05:56:12 +0800

    RouteLandingGameApiDoc

--- src/app/views/landing/game-api-doc/RouteLandingGameApiDoc.vue
+++ src/app/views/landing/game-api-doc/RouteLandingGameApiDoc.vue
@@ -1,19 +1,15 @@
-import { h } from 'vue';
-import { Options } from 'vue-property-decorator';
+<script>
+import { computed, ref } from 'vue';
 import { ViteMarkdownExport } from '../../../../../typings/markdown';
 import { buildPayloadErrorForStatusCode } from '../../../../_common/payload/payload-service';
-import {
-	LegacyRouteComponent,
-	OptionsForLegacyRoute,
-} from '../../../../_common/route/legacy-route-component';
+import { createAppRoute, defineAppRouteOptions } from '../../../../_common/route/route-component';
+import { $gettext } from '../../../../_common/translate/translate.service';
 
 const paths = import.meta.glob<ViteMarkdownExport>('../../../../lib/doc-game-api/v1.x/**/*.md');
 
-@Options({
-	name: 'RouteLandingGameApiDocContent',
-})
-@OptionsForLegacyRoute({
-	async resolver({ route }) {
+export default {
+		...defineAppRouteOptions({
+			async resolver({ route }) {
 		console.log(route.params.path);
 		// First check the path as is, then check with "index".
 		let path = ((route.params.path || []) as string[]).join('/');
@@ -31,27 +27,34 @@ const paths = import.meta.glob<ViteMarkdownExport>('../../../../lib/doc-game-api
 
 		return buildPayloadErrorForStatusCode(404);
 	},
-})
-export default class RouteLandingGameApiDoc extends LegacyRouteComponent {
-	content = '';
+		}),
+	};
+</script>
 
-	get routeTitle() {
-		let title = this.$gettext(`Game API Documentation`);
+<script lang="ts" setup>
+const content = ref('');
+
+createAppRoute({
+	routeTitle: computed(() => {
+		let title = $gettext(`Game API Documentation`);
 
 		// We try to pull the first h1 for the title of the page.
-		const matches = this.content.match(/<h1([^>]*)>(.*?)<\/h1>/);
+		const matches = content.value.match(/<h1([^>]*)>(.*?)<\/h1>/);
 		if (matches) {
 			title = `${matches[2]} - ${title}`;
 		}
 
 		return title;
-	}
+	}),
+	onResolved({ payload }) {
+		content.value = payload;
+	},
+});
+</script>
 
-	routeResolved($payload: any) {
-		this.content = $payload;
-	}
-
-	render() {
-		return h('div', { innerHTML: this.content });
-	}
-}
+<template>
+	<!--TODO(component-setup-refactor-routes-4): revisit this-->
+	<div>
+		{{ content }}
+	</div>
+</template>
--- src/app/views/landing/game-api-doc/game-api-doc.route.ts
+++ src/app/views/landing/game-api-doc/game-api-doc.route.ts
@@ -3,7 +3,7 @@ import { RouteRecordRaw } from 'vue-router';
 const routeLandingGameApiDocContent: RouteRecordRaw = {
 	name: 'landing.game-api-doc',
 	path: ':path*',
-	component: () => import('./game-api-doc-content'),
+	component: () => import('./RouteLandingGameApiDoc.vue'),
 };
 
 export const routeLandingGameApiDoc: RouteRecordRaw = {
