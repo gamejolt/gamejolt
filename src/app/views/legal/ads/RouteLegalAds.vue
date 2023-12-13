4767458a1bb11fe0e4d15ed02d27913a885b80b4
ramagj <93935871+ramagj@users.noreply.github.com>
2023-12-14 04:11:48 +0800

    RouteLegalAds

---
+++ src/app/views/legal/ads/RouteLegalAds.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { computed } from 'vue';
+import { createAppRoute, defineAppRouteOptions } from '../../../../_common/route/route-component';
+import { $gettext } from '../../../../_common/translate/translate.service';
+
+export default {
+	...defineAppRouteOptions({}),
+};
+</script>
+
+<script lang="ts" setup>
+createAppRoute({
+	routeTitle: computed(() => $gettext('Advertising Platforms')),
+});
+</script>
+
+<template>
+	<!--TODO(component-setup-refactor-routes-4): usage of v-html-->
+	<div v-html="html"></div>
+</template>
--- src/app/views/legal/ads/ads.route.ts
+++ src/app/views/legal/ads/ads.route.ts
@@ -3,5 +3,5 @@ import { RouteRecordRaw } from 'vue-router';
 export const routeLegalAds: RouteRecordRaw = {
 	name: 'legal.ads',
 	path: '/privacy/ads',
-	component: () => import('./ads'),
+	component: () => import('./RouteLegalAds.vue'),
 };
