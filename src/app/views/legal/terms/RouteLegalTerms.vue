cd3cf9bc98991e79e37f1a5cd935c8888f30b853
ramagj <93935871+ramagj@users.noreply.github.com>
2023-12-14 04:33:30 +0800

    RouteLegalTerms

---
+++ src/app/views/legal/terms/RouteLegalTerms.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { computed } from 'vue';
+import { createAppRoute, defineAppRouteOptions } from '../../../../_common/route/route-component';
+import { $gettext } from '../../../../_common/translate/translate.service';
+
+// TODO(component-setup-refactor-routes-4): html not available in this import?
+import { html } from '../../../../lib/terms/terms/global.md';
+
+export default {
+	...defineAppRouteOptions({}),
+};
+</script>
+
+<script lang="ts" setup>
+createAppRoute({
+	routeTitle: computed(() => $gettext('Terms of Use')),
+});
+</script>
+
+<template>
+	<div>
+		<div v-html="html" />
+	</div>
+</template>
