2cacc15d4d38a62434b038a3746d43aca6379de8
ramagj <93935871+ramagj@users.noreply.github.com>
2024-01-04 22:07:56 +0800

    FormCommunityCompetitionEdit

--- src/app/components/forms/community/competition/edit/FormCommunityCompetitionEdit.vue
+++ src/app/components/forms/community/competition/edit/FormCommunityCompetitionEdit.vue
@@ -1,73 +1,79 @@
-<script lang="ts">
-import { mixins, Options } from 'vue-property-decorator';
+<script lang="ts" setup>
+import { computed, ref, toRefs } from 'vue';
 import {
-$saveCommunityCompetition,
-CommunityCompetitionModel,
-CompetitionPeriodVoting,
+	$saveCommunityCompetition,
+	CommunityCompetitionModel,
+	CompetitionPeriodVoting,
 } from '../../../../../../_common/community/competition/competition.model';
-import { formatDate } from '../../../../../../_common/filters/date';
+import AppForm, {
+	FormController,
+	createForm,
+	defineFormProps,
+} from '../../../../../../_common/form-vue/AppForm.vue';
+import AppFormButton from '../../../../../../_common/form-vue/AppFormButton.vue';
+import AppFormControlErrors from '../../../../../../_common/form-vue/AppFormControlErrors.vue';
+import AppFormGroup from '../../../../../../_common/form-vue/AppFormGroup.vue';
 import AppFormLegend from '../../../../../../_common/form-vue/AppFormLegend.vue';
 import AppFormControlDate from '../../../../../../_common/form-vue/controls/AppFormControlDate.vue';
+import AppFormControlSelect from '../../../../../../_common/form-vue/controls/AppFormControlSelect.vue';
 import { FormTimezoneService } from '../../../../../../_common/form-vue/form-timezone.service';
-import { BaseForm } from '../../../../../../_common/form-vue/form.service';
 import AppLoading from '../../../../../../_common/loading/AppLoading.vue';
+import { $gettext } from '../../../../../../_common/translate/translate.service';
 import AppCommunityCompetitionDate from '../../../../community/competition/date/AppCommunityCompetitionDate.vue';
 
-class Wrapper extends BaseForm<CommunityCompetitionModel> {}
+const props = defineProps({
+	...defineFormProps<CommunityCompetitionModel>(true),
+});
 
-@Options({
-	components: {
-		AppFormLegend,
-		AppFormControlDate,
-		AppLoading,
-		AppCommunityCompetitionDate,
-	},
-})
-export default class FormCommunityCompetitionEdit extends mixins(Wrapper) {
-	modelClass = CommunityCompetitionModel;
-	modelSaveHandler = $saveCommunityCompetition;
+const { model } = toRefs(props);
 
-	timezoneService: FormTimezoneService<CommunityCompetitionModel> | null = null;
+const timezoneService = ref<FormTimezoneService<CommunityCompetitionModel> | null>(null);
 
-	readonly formatDate = formatDate;
+// Before and during the competition, start/end dates can be edited.
+const shouldShowSaveButton = computed(() => model!.value.periodNum < CompetitionPeriodVoting);
 
-	get shouldShowSaveButton() {
-		// Before and during the competition, start/end dates can be edited.
-		return this.model!.periodNum < CompetitionPeriodVoting;
-	}
-
+const form: FormController<CommunityCompetitionModel> = createForm({
+	model,
+	modelClass: CommunityCompetitionModel,
+	modelSaveHandler: $saveCommunityCompetition,
 	async onInit() {
-		this.timezoneService = new FormTimezoneService(this.form);
-		await this.timezoneService.load(true);
-	}
-}
+		timezoneService.value = new FormTimezoneService(form);
+		await timezoneService.value.load(true);
+	},
+});
 </script>
 
 <template>
 	<AppForm :controller="form">
 		<fieldset>
 			<AppFormLegend>
-				<AppTranslate>Date and Time</AppTranslate>
+				{{ $gettext(`Date and Time`) }}
 			</AppFormLegend>
 
 			<template v-if="model && timezoneService && timezoneService.loaded">
 				<template v-if="!model.hasStarted">
 					<AppFormGroup name="timezone" :label="$gettext(`Select a Timezone`)">
 						<p class="help-block">
-							<span v-translate>
-								All time selection below will use this timezone.
+							<span>
+								{{ $gettext(`All time selection below will use this timezone.`) }}
+								{{ ' ' }}
 								<i>
-									You can't change this once the jam starts, so make sure you've
-									set it correctly.
+									{{
+										$gettext(
+											`You can't change this once the jam starts, so make sure you've set it correctly.`
+										)
+									}}
 								</i>
 							</span>
 						</p>
 
 						<p class="help-block">
 							<strong>
-								<AppTranslate>
-									Should auto-detect, but if it doesn't, choose your closest city.
-								</AppTranslate>
+								{{
+									$gettext(
+										`Should auto-detect, but if it doesn't, choose your closest city.`
+									)
+								}}
 							</strong>
 						</p>
 
@@ -93,9 +99,7 @@ export default class FormCommunityCompetitionEdit extends mixins(Wrapper) {
 				<template v-else>
 					<AppFormGroup name="timezone" :label="$gettext(`Selected Timezone`)">
 						<p class="help-block">
-							<AppTranslate>
-								All time selection below are using this timezone.
-							</AppTranslate>
+							{{ $gettext(`All time selection below are using this timezone.`) }}
 						</p>
 						{{ timezoneService.activeTimezoneName }}
 					</AppFormGroup>
@@ -104,16 +108,23 @@ export default class FormCommunityCompetitionEdit extends mixins(Wrapper) {
 				<AppFormGroup name="starts_on" :label="$gettext(`Start Date and Time`)">
 					<template v-if="!model.hasStarted">
 						<p class="help-block">
-							<span v-translate>
-								Your jam begins on this date and time.
-								<i>You can change this anytime before the jam starts.</i>
+							<span>
+								{{ $gettext(`Your jam begins on this date and time.`) }}
+								{{ ' ' }}
+								<i>
+									{{
+										$gettext(
+											`You can change this anytime before the jam starts.`
+										)
+									}}
+								</i>
 							</span>
 						</p>
 
 						<AppFormControlDate
 							:timezone-offset="timezoneService.activeTimezoneOffset"
 							:min-date="timezoneService.now"
-							:max-date="formModel.ends_on"
+							:max-date="form.formModel.ends_on"
 						/>
 						<AppFormControlErrors />
 					</template>
@@ -125,7 +136,7 @@ export default class FormCommunityCompetitionEdit extends mixins(Wrapper) {
 						/>
 
 						<p class="help-block">
-							<AppTranslate>Your jam began on this date and time.</AppTranslate>
+							{{ $gettext(`Your jam began on this date and time.`) }}
 						</p>
 					</template>
 				</AppFormGroup>
@@ -134,16 +145,23 @@ export default class FormCommunityCompetitionEdit extends mixins(Wrapper) {
 					<template v-if="!model.hasEnded">
 						<p class="help-block">
 							<span v-translate>
-								Your jam ends on this date and time.
-								<i>You can change this anytime before the jam ends.</i>
+								{{ $gettext(`Your jam ends on this date and time.`) }}
+								{{ ' ' }}
+								<i>
+									{{
+										$gettext(`You can change this anytime before the jam ends.`)
+									}}
+								</i>
 							</span>
 						</p>
 
 						<AppFormControlDate
 							:timezone-offset="timezoneService.activeTimezoneOffset"
-							:min-date="formModel.starts_on"
+							:min-date="form.formModel.starts_on"
 							:max-date="
-								formModel.is_voting_enabled ? formModel.voting_ends_on : undefined
+								form.formModel.is_voting_enabled
+									? form.formModel.voting_ends_on
+									: undefined
 							"
 						/>
 						<AppFormControlErrors />
@@ -156,7 +174,7 @@ export default class FormCommunityCompetitionEdit extends mixins(Wrapper) {
 						/>
 
 						<p class="help-block">
-							<AppTranslate>Your jam ended on this date and time.</AppTranslate>
+							{{ $gettext(`Your jam ended on this date and time.`) }}
 						</p>
 					</template>
 				</AppFormGroup>
@@ -172,7 +190,7 @@ export default class FormCommunityCompetitionEdit extends mixins(Wrapper) {
 			show-when-valid
 			:disabled="!timezoneService || !timezoneService.loaded"
 		>
-			<AppTranslate>Save</AppTranslate>
+			{{ $gettext(`Save`) }}
 		</AppFormButton>
 	</AppForm>
 </template>
