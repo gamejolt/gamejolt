382c71f4c8e7efd4e68ea8fbabfcb9b61ef88960
hapurubokka <somekid11@gmail.com>
2024-01-04 14:16:59 -0500

    resolve todo

--- src/_common/ad/ad-store.ts
+++ src/_common/ad/ad-store.ts
@@ -17,9 +17,7 @@ type AdStore = ReturnType<typeof createAdStore>;
 const AdStoreKey: InjectionKey<AdStore> = Symbol('ads');
 
 // To show ads on the page for dev, just change this to false.
-// TODO(profile-scrunch) Swap these after review
-// export const AdsDisabledDev = GJ_BUILD_TYPE === 'serve-hmr' || GJ_BUILD_TYPE === 'serve-build';
-export const AdsDisabledDev = false;
+export const AdsDisabledDev = GJ_BUILD_TYPE === 'serve-hmr' || GJ_BUILD_TYPE === 'serve-build';
 
 const areAdsDisabledForDevice =
 	GJ_IS_DESKTOP_APP || import.meta.env.SSR || isDynamicGoogleBot() || AdsDisabledDev;
