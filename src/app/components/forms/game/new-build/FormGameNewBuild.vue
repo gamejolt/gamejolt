8648fb3967cfc328fc6aa797bf311c97bad9548c
David DeCarmine <hworld@holo-world.com>
2026-04-07 11:08:16 -0600

    Migrate FormGameNewBuild from BaseForm mixin to createForm script setup

---
+++ src/app/components/forms/game/new-build/FormGameNewBuild.vue
@@ -0,0 +1,162 @@
+<script lang="ts" setup>
+import { computed, ref, toRef, watch } from 'vue';
+import AppExpand from '../../../../../_common/expand/AppExpand.vue';
+import AppForm, { createForm, FormController } from '../../../../../_common/form-vue/AppForm.vue';
+import AppFormControlErrors from '../../../../../_common/form-vue/AppFormControlErrors.vue';
+import AppFormGroup from '../../../../../_common/form-vue/AppFormGroup.vue';
+import AppFormControlUpload from '../../../../../_common/form-vue/controls/upload/AppFormControlUpload.vue';
+import {
+	$saveGameBuild,
+	GameBuildModel,
+	GameBuildType,
+} from '../../../../../_common/game/build/build.model';
+import { GameModel } from '../../../../../_common/game/game.model';
+import { GamePackageModel } from '../../../../../_common/game/package/package.model';
+import { GameReleaseModel } from '../../../../../_common/game/release/release.model';
+import AppTranslate from '../../../../../_common/translate/AppTranslate.vue';
+
+type NewGameBuildFormModel = GameBuildModel & {
+	file: File;
+};
+
+type Props = {
+	type: 'downloadable' | 'browser';
+	game: GameModel;
+	package: GamePackageModel;
+	release: GameReleaseModel;
+	builds?: GameBuildModel[];
+	model?: GameBuildModel;
+};
+
+const props = defineProps<Props>();
+
+const maxFilesize = ref(0);
+const restrictedPlatforms = ref<string[]>([]);
+const forceOther = ref(false);
+const romTypes = ref<string[]>([]);
+let browserTypes: { [ext: string]: string } = {};
+
+const form: FormController<NewGameBuildFormModel> = createForm({
+	model: toRef(props, 'model'),
+	modelClass: GameBuildModel as any,
+	modelSaveHandler: $saveGameBuild,
+	reloadOnSubmit: true,
+	warnOnDiscard: false,
+	loadUrl: computed(
+		() =>
+			`/web/dash/developer/games/builds/save/${props.game.id}/${props.package.id}/${props.release.id}`
+	),
+	onInit() {
+		// Set the game ID on the form model from the game passed in.
+		form.formModel.type = props.type as any;
+		form.formModel.game_id = props.game.id;
+		form.formModel.game_package_id = props.package.id;
+		form.formModel.game_release_id = props.release.id;
+
+		browserTypes = {
+			'.zip': GameBuildType.Html,
+			'.swf': GameBuildType.Flash,
+			'.unity3d': GameBuildType.Unity,
+		};
+	},
+	onLoad(payload: any) {
+		maxFilesize.value = payload.maxFilesize;
+		restrictedPlatforms.value = payload.restrictedPlatforms;
+		forceOther.value = payload.forceOther;
+		romTypes.value = payload.romTypes;
+
+		// ROM types can change, so we pull from server.
+		if (romTypes.value) {
+			for (const ext of romTypes.value) {
+				browserTypes[ext] = GameBuildType.Rom;
+			}
+		}
+	},
+});
+
+const uploadAccept = computed(() => {
+	if (props.type !== 'browser') {
+		return undefined;
+	}
+	return Object.keys(browserTypes).join(',');
+});
+
+const browserTypeValid = computed(() => {
+	if (props.type !== 'browser' || !props.builds || !form.formModel.file) {
+		return true;
+	}
+
+	const releaseTypes = props.builds
+		.filter(build => build.type !== GameBuildType.Downloadable)
+		.map(build => build.type);
+
+	const accept = Object.entries(browserTypes)
+		.filter(entry => {
+			return releaseTypes.indexOf(entry[1]) === -1;
+		})
+		.map(entry => entry[0]);
+
+	const fileType = form.formModel.file.name.slice(form.formModel.file.name.lastIndexOf('.'));
+	return accept.indexOf(fileType) !== -1;
+});
+
+const hasBrowserTypeError = computed(() => form.hasCustomError('browserType'));
+
+watch(
+	() => form.formModel.file,
+	() => {
+		if (!browserTypeValid.value) {
+			form.setCustomError('browserType');
+		} else {
+			form.clearCustomError('browserType');
+		}
+	}
+);
+
+function submit() {
+	if (!form.formModel.file) {
+		return;
+	}
+	form.submit();
+}
+</script>
+
+<template>
+	<AppForm class="game-new-build-form" :controller="form">
+		<!--
+		Can only add files. Can't edit builds anymore.
+		They need to release a new version to do that.
+	-->
+		<AppFormGroup
+			name="file"
+			:label="$gettext('Upload File')"
+			:hide-label="true"
+			:optional="true"
+		>
+			<AppFormControlUpload
+				:validators="[validateFilesize(maxFilesize)]"
+				:accept="uploadAccept"
+				@changed="submit"
+			/>
+
+			<AppFormControlErrors :label="$gettext(`file`)" />
+
+			<AppExpand :when="hasBrowserTypeError">
+				<br />
+				<div class="alert alert-notice sans-margin-bottom">
+					<AppTranslate>
+						You can't upload multiple browser builds of the same type into the same
+						release. If you're trying to update your build, add a new release first.
+					</AppTranslate>
+				</div>
+			</AppExpand>
+
+			<p v-if="type === 'browser'" class="help-block">
+				<AppTranslate>
+					For HTML builds, upload a .zip archive containing all of your build's files and
+					assets. There must be an index.html file in the root folder.
+				</AppTranslate>
+			</p>
+		</AppFormGroup>
+	</AppForm>
+</template>
