e7e35a61da8ca13e4f445673ba39401937622862
David DeCarmine <hworld@holo-world.com>
2026-04-07 11:23:52 -0600

    Migrate RouteSiteEditor from LegacyRouteComponent to script setup

--- src/site-editor/views/index.ts
+++ src/site-editor/views/index.ts
@@ -1,46 +1,12 @@
-import { h } from 'vue';
-import { setup } from 'vue-class-component';
-import { Options } from 'vue-property-decorator';
 import { RouteRecordRaw } from 'vue-router';
-import { Api } from '../../_common/api/api.service';
-import {
-	LegacyRouteComponent,
-	OptionsForLegacyRoute,
-} from '../../_common/route/legacy-route-component';
 import { initRouter } from '../../utils/router';
-import { useSiteEditorStore } from '../store/index';
-
-// Empty route component. We just use it to send API calls and set up the store
-// for the app component to show the correct stuff.
-@Options({
-	name: 'RouteEditor',
-})
-@OptionsForLegacyRoute({
-	lazy: false,
-	cache: false,
-	reloadOn: 'always',
-	resolver({ route }) {
-		const siteId = parseInt(route.query.id as string, 10);
-		return Api.sendRequest(`/web/dash/sites/editor/${siteId}`);
-	},
-})
-class RouteEditor extends LegacyRouteComponent {
-	store = setup(() => useSiteEditorStore());
-
-	routeResolved(payload: any) {
-		this.store.bootstrapTab(this.$route.params.tab as any, payload);
-	}
-
-	render() {
-		return h('div');
-	}
-}
+import RouteSiteEditor from './RouteSiteEditor.vue';
 
 const routes: RouteRecordRaw[] = [
 	{
 		name: 'editor',
 		path: '/site-editor/:tab',
-		component: RouteEditor,
+		component: RouteSiteEditor,
 	},
 ];
 
